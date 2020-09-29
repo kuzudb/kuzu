520d7b2d9158753a0eed40692ee6573c46dfbc34
g31pranjal <g31.pranjal@gmail.com>
2020-09-29 16:51:51 -0400

    types and vertex columns

---
+++ src/main/graphflow/utils/types.h
@@ -0,0 +1,28 @@
+#ifndef GRAPHFLOW_UTILS_TYPES_H_
+#define GRAPHFLOW_UTILS_TYPES_H_
+
+#include <cstdint>
+#include <cmath>
+#include <cfloat>
+
+#include <string>
+
+namespace graphflow {
+namespace utils {
+
+typedef uint32_t gfLabel_t;
+typedef uint32_t gfNodeOffset_t;
+typedef int32_t gfInt_t;
+typedef double_t gfDouble_t;
+typedef uint8_t gfBool_t;
+typedef std::string * gfString_t;
+
+const gfInt_t NULL_GFINT = INT32_MIN;
+const gfDouble_t NULL_GFDOUBLE = DBL_MIN;
+const gfString_t NULL_gfSTRING = nullptr;
+const gfBool_t NULL_GFBOOL = 0;
+
+} //namespace storage
+} //namespace utils
+
+#endif //GRAPHFLOW_UTILS_TYPES_H_
