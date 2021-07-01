c1cc139705040f43cc45277b343929cca3a84dd9
Xiyang <xiyangf1997@gmail.com>
2021-06-30 20:47:18 -0700

    add antlr error for non-binary comparison

--- src/parser/include/antlr_parser/graphflow_cypher_parser.h
+++ src/parser/include/antlr_parser/graphflow_cypher_parser.h
@@ -19,6 +19,8 @@ public:
     void notifyEmptyToken(antlr4::Token* startToken) override;
 
     void notifyReturnNotAtEnd(antlr4::Token* startToken) override;
+
+    void notifyNonBinaryComparison(antlr4::Token* startToken) override;
 };
 
 } // namespace parser
