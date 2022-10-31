390e229450dc70f90f3193f4f2aa446dea9a2d05
a8chakra <anurag.chakraborty98@gmail.com>
2022-10-31 01:42:31 -0400

    refactor left & right operations

--- src/function/string/operations/include/left_operation.h
+++ src/function/string/operations/include/left_operation.h
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <cstring>
 
+#include "length_operation.h"
 #include "substr_operation.h"
 
 #include "src/common/types/include/gf_string.h"
@@ -19,33 +20,10 @@ struct Left {
 public:
     static inline void operation(
         gf_string_t& left, int64_t& right, gf_string_t& result, ValueVector& resultValueVector) {
-        auto totalCharCount = getLength(left);
-        int64_t len;
-        if (right > 0) {
-            len = min(totalCharCount, (uint32_t)right);
-        } else if ((totalCharCount + right) > 0) {
-            len = (totalCharCount + right);
-        } else {
-            len = 0;
-        }
+        int64_t leftLen;
+        Length::operation(left, leftLen);
+        int64_t len = (right > 0) ? min(leftLen, right) : max(leftLen + right, 0l);
         SubStr::operation(left, 1, len, result, resultValueVector);
-    }
-
-    static uint32_t getLength(gf_string_t& input) {
-        auto totalByteLength = input.len;
-        auto inputString = input.getAsString();
-        for (uint32_t i = 0; i < totalByteLength; i++) {
-            if (inputString[i] & 0x80) {
-                uint32_t length = 0;
-                utf8proc_grapheme_callback(
-                    inputString.c_str(), totalByteLength, [&](size_t start, size_t end) {
-                        length++;
-                        return true;
-                    });
-                return length;
-            }
-        }
-        return input.len;
     }
 };
 
