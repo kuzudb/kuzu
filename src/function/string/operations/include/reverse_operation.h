7c7167321aa0ca4c7a7d467e2864ca5423d93c17
a8chakra <anurag.chakraborty98@gmail.com>
2022-11-02 15:27:47 -0400

    add utf8 support for reverse function

--- src/function/string/operations/include/reverse_operation.h
+++ src/function/string/operations/include/reverse_operation.h
@@ -18,7 +18,39 @@ struct Reverse {
 public:
     static inline void operation(
         gf_string_t& input, gf_string_t& result, ValueVector& resultValueVector) {
-        BaseStrOperation::operation(input, result, resultValueVector, reverseStr);
+        bool isAscii = true;
+        string inputStr = input.getAsString();
+        for (uint32_t i = 0; i < input.len; i++) {
+            if (inputStr[i] & 0x80) {
+                isAscii = false;
+                break;
+            }
+        }
+        if (isAscii) {
+            BaseStrOperation::operation(input, result, resultValueVector, reverseStr);
+        } else {
+            result.len = input.len;
+            if (result.len <= gf_string_t::SHORT_STR_LENGTH) {
+                utf8proc_grapheme_callback(
+                    inputStr.c_str(), input.len, [&](size_t start, size_t end) {
+                        memcpy(
+                            result.prefix + input.len - end, input.getData() + start, end - start);
+                        return true;
+                    });
+            } else {
+                result.overflowPtr = reinterpret_cast<uint64_t>(
+                    resultValueVector.getOverflowBuffer().allocateSpace(input.len));
+                auto buffer = reinterpret_cast<char*>(result.overflowPtr);
+                utf8proc_grapheme_callback(
+                    inputStr.c_str(), input.len, [&](size_t start, size_t end) {
+                        memcpy(buffer + input.len - end, input.getData() + start, end - start);
+                        return true;
+                    });
+                memcpy(result.prefix, buffer,
+                    result.len < gf_string_t::PREFIX_LENGTH ? result.len :
+                                                              gf_string_t::PREFIX_LENGTH);
+            }
+        }
     }
 
 private:
