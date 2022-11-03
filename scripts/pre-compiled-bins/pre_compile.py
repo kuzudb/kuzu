44c93f591275cf944212c9a2795b75c89b2b4f1e
Chang Liu <liuc223@gmail.com>
2022-11-03 18:06:52 -0400

    Fix bin merge for arm64 mac

--- scripts/pre-compiled-bins/pre_compile.py
+++ scripts/pre-compiled-bins/pre_compile.py
@@ -35,6 +35,7 @@ def build():
         if "arm64" in archflags and platform.machine() == "x86_64":
             args.append("--macos_cpus=arm64")
             args.append("--cpu=darwin_arm64")
+            args.append('--cxxopt=--target=arm64-apple-darwin')
 
         # It seems bazel does not automatically pick up
         # MACOSX_DEPLOYMENT_TARGETfrom the environment, so we need to pass
@@ -66,8 +67,13 @@ def merge_libs():
         for file in files:
             if file.endswith(".o"):
                 input_files.append(os.path.join(root, file))
-
-    subprocess.run([cxx, "-shared", "-o", output_path, *input_files],
+    if sys.platform == 'darwin':
+        archflags = os.getenv("ARCHFLAGS", "")
+        if "arm64" in archflags and platform.machine() == "x86_64":
+            additional_args = ["--target=arm64-apple-darwin"]
+        else:
+            additional_args = []
+    subprocess.run([cxx, *additional_args, "-shared", "-o", output_path, *input_files],
                    cwd=base_dir, env=env_vars, check=True)
     logging.info("Shared lib merged")
 
