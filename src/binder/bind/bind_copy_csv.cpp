feae1eccc42dbb8832b704f47b6d34aeb0bd235f
xiyang <x74feng@uwaterloo.ca>
2022-11-08 14:55:45 +0800

    refactor binder

---
+++ src/binder/bind/bind_copy_csv.cpp
@@ -0,0 +1,80 @@
+#include "src/binder/bound_copy_csv/include/bound_copy_csv.h"
+#include "src/binder/expression/include/literal_expression.h"
+#include "src/binder/include/binder.h"
+#include "src/parser/copy_csv/include/copy_csv.h"
+
+namespace graphflow {
+namespace binder {
+
+unique_ptr<BoundStatement> Binder::bindCopyCSV(const Statement& statement) {
+    auto& copyCSV = (CopyCSV&)statement;
+    auto catalogContent = catalog.getReadOnlyVersion();
+    auto tableName = copyCSV.getTableName();
+    validateTableExist(catalog, tableName);
+    auto isNodeTable = catalogContent->containNodeTable(tableName);
+    auto tableID = isNodeTable ? catalogContent->getNodeTableIDFromName(tableName) :
+                                 catalogContent->getRelTableIDFromName(tableName);
+    auto filePath = copyCSV.getCSVFileName();
+    auto csvReaderConfig = bindParsingOptions(copyCSV.getParsingOptions());
+    return make_unique<BoundCopyCSV>(
+        CSVDescription(filePath, csvReaderConfig), TableSchema(tableName, tableID, isNodeTable));
+}
+
+CSVReaderConfig Binder::bindParsingOptions(
+    const unordered_map<string, unique_ptr<ParsedExpression>>* parsingOptions) {
+    CSVReaderConfig csvReaderConfig;
+    for (auto& parsingOption : *parsingOptions) {
+        auto copyOptionName = parsingOption.first;
+        bool isValidStringParsingOption = validateStringParsingOptionName(copyOptionName);
+        auto copyOptionExpression = parsingOption.second.get();
+        auto boundCopyOptionExpression = expressionBinder.bindExpression(*copyOptionExpression);
+        assert(boundCopyOptionExpression->expressionType = LITERAL);
+        if (copyOptionName == "HEADER") {
+            if (boundCopyOptionExpression->dataType.typeID != BOOL) {
+                throw BinderException(
+                    "The value type of parsing csv option " + copyOptionName + " must be boolean.");
+            }
+            csvReaderConfig.hasHeader =
+                ((LiteralExpression&)(*boundCopyOptionExpression)).literal->val.booleanVal;
+        } else if (boundCopyOptionExpression->dataType.typeID == STRING &&
+                   isValidStringParsingOption) {
+            if (boundCopyOptionExpression->dataType.typeID != STRING) {
+                throw BinderException(
+                    "The value type of parsing csv option " + copyOptionName + " must be string.");
+            }
+            auto copyOptionValue =
+                ((LiteralExpression&)(*boundCopyOptionExpression)).literal->strVal;
+            bindStringParsingOptions(csvReaderConfig, copyOptionName, copyOptionValue);
+        } else {
+            throw BinderException("Unrecognized parsing csv option: " + copyOptionName + ".");
+        }
+    }
+    return csvReaderConfig;
+}
+
+void Binder::bindStringParsingOptions(
+    CSVReaderConfig& csvReaderConfig, const string& optionName, string& optionValue) {
+    auto parsingOptionValue = bindParsingOptionValue(optionValue);
+    if (optionName == "ESCAPE") {
+        csvReaderConfig.escapeChar = parsingOptionValue;
+    } else if (optionName == "DELIM") {
+        csvReaderConfig.tokenSeparator = parsingOptionValue;
+    } else if (optionName == "QUOTE") {
+        csvReaderConfig.quoteChar = parsingOptionValue;
+    } else if (optionName == "LIST_BEGIN") {
+        csvReaderConfig.listBeginChar = parsingOptionValue;
+    } else if (optionName == "LIST_END") {
+        csvReaderConfig.listEndChar = parsingOptionValue;
+    }
+}
+
+char Binder::bindParsingOptionValue(string value) {
+    if (value.length() > 2 || (value.length() == 2 && value[0] != '\\')) {
+        throw BinderException("Copy csv option value can only be a single character with an "
+                              "optional escape character.");
+    }
+    return value[value.length() - 1];
+}
+
+} // namespace binder
+} // namespace graphflow
---
+++ src/binder/bind/bind_updating_clause.cpp
@@ -0,0 +1,125 @@
+#include "src/binder/expression/include/literal_expression.h"
+#include "src/binder/include/binder.h"
+#include "src/binder/query/updating_clause/include/bound_create_clause.h"
+#include "src/binder/query/updating_clause/include/bound_delete_clause.h"
+#include "src/binder/query/updating_clause/include/bound_set_clause.h"
+#include "src/parser/query/updating_clause/include/create_clause.h"
+#include "src/parser/query/updating_clause/include/delete_clause.h"
+#include "src/parser/query/updating_clause/include/set_clause.h"
+
+namespace graphflow {
+namespace binder {
+
+unique_ptr<BoundUpdatingClause> Binder::bindUpdatingClause(const UpdatingClause& updatingClause) {
+    switch (updatingClause.getClauseType()) {
+    case ClauseType::CREATE: {
+        return bindCreateClause(updatingClause);
+    }
+    case ClauseType::SET: {
+        return bindSetClause(updatingClause);
+    }
+    case ClauseType::DELETE: {
+        return bindDeleteClause(updatingClause);
+    }
+    default:
+        assert(false);
+    }
+}
+
+unique_ptr<BoundUpdatingClause> Binder::bindCreateClause(const UpdatingClause& updatingClause) {
+    auto& createClause = (CreateClause&)updatingClause;
+    auto prevVariablesInScope = variablesInScope;
+    auto [queryGraphCollection, propertyCollection] =
+        bindGraphPattern(createClause.getPatternElements());
+    vector<unique_ptr<BoundCreateNode>> boundCreateNodes;
+    vector<unique_ptr<BoundCreateRel>> boundCreateRels;
+    for (auto i = 0u; i < queryGraphCollection->getNumQueryGraphs(); ++i) {
+        auto queryGraph = queryGraphCollection->getQueryGraph(i);
+        for (auto j = 0u; j < queryGraph->getNumQueryNodes(); ++j) {
+            auto node = queryGraph->getQueryNode(j);
+            if (!prevVariablesInScope.contains(node->getRawName())) {
+                boundCreateNodes.push_back(bindCreateNode(node, *propertyCollection));
+            }
+        }
+        for (auto j = 0u; j < queryGraph->getNumQueryRels(); ++j) {
+            auto rel = queryGraph->getQueryRel(j);
+            if (!prevVariablesInScope.contains(rel->getRawName())) {
+                boundCreateRels.push_back(bindCreateRel(rel, *propertyCollection));
+            }
+        }
+    }
+    auto boundCreateClause =
+        make_unique<BoundCreateClause>(std::move(boundCreateNodes), std::move(boundCreateRels));
+    return boundCreateClause;
+}
+
+unique_ptr<BoundCreateNode> Binder::bindCreateNode(
+    shared_ptr<NodeExpression> node, const PropertyKeyValCollection& collection) {
+    auto nodeTableSchema = catalog.getReadOnlyVersion()->getNodeTableSchema(node->getTableID());
+    auto primaryKey = nodeTableSchema->getPrimaryKey();
+    shared_ptr<Expression> primaryKeyExpression;
+    vector<expression_pair> setItems;
+    for (auto& [key, val] : collection.getPropertyKeyValPairs(*node)) {
+        auto propertyExpression = static_pointer_cast<PropertyExpression>(key);
+        if (propertyExpression->getPropertyKey() == primaryKey.propertyID) {
+            primaryKeyExpression = val;
+        }
+        setItems.emplace_back(key, val);
+    }
+    if (primaryKeyExpression == nullptr) {
+        throw BinderException("Create node " + node->getRawName() + " expects primary key " +
+                              primaryKey.name + " as input.");
+    }
+    return make_unique<BoundCreateNode>(
+        std::move(node), std::move(primaryKeyExpression), std::move(setItems));
+}
+
+unique_ptr<BoundCreateRel> Binder::bindCreateRel(
+    shared_ptr<RelExpression> rel, const PropertyKeyValCollection& collection) {
+    auto catalogContent = catalog.getReadOnlyVersion();
+    // CreateRel requires all properties in schema as input. So we rewrite set property to
+    // null if user does not specify a property in the query.
+    vector<expression_pair> setItems;
+    for (auto& property : catalogContent->getRelProperties(rel->getTableID())) {
+        if (collection.hasPropertyKeyValPair(*rel, property.name)) {
+            setItems.push_back(collection.getPropertyKeyValPair(*rel, property.name));
+        } else {
+            auto propertyExpression = make_shared<PropertyExpression>(
+                property.dataType, property.name, property.propertyID, rel);
+            setItems.emplace_back(std::move(propertyExpression),
+                LiteralExpression::createNullLiteralExpression(property.dataType));
+        }
+    }
+    return make_unique<BoundCreateRel>(std::move(rel), std::move(setItems));
+}
+
+unique_ptr<BoundUpdatingClause> Binder::bindSetClause(const UpdatingClause& updatingClause) {
+    auto& setClause = (SetClause&)updatingClause;
+    auto boundSetClause = make_unique<BoundSetClause>();
+    for (auto i = 0u; i < setClause.getNumSetItems(); ++i) {
+        auto setItem = setClause.getSetItem(i);
+        auto boundLhs = expressionBinder.bindExpression(*setItem->origin);
+        auto boundRhs = expressionBinder.bindExpression(*setItem->target);
+        boundRhs = ExpressionBinder::implicitCastIfNecessary(boundRhs, boundLhs->dataType.typeID);
+        boundSetClause->addSetItem(make_pair(boundLhs, boundRhs));
+    }
+    return boundSetClause;
+}
+
+unique_ptr<BoundUpdatingClause> Binder::bindDeleteClause(const UpdatingClause& updatingClause) {
+    auto& deleteClause = (DeleteClause&)updatingClause;
+    auto boundDeleteClause = make_unique<BoundDeleteClause>();
+    for (auto i = 0u; i < deleteClause.getNumExpressions(); ++i) {
+        auto boundExpression = expressionBinder.bindExpression(*deleteClause.getExpression(i));
+        if (boundExpression->dataType.typeID != NODE) {
+            throw BinderException("Delete " +
+                                  expressionTypeToString(boundExpression->expressionType) +
+                                  " is not supported.");
+        }
+        boundDeleteClause->addExpression(move(boundExpression));
+    }
+    return boundDeleteClause;
+}
+
+} // namespace binder
+} // namespace graphflow
