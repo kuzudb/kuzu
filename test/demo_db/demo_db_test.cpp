802e87f6999c977c6ec2c939fc45a3518535938b
Guodong Jin <guod.jin@gmail.com>
2022-11-14 21:44:16 -0500

    add update test queries from documentation

--- test/demo_db/demo_db_test.cpp
+++ test/demo_db/demo_db_test.cpp
@@ -32,3 +32,34 @@ TEST_F(DemoDBTest, CreateAvgNullTest) {
         "Karissa|40|40.000000|40|1|1", "Adam|30|30.000000|30|1|1", "Noura|25|25.000000|25|1|1"};
     ASSERT_EQ(TestHelper::convertResultToString(*result, true /* check order */), groundTruth);
 }
+
+TEST_F(DemoDBTest, DeleteNodeTest) {
+    ASSERT_TRUE(conn->query("CREATE (u:User {name: 'Alice', age: 35});")->isSuccess());
+    auto result = conn->query("MATCH (u:User) RETURN COUNT(*)");
+    auto groundTruth = vector<string>{"5"};
+    ASSERT_EQ(TestHelper::convertResultToString(*result, true /* check order */), groundTruth);
+    ASSERT_TRUE(conn->query("MATCH (u:User) WHERE u.name = 'Alice' DELETE u;")->isSuccess());
+    result = conn->query("MATCH (u:User) RETURN COUNT(*)");
+    groundTruth = vector<string>{"4"};
+    ASSERT_EQ(TestHelper::convertResultToString(*result, true /* check order */), groundTruth);
+}
+
+TEST_F(DemoDBTest, DeleteWithExceptionTest) {
+    auto result = conn->query("MATCH (u:User) WHERE u.name = 'Adam' DELETE u;");
+    ASSERT_FALSE(result->isSuccess());
+    ASSERT_EQ(result->getErrorMessage(),
+        "Runtime exception: Currently deleting a node with edges is not supported. node table 0 "
+        "nodeOffset 0 has 1 (one-to-many or many-to-many) edges for edge file: "
+        "test/unittest_temp/r-3-0-0.lists.");
+}
+
+TEST_F(DemoDBTest, SetNodeTest) {
+    ASSERT_TRUE(conn->query("MATCH (u:User) WHERE u.name = 'Adam' SET u.age = 50")->isSuccess());
+    auto result = conn->query("MATCH (u:User) WHERE u.name='Adam' RETURN u.age");
+    auto groundTruth = vector<string>{"50"};
+    ASSERT_EQ(TestHelper::convertResultToString(*result, true /* check order */), groundTruth);
+    ASSERT_TRUE(conn->query("MATCH (u:User) WHERE u.name = 'Adam' SET u.age = NULL")->isSuccess());
+    result = conn->query("MATCH (u:User) WHERE u.name='Adam' RETURN u.age");
+    groundTruth = vector<string>{""};
+    ASSERT_EQ(TestHelper::convertResultToString(*result, true /* check order */), groundTruth);
+}
