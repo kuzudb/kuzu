1d270257c80f7f0bd02107f53041eb5d3f29b005
Yash Oza <ynoza@uwaterloo.ca>
2023-01-25 01:12:02 -0500

    removed misc code and added functionality to pass query as param to execute

--- tools/nodejs_api/src/Samples/classexample.h
+++ tools/nodejs_api/src/Samples/classexample.h
@@ -1,17 +1,15 @@
 #include <napi.h>
 #include <iostream>
-#include "actualclass.h"
+#include "main/kuzu.h"
 
 class ClassExample : public Napi::ObjectWrap<ClassExample> {
  public:
   static Napi::Object Init(Napi::Env env, Napi::Object exports);
   ClassExample(const Napi::CallbackInfo& info);
-  ActualClass* GetInternalInstance();
 
  private:
   static Napi::FunctionReference constructor;
   Napi::Value Temp(const Napi::CallbackInfo& info);
-  Napi::Value GetValue(const Napi::CallbackInfo& info);
-  Napi::Value Add(const Napi::CallbackInfo& info);
-  ActualClass *actualClass_;
+  kuzu::main::Database *database_;
+  kuzu::main::Connection *connection_;
 };
