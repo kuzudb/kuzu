e421d656cdbea62e7dead1a8ceef6f1069280ff3
Yash Oza <ynoza@uwaterloo.ca>
2023-01-25 01:12:02 -0500

    removed misc code and added functionality to pass query as param to execute

--- tools/nodejs_api/src/Samples/classexample.cpp
+++ tools/nodejs_api/src/Samples/classexample.cpp
@@ -10,8 +10,6 @@ Napi::Object ClassExample::Init(Napi::Env env, Napi::Object exports) {
 
   Napi::Function t = DefineClass(env, "ClassExample", {
       InstanceMethod("temp", &ClassExample::Temp),
-      InstanceMethod("add", &ClassExample::Add),
-      InstanceMethod("getValue", &ClassExample::GetValue),
   });
 
   constructor = Napi::Persistent(t);
@@ -26,94 +24,48 @@ ClassExample::ClassExample(const Napi::CallbackInfo& info) : Napi::ObjectWrap<Cl
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
 
-  int length = info.Length();
-  
-  if (length != 1) {
-    Napi::TypeError::New(env, "Only one argument expected").ThrowAsJavaScriptException();
-  }
+  DatabaseConfig databaseConfig("test");
+  SystemConfig systemConfig(1ull << 30 /* set buffer manager size to 2GB */);
+  Database * database = new Database(databaseConfig, systemConfig);
 
-  if(!info[0].IsNumber()){
-    Napi::Object object_parent = info[0].As<Napi::Object>();
-    ClassExample* example_parent = Napi::ObjectWrap<ClassExample>::Unwrap(object_parent);
-    ActualClass* parent_actual_class_instance = example_parent->GetInternalInstance();
-    this->actualClass_ = new ActualClass(parent_actual_class_instance->getValue());
-    return;
-  }
+  this->database_ = database;
 
-  Napi::Number value = info[0].As<Napi::Number>();
-  this->actualClass_ = new ActualClass(value.DoubleValue());
+  auto connection = new Connection(this->database_);
+  this->connection_ = connection;
 }
 
 Napi::Value ClassExample::Temp(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
 
-  if (info.Length() != 1) {
-      Napi::TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
-      return Napi::Object::New(env);
+  // add parsing for queries TODO: make this smart? like check whether a user created before matching???
+  std::string query = "";
+  if (info.Length()>0) {
+    query = info[0].ToString().Utf8Value().c_str();
+    if (!query.starts_with("MATCH") && !query.starts_with("create") && !query.starts_with("COPY")) {
+        Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+        return Napi::Object::New(env);
+    }
   }
-  if (!info[0].IsString()) {
-      Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
-      return Napi::Object::New(env);
-  }
 
-  std::cout << "Arg: " << info[0].ToString().Utf8Value().c_str() << std::endl;
+  std::cout << "Arg: " << query << std::endl;
 
-  std::string fields = info[0].ToString().Utf8Value().c_str();
-
-  DatabaseConfig databaseConfig("test");
-  SystemConfig systemConfig(1ull << 30 /* set buffer manager size to 2GB */);
-  Database database(databaseConfig, systemConfig);
-
-  auto connection = Connection(&database);
-  connection.query("create node table person (ID INt64, fName StRING, gender INT64, isStudent "
-                   "BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, "
-                   "registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], "
-                   "usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));");
-  connection.query("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);");
-
-  auto result =
-      connection.query("MATCH (a:person { fName: \"Alice\" }) RETURN a.fName, a.age, a.eyeSight, a.isStudent;");
+  auto result = this->connection_->query(query);
 
   Napi::Object output = Napi::Object::New(env);
-  auto i = 0;
-  while (result->hasNext()) {
-    auto row = result->getNext();
-    Napi::Object obj = Napi::Object::New(env);
-    std::string fName = row->getResultValue(0)->getValue<std::string>();
-    obj.Set("fName", Napi::String::New(env, fName));
-    obj.Set("gender", Napi::Number::New(env, row->getResultValue(1)->getValue<int64_t>()));
-    obj.Set("eyeSight", Napi::Number::New(env, row->getResultValue(2)->getValue<double>()));
-    obj.Set("isStudent", row->getResultValue(3)->getValue<bool>());
-    output.Set(uint32_t(i), obj);
-    ++i;
+  if (query.starts_with("MATCH")) { // TODO: make this check more robust go through all return types for queries
+    auto i = 0;
+    while (result->hasNext()) {
+        auto row = result->getNext();
+        Napi::Object obj = Napi::Object::New(env);
+        std::string fName = row->getResultValue(0)->getValue<std::string>();
+        obj.Set("fName", Napi::String::New(env, fName));
+        obj.Set("gender", Napi::Number::New(env, row->getResultValue(1)->getValue<int64_t>()));
+        obj.Set("eyeSight", Napi::Number::New(env, row->getResultValue(2)->getValue<double>()));
+        obj.Set("isStudent", row->getResultValue(3)->getValue<bool>());
+        output.Set(uint32_t(i), obj);
+        ++i;
+    }
   }
   return output;
-
-}
-
-Napi::Value ClassExample::GetValue(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  Napi::HandleScope scope(env);
-
-  double num = this->actualClass_->getValue();
-  return Napi::Number::New(env, num);
-}
-
-Napi::Value ClassExample::Add(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  Napi::HandleScope scope(env);
-
-  if (info.Length() != 1 || !info[0].IsNumber()) {
-    Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
-  }
-
-  Napi::Number toAdd = info[0].As<Napi::Number>();
-  double answer = this->actualClass_->add(toAdd.DoubleValue());
-
-  return Napi::Number::New(info.Env(), answer);
-}
-
-ActualClass* ClassExample::GetInternalInstance() {
-  return this->actualClass_;
 }
--- tools/nodejs_api/test.js
+++ tools/nodejs_api/test.js
@@ -14,11 +14,12 @@ const kuzu = require("./build/Release/kuzujs.node");
 // const reuslt = new kuzu.test("Argument 1");
 // console.log(reuslt);
 
-console.log('addon',kuzu);
+console.log('addon', kuzu);
 
-const prevInstance = new kuzu.ClassExample(4.3);
-console.log('Initial value : ', prevInstance.getValue());
-console.log('After adding 3.3 : ', prevInstance.add(3.3));
-console.log('Calling Kuzu: ', prevInstance.temp("Alice"));
+const prevInstance = new kuzu.ClassExample();
+console.log('Kuzu Create: ', prevInstance.temp("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));"));
+console.log('Kuzu Copy: ', prevInstance.temp("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);"));
+console.log('Kuzu Match: ', prevInstance.temp("MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;"));
+
 
 module.exports = kuzu;
