816aecd4667e10050ed1905487ea8fd7b6798588
Yash Oza <yashoza@Yashs-Air.wlu.ca>
2023-01-24 20:37:53 -0500

    updated gitignore remove test directory binaries

--- tools/nodejs_api/sample.js
+++ tools/nodejs_api/sample.js
@@ -1,14 +1,106 @@
 // Make sure the test directory is removed as it will be recreated
 const fs = require("fs");
-const {Database, Connection} = require("./src_nodejs/index.js");
+const {Database, Connection} = require("./build/kuzu");
 
 try {
   fs.rmSync("./test", { recursive: true });
 } catch (e) {
   // ignore
 }
-const database = new Database("test", 1 << 30);
+
+async function executeAllCallback(err, queryResult) {
+  if (err) {
+    console.log(err);
+  } else {
+    await queryResult.all((err, result) => {
+      console.log("All result received Callback");
+      console.log(result);
+    });
+  }
+}
+
+async function executeAllPromise(err, queryResult) {
+  if (err) {
+    console.log(err);
+  } else {
+    await queryResult.all().then(result => {
+      console.log("All result received Promise");
+      console.log(result);
+    }).catch(error => {
+      console.log("All with Promise failed");
+      console.log(error);
+    });
+  }
+}
+
+// Basic Case with all callback
+const database = new Database("test", 1000000000);
+console.log("The database looks like: ", database);
 const connection = new Connection(database);
-console.log(connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));"));
-console.log(connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);"));
-console.log(connection.execute("MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;"));
+console.log ("The connection looks like: ", connection);
+connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));", executeAllCallback);
+connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);", executeAllPromise);
+const executeQuery = "MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;";
+const parameterizedExecuteQuery = "MATCH (a:person) WHERE a.age > $1 and a.isStudent = $2 and a.fName < $3  RETURN a.fName, a.age, a.eyeSight, a.isStudent;";
+
+connection.execute(executeQuery, executeAllPromise);
+connection.execute(parameterizedExecuteQuery, executeAllPromise, [["1", 29], ["2", true], ["3", "B"]]);
+
+// Extensive Case
+database.resizeBufferManager(2000000000);
+connection.setMaxNumThreadForExec(2);
+connection.execute(executeQuery, executeAllCallback);
+console.log(connection.getNodePropertyNames("person"));
+
+// Execute with each callback
+connection.execute(executeQuery,  async (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    await result.each(
+        (err, rowResult) => {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(rowResult);
+          }
+        },
+        () => {
+          console.log("all of the each's are done callback");
+        }
+    );
+  }
+});
+
+// Execute with promise + await
+connection.execute(executeQuery).then(async queryResult => {
+  await queryResult.all((err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("All result received for execution with a promise");
+      console.log(result);
+    }
+  });
+}).catch(error => {
+  console.log("Execution with a promise failed");
+  console.log(error);
+});
+
+async function asyncAwaitExecute(executeQuery) {
+  const queryResult = await connection.execute(executeQuery);
+  return queryResult;
+}
+asyncAwaitExecute(executeQuery).then(async queryResult => {
+  await queryResult.all((err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("All result received for execution with await");
+      console.log(result);
+    }
+  });
+}).catch(error => {
+  console.log("Execution with await failed");
+  console.log(error);
+});
--- tools/nodejs_api/src_cpp/node_connection.cpp
+++ tools/nodejs_api/src_cpp/node_connection.cpp
@@ -1,77 +1,105 @@
-#include "node_connection.h"
-#include "node_database.h"
+#include "include/node_connection.h"
 
+#include "include/execute_async_worker.h"
+#include "include/node_database.h"
+#include "include/node_query_result.h"
 #include "main/kuzu.h"
+#include "include/util.h"
 
 using namespace kuzu::main;
 
 Napi::FunctionReference NodeConnection::constructor;
 
 Napi::Object NodeConnection::Init(Napi::Env env, Napi::Object exports) {
-  Napi::HandleScope scope(env);
+    Napi::HandleScope scope(env);
 
-  Napi::Function t = DefineClass(env, "NodeConnection", {
-      InstanceMethod("execute", &NodeConnection::Execute),
-  });
+    Napi::Function t = DefineClass(env, "NodeConnection", {
+                                                              InstanceMethod("execute", &NodeConnection::Execute),
+                                                              InstanceMethod("setMaxNumThreadForExec", &NodeConnection::SetMaxNumThreadForExec),
+                                                              InstanceMethod("getNodePropertyNames", &NodeConnection::GetNodePropertyNames),
+                                                          });
 
-  constructor = Napi::Persistent(t);
-  constructor.SuppressDestruct();
-
-  exports.Set("NodeConnection", t);
-  return exports;
+    exports.Set("NodeConnection", t);
+    return exports;
 }
 
 
 NodeConnection::NodeConnection(const Napi::CallbackInfo& info) : Napi::ObjectWrap<NodeConnection>(info)  {
-  Napi::Env env = info.Env();
-  Napi::HandleScope scope(env);
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
 
-  if (info.Length()!=1 || !info[0].IsObject()) {
-      Napi::TypeError::New(env, "Need database class passed in").ThrowAsJavaScriptException();
-  }
+    if (info.Length()!=2 || !info[0].IsObject() || !info[1].IsNumber()) {
+        Napi::TypeError::New(env, "Need a valid database class passed in").ThrowAsJavaScriptException();
+        return;
+    }
+    NodeDatabase * nodeDatabase = Napi::ObjectWrap<NodeDatabase>::Unwrap(info[0].As<Napi::Object>());
+    uint64_t numThreads = info[1].As<Napi::Number>().DoubleValue();
 
-  NodeDatabase * nodeDatabase = NodeDatabase::Unwrap(info[0].As<Napi::Object>());
-
-  auto connection = new Connection(nodeDatabase->database_);
-  this->connection_ = connection;
+    try {
+        this->connection = make_unique<kuzu::main::Connection>(nodeDatabase->database.get());
+        if (numThreads > 0) {
+            this->connection->setMaxNumThreadForExec(numThreads);
+        }
+    } catch(const std::exception &exc){
+        Napi::TypeError::New(env, "Unsuccessful Connection Initialization: " + std::string(exc.what())).ThrowAsJavaScriptException();
+    }
 }
 
-NodeConnection::~NodeConnection() {
-  delete this->connection_;
-}
+NodeConnection::~NodeConnection() {}
 
 Napi::Value NodeConnection::Execute(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  Napi::HandleScope scope(env);
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
 
-  // add parsing for queries TODO: make this smart? like check whether a user created before matching???
-  std::string query = "";
-  if (info.Length()>0) {
-    query = info[0].ToString().Utf8Value().c_str();
-    if (!query.starts_with("MATCH") && !query.starts_with("create") && !query.starts_with("COPY")) {
-        Napi::TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    if (!info.Length()==4 || !info[0].IsString() || !info[1].IsFunction() || !info[2].IsObject() || !info[3].IsArray()) {
+        Napi::TypeError::New(env, "Execute needs query parameter").ThrowAsJavaScriptException();
         return Napi::Object::New(env);
     }
-  }
-
-  std::cout << "Arg: " << query << std::endl;
-
-  auto result = this->connection_->query(query);
-
-  Napi::Object output = Napi::Object::New(env);
-  if (query.starts_with("MATCH")) { // TODO: make this check more robust go through all return types for queries
-    auto i = 0;
-    while (result->hasNext()) {
-        auto row = result->getNext();
-        Napi::Object obj = Napi::Object::New(env);
-        std::string fName = row->getResultValue(0)->getValue<std::string>();
-        obj.Set("fName", Napi::String::New(env, fName));
-        obj.Set("gender", Napi::Number::New(env, row->getResultValue(1)->getValue<int64_t>()));
-        obj.Set("eyeSight", Napi::Number::New(env, row->getResultValue(2)->getValue<double>()));
-        obj.Set("isStudent", row->getResultValue(3)->getValue<bool>());
-        output.Set(uint32_t(i), obj);
-        ++i;
+    std::string query = info[0].ToString();
+    Function callback = info[1].As<Function>();
+    NodeQueryResult * nodeQueryResult = Napi::ObjectWrap<NodeQueryResult>::Unwrap(info[2].As<Napi::Object>());
+    auto params = Util::transformParameters(info[3].As<Napi::Array>());
+    try {
+        ExecuteAsyncWorker* asyncWorker = new ExecuteAsyncWorker(callback, connection, query, nodeQueryResult, params);
+        asyncWorker->Queue();
+    } catch(const std::exception &exc) {
+        Napi::TypeError::New(env, "Unsuccessful execute: " + std::string(exc.what())).ThrowAsJavaScriptException();
     }
-  }
-  return output;
+    return info.Env().Undefined();
+}
+
+void NodeConnection::SetMaxNumThreadForExec(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
+
+    if (info.Length()!=1 || !info[0].IsNumber()) {
+        Napi::TypeError::New(env, "Need Integer Number of Threads as an argument").ThrowAsJavaScriptException();
+        return;
+    }
+    uint64_t numThreads = info[0].ToNumber().DoubleValue();
+    try {
+        this->connection->setMaxNumThreadForExec(numThreads);
+    } catch(const std::exception &exc) {
+        Napi::TypeError::New(env, "Unsuccessful setMaxNumThreadForExec: " + std::string(exc.what())).ThrowAsJavaScriptException();
+    }
+    return;
+}
+
+Napi::Value NodeConnection::GetNodePropertyNames(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
+
+    if (info.Length()!=1 || !info[0].IsString()) {
+        Napi::TypeError::New(env, "Need Table Name as an argument").ThrowAsJavaScriptException();
+        return Napi::Object::New(env);
+    }
+    std::string tableName = info[0].ToString();
+    std::string propertyNames;
+    try {
+        propertyNames = this->connection->getNodePropertyNames(tableName);
+    } catch(const std::exception &exc) {
+        Napi::TypeError::New(env, "Unsuccessful getNodePropertyNames: " + std::string(exc.what())).ThrowAsJavaScriptException();
+        return Napi::Object::New(env);
+    }
+    return Napi::String::New(env, propertyNames);;
 }
