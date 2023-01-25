8ff4d64d507e19da7d07a192fe1b3f9febd1fc80
Yash Oza <ynoza@uwaterloo.ca>
2023-01-25 01:28:10 -0500

    rename names, directories to be similar to python binder

--- tools/nodejs_api/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <napi.h>
-#include "Samples/classexample.h"
-
-Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return ClassExample::Init(env, exports);
-}
-
-NODE_API_MODULE(NODE_GYP_MODULE_NAME, InitAll)
