a8ca571f0a647cf6f51697fdf273eb40ad3e38bb
Yash Oza <ynoza@uwaterloo.ca>
2023-01-25 01:28:10 -0500

    rename names, directories to be similar to python binder

--- tools/nodejs_api/src_nodejs/sample.js
+++ tools/nodejs_api/src_nodejs/sample.js
@@ -8,7 +8,7 @@ try {
 
 
 // Import the module
-const kuzu = require("./build/Release/kuzujs.node");
+const kuzu = require("../build/Release/kuzujs.node");
 
 
 // const reuslt = new kuzu.test("Argument 1");
@@ -16,7 +16,7 @@ const kuzu = require("./build/Release/kuzujs.node");
 
 console.log('addon', kuzu);
 
-const prevInstance = new kuzu.ClassExample();
+const prevInstance = new kuzu.NjsDatabase();
 console.log('Kuzu Create: ', prevInstance.temp("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));"));
 console.log('Kuzu Copy: ', prevInstance.temp("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);"));
 console.log('Kuzu Match: ', prevInstance.temp("MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;"));
