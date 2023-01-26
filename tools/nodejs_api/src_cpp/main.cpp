51851b4a596c9139148891283039a08b6d1b3f8b
Yash Oza <ynoza@uwaterloo.ca>
2023-01-26 13:21:05 -0500

    change prefix, etc.

--- tools/nodejs_api/src_cpp/main.cpp
+++ tools/nodejs_api/src_cpp/main.cpp
@@ -1,8 +1,10 @@
+#include "node_connection.h"
+#include "node_database.h"
 #include <napi.h>
-#include "njsdatabase.h"
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return NjsDatabase::Init(env, exports);
+    NodeDatabase::Init(env, exports);
+    return NodeConnection::Init(env, exports);
 }
 
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, InitAll)
