2fcd5d598514e7cce2cd900c1e920ca2a1f2a314
Yash Oza <ynoza@uwaterloo.ca>
2023-01-30 13:11:00 -0500

    index imports, etc

--- tools/nodejs_api/src_nodejs/index.js
+++ tools/nodejs_api/src_nodejs/index.js
@@ -1,4 +1,4 @@
-const QueryResult = require("./queryResult.js");
 const Database = require("./database.js");
 const Connection = require("./connection.js");
-module.exports = {Database, Connection, QueryResult}
+
+module.exports = {Database, Connection}
