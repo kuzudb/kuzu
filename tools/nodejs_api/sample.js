2b5ca7c4fa9dc607b4d1ce18a008b79e14a6acc5
Yash Oza <ynoza@uwaterloo.ca>
2023-01-30 13:11:00 -0500

    index imports, etc

--- tools/nodejs_api/sample.js
+++ tools/nodejs_api/sample.js
@@ -1,12 +1,12 @@
 // Make sure the test directory is removed as it will be recreated
 const fs = require("fs");
+const {Database, Connection} = require("./src_nodejs/index.js");
+
 try {
   fs.rmSync("./test", { recursive: true });
 } catch (e) {
   // ignore
 }
-const Database = require("./src_nodejs/database.js");
-const Connection = require("./src_nodejs/connection.js");
 const database = new Database("test", 1 << 30);
 const connection = new Connection(database);
 console.log(connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));"));
