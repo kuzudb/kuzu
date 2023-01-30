067373052d0fd4bc97f174b67208431f137fb15e
Yash Oza <ynoza@uwaterloo.ca>
2023-01-30 12:20:46 -0500

    added Database Class, all its methods, database java class, and added it to connection

--- tools/nodejs_api/sample.js
+++ tools/nodejs_api/sample.js
@@ -5,9 +5,10 @@ try {
 } catch (e) {
   // ignore
 }
-
+const Database = require("./src_nodejs/database.js");
 const Connection = require("./src_nodejs/connection.js");
-const connection = new Connection("test");
+const database = new Database("test", 1 << 30);
+const connection = new Connection(database);
 console.log(connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));"));
 console.log(connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);"));
 console.log(connection.execute("MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;"));
