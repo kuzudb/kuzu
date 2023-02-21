ff6f6558122082d9de7ffb0e438d761d0219c07e
ynoza <ynoza@uwaterloo.ca>
2023-02-21 17:35:13 -0500

    all callback

--- tools/nodejs_api/sample.js
+++ tools/nodejs_api/sample.js
@@ -10,10 +10,19 @@ try {
 
 function executeAllCallback(queryResult) {
     queryResult.all( result => {
-        console.log("All result received");
+        console.log("All result received Callback");
         console.log(result);
     });
-    queryResult.close();
+}
+
+function executeAllPromise(queryResult) {
+    queryResult.all().then( result => {
+        console.log("All result received Promise");
+        console.log(result);
+    }).catch( error => {
+        console.log("All with Promise failed");
+        console.log(error);
+    });
 }
 
 // Basic Case with all callback
@@ -22,9 +31,10 @@ console.log("The database looks like: ", database);
 const connection = new Connection(database);
 console.log ("The connection looks like: ", connection);
 connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));", executeAllCallback);
-connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);");
+connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);", executeAllPromise);
 const executeQuery = "MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;";
 connection.execute(executeQuery, executeAllCallback);
+connection.execute(executeQuery, executeAllPromise);
 
 // Extensive Case
 database.resizeBufferManager(2000000000);
@@ -33,10 +43,11 @@ connection.execute(executeQuery, executeAllCallback);
 console.log(connection.getNodePropertyNames("person"));
 
 // Execute with each callback
-connection.execute(executeQuery, result => {
+connection.execute(executeQuery,  result => {
     result.each(
         row => { console.log(row); },
-        () => { console.log("all done"); })
+        () => { console.log("all of the each's are done callback"); }
+    );
 });
 
 // Execute with promise + await
