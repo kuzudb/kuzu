0111ba43d27a3081dfe3e6077917ed77ea946f5f
ynoza <ynoza@uwaterloo.ca>
2023-02-20 20:24:37 -0500

    type dynamic conversion

--- tools/nodejs_api/src_cpp/all_each_async_worker.cpp
+++ tools/nodejs_api/src_cpp/all_each_async_worker.cpp
@@ -16,9 +16,9 @@ void AllEachAsyncWorker::Execute() {
     unsigned int i = 0;
     while(this->queryResult->hasNext()) {
         auto row = this->queryResult->getNext();
-        allResult.emplace_back(vector<string>(row->len()));
-        for (int j = 0; j < row->len(); j++) {
-            allResult[i][j] = row->getValue(j)->toString();
+        allResult.emplace_back(vector<unique_ptr<kuzu::common::Value>>(row->len()));
+        for (unsigned int j = 0; j < row->len(); j++) {
+            allResult[i][j] = make_unique<kuzu::common::Value>(kuzu::common::Value(*row->getValue(j)));
         }
         i++;
     }
@@ -30,7 +30,7 @@ void AllEachAsyncWorker::OnOK() {
         Napi::Array rowArray = Napi::Array::New(Env(), allResult[i].size());
         unsigned int j = 0;
         for (; j < allResult[i].size(); j++){
-            rowArray.Set(j, allResult[i][j]);
+            rowArray.Set(j, ConvertToNapiObject(*allResult[i][j], Env()));
         }
         if (executionType == "each") {
             rowArray.Set(j, allResult.size()-i-1);
@@ -43,3 +43,35 @@ void AllEachAsyncWorker::OnOK() {
         Callback().Call({Env().Null(), arr});
     }
 };
+
+
+Napi::Value AllEachAsyncWorker::ConvertToNapiObject(const kuzu::common::Value& value, Napi::Env env) {
+    if (value.isNull()) {
+        return Napi::Value();
+    }
+    auto dataType = value.getDataType();
+    switch (dataType.typeID) {
+        case kuzu::common::BOOL: {
+            return Napi::Boolean::New(env, value.getValue<bool>());
+        }
+        case kuzu::common::INT64: {
+            return Napi::Number::New(env, value.getValue<int64_t>());
+        }
+        case kuzu::common::DOUBLE: {
+            return Napi::Number::New(env, value.getValue<double>());
+        }
+        case kuzu::common::STRING: {
+            return Napi::String::New(env, value.getValue<string>());
+        }
+        case kuzu::common::LIST: {
+            auto& listVal = value.getListValReference();
+            Napi::Array arr = Napi::Array::New(env, listVal.size());
+            for (auto i = 0u; i < listVal.size(); ++i) {
+                arr.Set(i, ConvertToNapiObject(*listVal[i], env));
+            }
+            return arr;
+        }
+        default:
+            throw NotImplementedException("Unsupported type: " + kuzu::common::Types::dataTypeToString(dataType));
+    }
+}
