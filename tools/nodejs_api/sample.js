bc56159d83e55210820d93e2c313a2663c44073e
ynoza <ynoza@uwaterloo.ca>
2023-03-11 01:35:52 -0500

    connection error checking moved to js

--- tools/nodejs_api/sample.js
+++ tools/nodejs_api/sample.js
@@ -38,22 +38,22 @@ const database = new Database("test", 1000000000);
 console.log("The database looks like: ", database);
 const connection = new Connection(database);
 console.log ("The connection looks like: ", connection);
-connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));", executeAllCallback);
-connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);", executeAllPromise);
+connection.execute("create node table person (ID INt64, fName StRING, gender INT64, isStudent BoOLEAN, isWorker BOOLEAN, age INT64, eyeSight DOUBLE, birthdate DATE, registerTime TIMESTAMP, lastJobDuration interval, workedHours INT64[], usedNames STRING[], courseScoresPerTerm INT64[][], PRIMARY KEY (ID));", {"callback": executeAllCallback});
+connection.execute("COPY person FROM \"../../dataset/tinysnb/vPerson.csv\" (HEADER=true);", {"callback": executeAllPromise});
 const executeQuery = "MATCH (a:person) RETURN a.fName, a.age, a.eyeSight, a.isStudent;";
 const parameterizedExecuteQuery = "MATCH (a:person) WHERE a.age > $1 and a.isStudent = $2 and a.fName < $3  RETURN a.fName, a.age, a.eyeSight, a.isStudent;";
 
-connection.execute(executeQuery, executeAllPromise);
-// connection.execute(parameterizedExecuteQuery, executeAllPromise, [["1", 29], ["2", true], ["3", "B"]]);
+connection.execute(executeQuery, {"callback": executeAllPromise});
+// connection.execute(parameterizedExecuteQuery, {"callback": executeAllPromise, "params":[["1", 29], ["2", true], ["3", "B"]]});
 
 // Extensive Case
 database.resizeBufferManager(2000000000);
 connection.setMaxNumThreadForExec(2);
-connection.execute(executeQuery, executeAllCallback);
+connection.execute(executeQuery, {"callback": executeAllCallback});
 console.log(connection.getNodePropertyNames("person"));
 
 // Execute with each callback
-connection.execute(executeQuery,  async (err, result) => {
+connection.execute(executeQuery, {"callback": async (err, result) => {
     if (err) {
         console.log(err);
     } else {
@@ -70,7 +70,7 @@ connection.execute(executeQuery,  async (err, result) => {
             }
         );
     }
-});
+}});
 
 // Execute with promise + await
 connection.execute(executeQuery).then(async queryResult => {
--- tools/nodejs_api/src_nodejs/connection.js
+++ tools/nodejs_api/src_nodejs/connection.js
@@ -4,12 +4,31 @@ const QueryResult = require("./queryResult.js");
 class Connection {
     #connection;
     constructor(database, numThreads = 0) {
+        if (typeof database !== "object" || database.constructor.name !==  "Database" || typeof numThreads !== "number" || !Number.isInteger(numThreads)){
+            throw new Error("connection constructor requires a database object and optional numThreads integer as argument(s)");
+        }
         this.#connection = new kuzu.NodeConnection(database.database, numThreads);
     }
 
-    execute(query, callback=null, params=[]){
+    execute(query, opts = {}) {
+        if (typeof opts !== "object") {
+            throw new Error("optional opts in execute must be an object");
+        }
+
+        let params = [];
+        if ('params' in opts) {
+            params = opts['params'];
+        }
+        if (typeof query !== "string" || !Array.isArray(params)) {
+            throw new Error("execute takes a string query and optional parameter array");
+        }
+
         const nodeQueryResult = new kuzu.NodeQueryResult();
-        if (callback) {
+        if ('callback' in opts) {
+            const callback = opts['callback'];
+            if (typeof callback !== "function" || callback.length != 2){
+                throw new Error("if execute is given a callback that takes 2 arguments");
+            }
             this.#connection.execute(query, err => {
                 const queryResult = new QueryResult(nodeQueryResult);
                 callback(err, queryResult);
@@ -25,10 +44,16 @@ class Connection {
     }
 
     setMaxNumThreadForExec(numThreads) {
+        if (typeof numThreads !== "number" || !Number.isInteger(numThreads)){
+            throw new Error("setMaxNumThreadForExec needs an integer numThreads as an argument");
+        }
         this.#connection.setMaxNumThreadForExec(numThreads);
     }
 
     getNodePropertyNames(tableName) {
+        if (typeof tableName !== "string"){
+            throw new Error("getNodePropertyNames needs a string tableName as an argument");
+        }
         return this.#connection.getNodePropertyNames(tableName);
     }
 
