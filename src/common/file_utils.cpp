2d69ed6579464382a68333280c17a9160357b1ab
Benjamin Winger <bmw@disroot.org>
2023-05-10 11:00:20 -0400

    Implement simple glob without globbing on windows pending cross-platform solution

--- src/common/file_utils.cpp
+++ src/common/file_utils.cpp
@@ -3,6 +3,9 @@
 #include "common/exception.h"
 #include "common/string_utils.h"
 #include "common/utils.h"
+#ifndef _WIN32
+#include <glob.h>
+#endif
 
 namespace kuzu {
 namespace common {
@@ -141,12 +144,19 @@ void FileUtils::removeFileIfExists(const std::string& path) {
 
 std::vector<std::string> FileUtils::globFilePath(const std::string& path) {
     std::vector<std::string> result;
+    // TODO: Windows globbing
+#ifdef _WIN32
+    if (std::filesystem::exists(std::filesystem::path(path))) {
+        result.push_back(path);
+    }
+#else
     glob_t globResult;
     glob(path.c_str(), GLOB_TILDE, nullptr, &globResult);
     for (auto i = 0u; i < globResult.gl_pathc; ++i) {
         result.emplace_back(globResult.gl_pathv[i]);
     }
     globfree(&globResult);
+#endif
     return result;
 }
 
