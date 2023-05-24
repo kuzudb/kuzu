73317cab4874232850fb2634428f84d4eb76faea
aziz-mu <aziz20019@gmail.com>
2023-05-24 09:32:26 -0400

    add LSQB queries as test

---
+++ test/lsqb/lsqb.cpp
@@ -0,0 +1,21 @@
+#include "graph_test/graph_test.h"
+
+using ::testing::Test;
+using namespace kuzu::testing;
+
+class LSQBTest : public DBTest {
+public:
+    void SetUp() override {
+        BaseGraphTest::SetUp();
+        systemConfig->bufferPoolSize =
+            kuzu::common::BufferPoolConstants::DEFAULT_BUFFER_POOL_SIZE_FOR_TESTING * 8;
+        createDBAndConn();
+        initGraph();
+    }
+
+    std::string getInputDir() override { return TestHelper::appendKuzuRootPath("dataset/sf-0.1/"); }
+};
+
+TEST_F(LSQBTest, LSQBTest) {
+    runTest(TestHelper::appendKuzuRootPath("test/test_files/lsqb/lsqb_queries.test"));
+}
