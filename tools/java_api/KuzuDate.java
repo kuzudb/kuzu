1f580653583c9d57f5cc5f1a89e373868a103032
Ruiling Ma <maruiling667216@gmail.com>
2023-06-16 23:47:30 -0400

    Removing extra class

--- tools/java_api/KuzuDate.java
+++
@@ -1,6 +0,0 @@
-package tools.java_api;
-
-public class KuzuDate {
-    // Days since 1970-01-01 00:00:00 UTC.
-    public int days;
-}
--- tools/java_api/KuzuTimestamp.java
+++
@@ -1,6 +0,0 @@
-package tools.java_api;
-
-public class KuzuTimestamp {
-    // Microseconds since 1970-01-01 00:00:00 UTC.
-    public long value;
-}
