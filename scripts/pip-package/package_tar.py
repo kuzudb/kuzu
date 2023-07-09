b997966a42f3dad7f091e2fe3e95f3d88ace18cd
Chang Liu <liuc223@gmail.com>
2023-07-09 03:00:48 -0400

    Create source dist with version number

--- scripts/pip-package/package_tar.py
+++ scripts/pip-package/package_tar.py
@@ -7,7 +7,22 @@ import sys
 import tarfile
 from tempfile import TemporaryDirectory
 
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def _get_kuzu_version():
+    cmake_file = os.path.abspath(os.path.join(base_dir, '..', 'CMakeLists.txt'))
+    with open(cmake_file) as f:
+        for line in f:
+            if line.startswith('project(Kuzu VERSION'):
+                return line.split(' ')[2].strip()
+
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        file_name = sys.argv[1]
+    else:
+        file_name = "kuzu-%s-sdist.tar.gz" % _get_kuzu_version()
+    print("Creating %s..." % file_name)
+
     with TemporaryDirectory() as tempdir:
         subprocess.check_call(
             [
@@ -24,6 +39,8 @@ if __name__ == "__main__":
 
         with tarfile.open(os.path.join(tempdir, "kuzu-source.tar")) as tar:
             tar.extractall(path=os.path.join(tempdir, "kuzu-source"))
+        
+        os.remove(os.path.join(tempdir, "kuzu-source.tar"))
 
         os.makedirs(os.path.join(tempdir, "kuzu"))
         for path in ["setup.py", "setup.cfg", "MANIFEST.in"]:
@@ -34,5 +51,5 @@ if __name__ == "__main__":
 
         subprocess.check_call([sys.executable, "setup.py", "egg_info"], cwd=tempdir)
 
-        with tarfile.open("kuzu.tar.gz", "w:gz") as sdist:
-            sdist.add(tempdir, "kuzu")
+        with tarfile.open(file_name, "w:gz") as sdist:
+            sdist.add(tempdir, "sdist")
