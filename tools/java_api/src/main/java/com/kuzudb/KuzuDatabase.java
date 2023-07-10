e03053cdea2e535794d1ddbedf1a45d7e1d2fbb4
Guodong Jin <guod.jin@gmail.com>
2023-07-10 13:52:51 +0800

    Add Javadoc for Java apis (#1785)
    * draft javadoc
    
    * clean
    
    * remove iml
    
    * revert remove of isOwnedByCPP()
    
    * revert remove of kuzu data type constructor for nested data type
    
    * Add constructor of db without buffer size
    
    * Add package info
    
    * Fix typos in doc
    
    * Add missing \n
    
    * fix errors for javadoc
    
    ---------
    
    Co-authored-by: Chang Liu <liuc223@gmail.com>

--- tools/java_api/src/main/java/com/kuzudb/KuzuDatabase.java
+++ tools/java_api/src/main/java/com/kuzudb/KuzuDatabase.java
@@ -1,5 +1,8 @@
 package com.kuzudb;
 
+/**
+* The KuzuDatabase class is the main class of KuzuDB. It manages all database components.
+*/
 public class KuzuDatabase {
 
     long db_ref;
@@ -7,26 +10,57 @@ public class KuzuDatabase {
     long buffer_size;
     boolean destroyed = false;
 
-    public KuzuDatabase(String database_path, long buffer_pool_size) {
-        this.db_path = database_path;
-        this.buffer_size = buffer_pool_size;
-        db_ref = KuzuNative.kuzu_database_init(database_path, buffer_pool_size);
+    /**
+     * Creates a database object.
+     * @param databasePath: Database path. If the database does not already exist, it will be created.
+     */
+    public KuzuDatabase(String databasePath) {
+        this.db_path = databasePath;
+        this.buffer_size = 0;
+        db_ref = KuzuNative.kuzu_database_init(databasePath, 0);
     }
 
-    public static void setLoggingLevel(String logging_level) {
-        KuzuNative.kuzu_database_set_logging_level(logging_level);
+    /**
+    * Creates a database object.
+    * @param databasePath: Database path. If the database does not already exist, it will be created.
+    * @param bufferPoolSize: Max size of the buffer pool in bytes.
+    */
+    public KuzuDatabase(String databasePath, long bufferPoolSize) {
+        this.db_path = databasePath;
+        this.buffer_size = bufferPoolSize;
+        db_ref = KuzuNative.kuzu_database_init(databasePath, bufferPoolSize);
     }
 
+    /**
+    * Sets the logging level of the database instance.
+    * @param loggingLevel: New logging level. (Supported logging levels are: 'info', 'debug', 'err').
+    */
+    public static void setLoggingLevel(String loggingLevel) {
+        KuzuNative.kuzu_database_set_logging_level(loggingLevel);
+    }
+
+    /**
+    * Checks if the database instance has been destroyed.
+    * @throws KuzuObjectRefDestroyedException If the database instance is destroyed.
+    */
     private void checkNotDestroyed() throws KuzuObjectRefDestroyedException {
         if (destroyed)
             throw new KuzuObjectRefDestroyedException("KuzuDatabase has been destroyed.");
     }
 
+    /**
+    * Finalize.
+    * @throws KuzuObjectRefDestroyedException If the database instance has been destroyed.
+    */
     @Override
     protected void finalize() throws KuzuObjectRefDestroyedException {
         destroy();
     }
 
+    /**
+    * Destroy the database instance.
+    * @throws KuzuObjectRefDestroyedException If the database instance has been destroyed.
+    */
     public void destroy() throws KuzuObjectRefDestroyedException {
         checkNotDestroyed();
         KuzuNative.kuzu_database_destroy(this);
