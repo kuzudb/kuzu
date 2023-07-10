89d293b0707a596e20ab478221b338e17953039d
Chang Liu <liuc223@gmail.com>
2023-07-10 01:23:30 -0400

    Add package info

---
+++ tools/java_api/src/main/java/com/kuzudb/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Kùzu Java API.
+ * 
+ * <p>
+ * Kùzu is an in-process property graph database management system built for 
+ * query speed and scalability.
+ * </p>
+ */
+
+package com.kuzudb;
