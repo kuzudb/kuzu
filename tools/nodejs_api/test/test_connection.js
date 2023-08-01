081606743f4f967a2c85390a88f86e5313aa87a0
Guodong Jin <guod.jin@gmail.com>
2023-08-01 15:52:27 +0800

    fix nodejs test

--- tools/nodejs_api/test/test_connection.js
+++ tools/nodejs_api/test/test_connection.js
@@ -208,10 +208,9 @@ describe("Get node table names", function () {
     assert.exists(nodeTableNames);
     assert.isArray(nodeTableNames);
     nodeTableNames.sort();
-    assert.equal(nodeTableNames.length, 4);
+    assert.equal(nodeTableNames.length, 3);
     assert.deepEqual(nodeTableNames, [
       "movies",
-//       "moviesSerial",
       "organisation",
       "person",
     ]);
