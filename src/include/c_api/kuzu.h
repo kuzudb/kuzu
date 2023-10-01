e87c39b789309f0a402fcae52d4f1a0961d2f8fe
Benjamin Winger <bmw@disroot.org>
2023-10-01 15:07:26 -0400

    Inline export headers

--- src/include/c_api/kuzu.h
+++ src/include/c_api/kuzu.h
@@ -4,7 +4,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "common/api.h"
+/* Export header from common/api.h */
+// Helpers
+#if defined _WIN32 || defined __CYGWIN__
+#define KUZU_HELPER_DLL_IMPORT __declspec(dllimport)
+#define KUZU_HELPER_DLL_EXPORT __declspec(dllexport)
+#define KUZU_HELPER_DLL_LOCAL
+#define KUZU_HELPER_DEPRECATED __declspec(deprecated)
+#else
+#define KUZU_HELPER_DLL_IMPORT __attribute__((visibility("default")))
+#define KUZU_HELPER_DLL_EXPORT __attribute__((visibility("default")))
+#define KUZU_HELPER_DLL_LOCAL __attribute__((visibility("hidden")))
+#define KUZU_HELPER_DEPRECATED __attribute__((__deprecated__))
+#endif
+
+#ifdef KUZU_STATIC_DEFINE
+#define KUZU_API
+#define KUZU_NO_EXPORT
+#else
+#ifndef KUZU_API
+#ifdef kuzu_shared_EXPORTS
+/* We are building this library */
+#define KUZU_API KUZU_HELPER_DLL_EXPORT
+#else
+/* We are using this library */
+#define KUZU_API KUZU_HELPER_DLL_IMPORT
+#endif
+#endif
+
+#endif
+
+#ifndef KUZU_DEPRECATED
+#define KUZU_DEPRECATED KUZU_HELPER_DEPRECATED
+#endif
+
+#ifndef KUZU_DEPRECATED_EXPORT
+#define KUZU_DEPRECATED_EXPORT KUZU_API KUZU_DEPRECATED
+#endif
+/* end export header */
 
 // The Arrow C data interface.
 // https://arrow.apache.org/docs/format/CDataInterface.html
