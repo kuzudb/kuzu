8dd42888306240f7ca1c8d753616a8fc7a1a1fd8
Chang Liu <liuc223@gmail.com>
2023-10-31 16:04:58 +0800

    Bump version number to 0.0.12

--- src/include/storage/storage_info.h
+++ src/include/storage/storage_info.h
@@ -12,8 +12,8 @@ using storage_version_t = uint64_t;
 
 struct StorageVersionInfo {
     static std::unordered_map<std::string, storage_version_t> getStorageVersionInfo() {
-        return {{"0.0.11", 23}, {"0.0.10", 23}, {"0.0.9", 23}, {"0.0.8", 17}, {"0.0.7", 15},
-            {"0.0.6", 9}, {"0.0.5", 8}, {"0.0.4", 7}, {"0.0.3", 1}};
+        return {{"0.0.12", 23}, {"0.0.11", 23}, {"0.0.10", 23}, {"0.0.9", 23}, {"0.0.8", 17},
+            {"0.0.7", 15}, {"0.0.6", 9}, {"0.0.5", 8}, {"0.0.4", 7}, {"0.0.3", 1}};
     }
 
     static storage_version_t getStorageVersion();
