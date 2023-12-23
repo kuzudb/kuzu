99dbf7a6e5f8eba10ed35cb3046a377c4eb4b579
Keenan Gugeler <kuzu@kgugeler.ca>
2023-12-22 22:05:57 -0500

    common: add MPSC Queue
    This queue is core to the new parallel hash index. It implements the
    design in this [blog][1], which is also used by Rust. The design is quite
    simple, and producers are wait-free. The main disadvantage is it is
    non-blocking, but adding blocking pop() support is tricky, and currently
    not needed.
    
      [1]: https://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue

--- src/include/common/copy_constructors.h
+++ src/include/common/copy_constructors.h
@@ -2,29 +2,37 @@
 
 // This file defines many macros for controlling copy constructors and move constructors on classes.
 
-#define DELETE_COPY(Object) Object(const Object& other) = delete
-
+#define DELETE_COPY_CONSTRUCT(Object) Object(const Object& other) = delete
 #define DELETE_COPY_ASSN(Object) Object& operator=(const Object& other) = delete
+#define DELETE_MOVE_CONSTRUCT(Object) Object(Object&& other) = delete
+#define DELETE_MOVE_ASSN(Object) Object& operator=(Object&& other) = delete
 
-#define DELETE_MOVE(Object)                                                                        \
-    Object(Object&& other) = delete;                                                               \
-    Object& operator=(Object&& other) = delete
+#define DELETE_BOTH_COPY(Object)                                                                   \
+    DELETE_COPY_CONSTRUCT(Object);                                                                 \
+    DELETE_COPY_ASSN(Object)
 
-#define ENABLE_MOVE(Object)                                                                        \
-    Object(Object&& other) = default;                                                              \
-    Object& operator=(Object&& other) = default
+#define DELETE_BOTH_MOVE(Object)                                                                   \
+    DELETE_MOVE_CONSTRUCT(Object);                                                                 \
+    DELETE_MOVE_ASSN(Object)
+
+#define DEFAULT_MOVE_CONSTRUCT(Object) Object(Object&& other) = default
+#define DEFAULT_MOVE_ASSN(Object) Object& operator=(Object&& other) = default
+
+#define DEFAULT_BOTH_MOVE(Object)                                                                  \
+    DEFAULT_MOVE_CONSTRUCT(Object);                                                                \
+    DEFAULT_MOVE_ASSN(Object)
 
 #define EXPLICIT_COPY_METHOD(Object)                                                               \
     Object copy() const { return *this; }
 
-// EXPLICIT_COPY should be the default choice. It expects a PRIVATE copy constructor to be defined,
-// which will be used by an explicit `copy()` method. For instance:
+// EXPLICIT_COPY_DEFAULT_MOVE should be the default choice. It expects a PRIVATE copy constructor to
+// be defined, which will be used by an explicit `copy()` method. For instance:
 //
 //   private:
 //     MyClass(const MyClass& other) : field(other.field.copy()) {}
 //
 //   public:
-//     EXPLICIT_COPY(MyClass);
+//     EXPLICIT_COPY_DEFAULT_MOVE(MyClass);
 //
 // Now:
 //
@@ -32,20 +40,19 @@
 // MyClass o2 = o1; // Compile error, copy assignment deleted.
 // MyClass o2 = o1.copy(); // OK.
 // MyClass o2(o1); // Compile error, copy constructor is private.
-#define EXPLICIT_COPY(Object)                                                                      \
+#define EXPLICIT_COPY_DEFAULT_MOVE(Object)                                                         \
     DELETE_COPY_ASSN(Object);                                                                      \
-    ENABLE_MOVE(Object);                                                                           \
+    DEFAULT_BOTH_MOVE(Object);                                                                     \
     EXPLICIT_COPY_METHOD(Object)
 
-// NO_COPY should be used for objects that for whatever reason, should never be copied.
-#define NO_COPY(Object)                                                                            \
-    DELETE_COPY(Object);                                                                           \
-    DELETE_COPY_ASSN(Object);                                                                      \
-    ENABLE_MOVE(Object)
+// NO_COPY should be used for objects that for whatever reason, should never be copied, but can be
+// moved.
+#define DELETE_COPY_DEFAULT_MOVE(Object)                                                           \
+    DELETE_BOTH_COPY(Object);                                                                      \
+    DEFAULT_BOTH_MOVE(Object)
 
 // NO_MOVE_OR_COPY exists solely for explicitness, when an object cannot be moved nor copied. Any
 // object containing a lock cannot be moved or copied.
-#define NO_MOVE_OR_COPY(Object)                                                                    \
-    DELETE_COPY(Object);                                                                           \
-    DELETE_COPY_ASSN(Object);                                                                      \
-    DELETE_MOVE(Object)
+#define DELETE_COPY_AND_MOVE(Object)                                                               \
+    DELETE_BOTH_COPY(Object);                                                                      \
+    DELETE_BOTH_MOVE(Object)
