b2f2cfa1a485776718c0b1d82f21cad4b62ddc24
Chang Liu <liuc223@gmail.com>
2024-02-16 13:12:11 +0800

    Fix Node.js extension loading on Linux

--- tools/nodejs_api/test/test_extension.js
+++ tools/nodejs_api/test/test_extension.js
@@ -2,8 +2,8 @@ const { assert } = require("chai");
 
 describe("Extension loading", () => {
   it("should install and load httpfs extension", async function () {
+    this.timeout(10000);
     await conn.query("INSTALL httpfs");
-    // Skip this test until the fix is in place
-    // await conn.query("LOAD EXTENSION httpfs");
+    await conn.query("LOAD EXTENSION httpfs");
   });
 });
