45c5aa96fe36cd08ba5c290a4ae335b3f73e7aa9
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2024-03-09 11:26:46 +0400

    Support use of QueryResult as a context manager (#3009)

--- tools/python_api/test/test_query_result.py
+++ tools/python_api/test/test_query_result.py
@@ -24,3 +24,13 @@ def test_explain(establish_connection):
     result = conn.execute("EXPLAIN MATCH (a:person) WHERE a.ID = 0 RETURN a")
     assert result.get_num_tuples() == 1
     result.close()
+
+def test_context_manager(establish_connection):
+    conn, db = establish_connection
+    with conn.execute("MATCH (a:person) WHERE a.ID = 0 RETURN a") as result:
+        assert result.get_num_tuples() == 1
+        assert result.get_compiling_time() > 0
+
+    # context exit guarantees immediately 'close' of the underlying QueryResult
+    # (don't have to wait for __del__, which may not ever actually get called)
+    assert result.is_closed
