ebaefe0bc8d9451fbe79bc85aa025f816d62f157
Chang Liu <liuc223@gmail.com>
2024-03-11 20:03:12 +0800

    X

--- scripts/multiplatform-test-helper/collect-results.py
+++ scripts/multiplatform-test-helper/collect-results.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+import pandas as pd
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: collect-results.py <results-dir>")
+        sys.exit(1)
+
+    results_dir = sys.argv[1]
+    results = {}
+
+    for root, dirs, files in os.walk(results_dir):
+        for file in files:
+            if file.endswith(".csv"):
+                results[file] = pd.read_csv(os.path.join(root, file))
