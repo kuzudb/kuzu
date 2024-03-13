7110f91a7d3fa7d685318ff86cfd200c1bb52c75
Alexander Beedie <alexander-beedie@users.noreply.github.com>
2024-03-13 22:14:33 +0400

    First-pass lint/format for Python `shell` tests (#3034)

--- tools/shell/test/test_shell_esc_search.py
+++ tools/shell/test/test_shell_esc_search.py
@@ -1,10 +1,10 @@
-import pytest
 import pexpect
-from test_helper import *
+import pytest
 from conftest import ShellTest
+from test_helper import KEY_ACTION
 
 
-def set_up_search(test):
+def set_up_search(test) -> None:
     test.send_finished_statement('RETURN "databases rule" AS a;\r')
     assert test.shell_process.expect_exact(["| databases rule |", pexpect.EOF]) == 0
     test.send_finished_statement('RETURN "kuzu is cool" AS b;\r')
@@ -12,9 +12,9 @@ def set_up_search(test):
     test.send_finished_statement('RETURN "the shell is fun" AS c;\r')
     assert test.shell_process.expect_exact(["| the shell is fun |", pexpect.EOF]) == 0
     test.send_control_statement(KEY_ACTION.CTRL_R.value)
-    
 
-def test_accept_search(temp_db):
+
+def test_accept_search(temp_db) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     test.send_finished_statement('RETURN "databases" AS a;\r')
@@ -24,21 +24,29 @@ def test_accept_search(temp_db):
     test.send_statement("\x1b\x1b")
     test.send_finished_statement(" rule\r")
     assert test.shell_process.expect_exact(["| databases rule |", pexpect.EOF]) == 0
-    
+
 
 @pytest.mark.parametrize(
     "esc",
     [
-        ("\x1b[H"),
-        ("\x1b[1~"),
-        ("\x1bOH"),
-    ]
+        "\x1b[H",
+        "\x1b[1~",
+        "\x1bOH",
+    ],
 )
-def test_accept_move_home(temp_db, esc):
+def test_accept_move_home(temp_db, esc) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     test.send_finished_statement('"databases rule" AS a;\r')
-    assert test.shell_process.expect_exact(['Error: Parser exception: extraneous input \'"databases rule"\'', pexpect.EOF]) == 0
+    assert (
+        test.shell_process.expect_exact(
+            [
+                "Error: Parser exception: extraneous input '\"databases rule\"'",
+                pexpect.EOF,
+            ],
+        )
+        == 0
+    )
     test.send_control_statement(KEY_ACTION.CTRL_R.value)
     test.send_statement("databases")
     test.send_statement(esc)
@@ -50,26 +58,31 @@ def test_accept_move_home(temp_db, esc):
 @pytest.mark.parametrize(
     "esc",
     [
-        ("\x1b[4~"),
-        ("\x1b[8~"),
-        ("\x1b[F"),
-        ("\x1bOF"),
-    ]        
+        "\x1b[4~",
+        "\x1b[8~",
+        "\x1b[F",
+        "\x1bOF",
+    ],
 )
-def test_accept_move_end(temp_db, esc):
+def test_accept_move_end(temp_db, esc) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     test.send_finished_statement('RETURN "databases ;\r')
-    assert test.shell_process.expect_exact(['Error: Parser exception: Invalid input <RETURN ">:', pexpect.EOF]) == 0
+    assert (
+        test.shell_process.expect_exact(
+            ['Error: Parser exception: Invalid input <RETURN ">:', pexpect.EOF],
+        )
+        == 0
+    )
     test.send_control_statement(KEY_ACTION.CTRL_R.value)
     test.send_statement("databases")
     test.send_statement(esc)
-    test.send_statement(KEY_ACTION.BACKSPACE.value) # remove semicolon
+    test.send_statement(KEY_ACTION.BACKSPACE.value)  # remove semicolon
     test.send_finished_statement('rule" AS a;\r')
     assert test.shell_process.expect_exact(["| databases rule |", pexpect.EOF]) == 0
 
 
-def test_next_history(temp_db):
+def test_next_history(temp_db) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     set_up_search(test)
@@ -78,9 +91,9 @@ def test_next_history(temp_db):
     test.send_statement("\x1b[A" * 2)
     test.send_finished_statement(KEY_ACTION.ENTER.value)
     assert test.shell_process.expect_exact(["| databases rule |", pexpect.EOF]) == 0
-    
 
-def test_prev_history(temp_db):
+
+def test_prev_history(temp_db) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     set_up_search(test)
@@ -91,7 +104,7 @@ def test_prev_history(temp_db):
     assert test.shell_process.expect_exact(["| the shell is fun |", pexpect.EOF]) == 0
 
 
-def test_move_left(temp_db):
+def test_move_left(temp_db) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     test.send_finished_statement('RETURN "databasesrule" AS a;\r')
@@ -99,11 +112,11 @@ def test_move_left(temp_db):
     test.send_control_statement(KEY_ACTION.CTRL_R.value)
     test.send_statement("databasesr")
     test.send_statement("\x1b[D")
-    test.send_finished_statement(' \r')
+    test.send_finished_statement(" \r")
     assert test.shell_process.expect_exact(["| databases rule |", pexpect.EOF]) == 0
 
 
-def test_move_right(temp_db):
+def test_move_right(temp_db) -> None:
     test = ShellTest().add_argument(temp_db)
     test.start()
     test.send_finished_statement('RETURN "databasesrule" AS a;\r')
@@ -111,5 +124,5 @@ def test_move_right(temp_db):
     test.send_control_statement(KEY_ACTION.CTRL_R.value)
     test.send_statement("database")
     test.send_statement("\x1b[C")
-    test.send_finished_statement(' \r')
+    test.send_finished_statement(" \r")
     assert test.shell_process.expect_exact(["| databases rule |", pexpect.EOF]) == 0
