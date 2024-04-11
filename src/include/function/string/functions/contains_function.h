eb56a196878e9c54c943dc6f0dc2a27fe6b32828
Chang Liu <liuc223@gmail.com>
2024-04-11 15:07:26 +0800

    Remove unused strings

--- src/include/function/string/functions/contains_function.h
+++ src/include/function/string/functions/contains_function.h
@@ -9,8 +9,6 @@ namespace function {
 struct Contains {
     static inline void operation(common::ku_string_t& left, common::ku_string_t& right,
         uint8_t& result) {
-        auto lStr = left.getAsString();
-        auto rStr = right.getAsString();
         int64_t pos;
         Find::operation(left, right, pos);
         result = (pos != 0);
