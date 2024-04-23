d46fbb02f54f63f06a54a32d696c7f36c766400d
ziyi chen <chenziyi990424@gmail.com>
2024-04-23 10:03:17 +0800

    Fix distinct hash table resizing

--- src/processor/operator/aggregate/aggregate_hash_table.cpp
+++ src/processor/operator/aggregate/aggregate_hash_table.cpp
@@ -49,23 +49,11 @@ bool AggregateHashTable::isAggregateValueDistinctForGroupByKeys(
         distinctKeyVectors[i] = groupByFlatKeyVectors[i];
     }
     distinctKeyVectors[groupByFlatKeyVectors.size()] = aggregateVector;
-    if (groupByFlatKeyVectors.empty()) {
-        VectorHashFunction::computeHash(aggregateVector, hashVector.get());
-    } else {
-        VectorHashFunction::computeHash(groupByFlatKeyVectors[0], hashVector.get());
-        computeAndCombineVecHash(groupByFlatKeyVectors, 1 /* startVecIdx */);
-        auto tmpHashResultVector =
-            std::make_unique<ValueVector>(LogicalTypeID::INT64, &memoryManager);
-        auto tmpHashCombineResultVector =
-            std::make_unique<ValueVector>(LogicalTypeID::INT64, &memoryManager);
-        VectorHashFunction::computeHash(aggregateVector, tmpHashResultVector.get());
-        VectorHashFunction::combineHash(hashVector.get(), tmpHashResultVector.get(),
-            tmpHashCombineResultVector.get());
-        hashVector = std::move(tmpHashResultVector);
-    }
+    computeVectorHashes(distinctKeyVectors, std::vector<ValueVector*>() /* unFlatKeyVectors */);
     hash_t hash = hashVector->getValue<hash_t>(hashVector->state->selVector->selectedPositions[0]);
     auto distinctHTEntry = findEntryInDistinctHT(distinctKeyVectors, hash);
     if (distinctHTEntry == nullptr) {
+        resizeHashTableIfNecessary(1);
         createEntryInDistinctHT(distinctKeyVectors, hash);
         return true;
     }
