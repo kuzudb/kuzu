158e6b12c07cc174a8f0172ed62e69a49cb4a954
Benjamin Winger <bmw@disroot.org>
2024-05-01 15:42:16 -0400

    Merge the HashIndex bulkstorage with the local storage for inserts

--- src/include/storage/index/in_mem_hash_index.h
+++ src/include/storage/index/in_mem_hash_index.h
@@ -72,26 +72,31 @@ public:
 
     using BufferKeyType =
         typename std::conditional<std::same_as<T, common::ku_string_t>, std::string, T>::type;
+    using Key =
+        typename std::conditional<std::same_as<T, common::ku_string_t>, std::string_view, T>::type;
     // Appends the buffer to the index. Returns the number of values successfully inserted.
     // I.e. if a key fails to insert, its index will be the return value
     size_t append(const IndexBuffer<BufferKeyType>& buffer);
-    using Key =
-        typename std::conditional<std::same_as<T, common::ku_string_t>, std::string_view, T>::type;
+    inline bool append(Key key, common::offset_t value) {
+        reserve(indexHeader.numEntries + 1);
+        return appendInternal(key, value, HashIndexUtils::hash(key));
+    }
     bool lookup(Key key, common::offset_t& result);
 
-    uint64_t size() { return this->indexHeader.numEntries; }
+    uint64_t size() const { return this->indexHeader.numEntries; }
+    bool empty() const { return size() == 0; }
 
     void clear();
 
     struct SlotIterator {
-        explicit SlotIterator(slot_id_t newSlotId, InMemHashIndex<T>* builder)
+        explicit SlotIterator(slot_id_t newSlotId, const InMemHashIndex<T>* builder)
             : slotInfo{newSlotId, SlotType::PRIMARY}, slot(builder->getSlot(slotInfo)) {}
         SlotInfo slotInfo;
         Slot<T>* slot;
     };
 
     // Leaves the slot pointer pointing at the last slot to make it easier to add a new one
-    inline bool nextChainedSlot(SlotIterator& iter) {
+    inline bool nextChainedSlot(SlotIterator& iter) const {
         iter.slotInfo.slotId = iter.slot->header.nextOvfSlotId;
         iter.slotInfo.slotType = SlotType::OVF;
         if (iter.slot->header.nextOvfSlotId != 0) {
@@ -104,12 +109,16 @@ public:
     inline uint64_t numPrimarySlots() const { return pSlots->size(); }
     inline uint64_t numOverflowSlots() const { return oSlots->size(); }
 
-    inline HashIndexHeader getIndexHeader() { return indexHeader; }
+    inline const HashIndexHeader& getIndexHeader() const { return indexHeader; }
+
+    // Deletes key, maintaining gapless structure by replacing it with the last entry in the
+    // slot
+    bool deleteKey(Key key);
 
 private:
     // Assumes that space has already been allocated for the entry
     bool appendInternal(Key key, common::offset_t value, common::hash_t hash);
-    Slot<T>* getSlot(const SlotInfo& slotInfo);
+    Slot<T>* getSlot(const SlotInfo& slotInfo) const;
 
     uint32_t allocatePSlots(uint32_t numSlotsToAllocate);
     uint32_t allocateAOSlot();
