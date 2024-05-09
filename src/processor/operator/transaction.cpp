c866e98ad05e5f66d35f7adfb8013ef45f38cef6
Guodong Jin <guod.jin@gmail.com>
2024-05-09 14:45:43 -0400

    throw exceptions for nested transactions; correct error message

--- src/processor/operator/transaction.cpp
+++ src/processor/operator/transaction.cpp
@@ -1,8 +1,10 @@
 #include "processor/operator/transaction.h"
 
+#include "common/exception/transaction_manager.h"
 #include "transaction/transaction_context.h"
 
 using namespace kuzu::common;
+using namespace kuzu::transaction;
 
 namespace kuzu {
 namespace processor {
@@ -13,29 +15,60 @@ bool Transaction::getNextTuplesInternal(ExecutionContext* context) {
     }
     hasExecuted = true;
     auto transactionContext = context->clientContext->getTransactionContext();
+    validateActiveTransaction(*transactionContext);
     switch (transactionAction) {
-    case transaction::TransactionAction::BEGIN_READ: {
+    case TransactionAction::BEGIN_READ: {
         transactionContext->beginReadTransaction();
     } break;
-    case transaction::TransactionAction::BEGIN_WRITE: {
+    case TransactionAction::BEGIN_WRITE: {
         transactionContext->beginWriteTransaction();
     } break;
-    case transaction::TransactionAction::COMMIT: {
+    case TransactionAction::COMMIT: {
         transactionContext->commit();
     } break;
-    case transaction::TransactionAction::COMMIT_SKIP_CHECKPOINTING: {
+    case TransactionAction::COMMIT_SKIP_CHECKPOINTING: {
         transactionContext->commitSkipCheckPointing();
     } break;
-    case transaction::TransactionAction::ROLLBACK: {
+    case TransactionAction::ROLLBACK: {
         transactionContext->rollback();
     } break;
-    case transaction::TransactionAction::ROLLBACK_SKIP_CHECKPOINTING: {
+    case TransactionAction::ROLLBACK_SKIP_CHECKPOINTING: {
         transactionContext->rollbackSkipCheckPointing();
     } break;
-    default:
+    default: {
         KU_UNREACHABLE;
     }
+    }
     return true;
+}
+
+void Transaction::validateActiveTransaction(const TransactionContext& context) const {
+    switch (transactionAction) {
+    case TransactionAction::BEGIN_READ:
+    case TransactionAction::BEGIN_WRITE: {
+        if (context.hasActiveTransaction()) {
+            throw TransactionManagerException(
+                "Connection already has an active transaction. Cannot start a transaction within "
+                "another one. For concurrent multiple transactions, please open other "
+                "connections.");
+        }
+    } break;
+    case TransactionAction::COMMIT:
+    case TransactionAction::COMMIT_SKIP_CHECKPOINTING: {
+        if (!context.hasActiveTransaction()) {
+            throw TransactionManagerException("No active transaction for COMMIT.");
+        }
+    } break;
+    case TransactionAction::ROLLBACK:
+    case TransactionAction::ROLLBACK_SKIP_CHECKPOINTING: {
+        if (!context.hasActiveTransaction()) {
+            throw TransactionManagerException("No active transaction for ROLLBACK.");
+        }
+    } break;
+    default: {
+        KU_UNREACHABLE;
+    }
+    }
 }
 
 } // namespace processor
