a1ff10552978a8451a5baf81a286496aebe83f50
Benjamin Winger <bmw@disroot.org>
2024-05-16 14:23:54 -0400

    Make serializer tool able to be run standalone (#3501)
    and add a script to generate the binary db

--- benchmark/serializer.py
+++ benchmark/serializer.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 import subprocess
 import re
+import argparse
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 kuzu_exec_path = os.path.join(
@@ -18,7 +19,7 @@ def _get_kuzu_version():
                 return line.split(' ')[2].strip()
 
 
-def serialize(dataset_name, dataset_path, serialized_graph_path, benchmark_copy_log_dir):
+def serialize(dataset_name, dataset_path, serialized_graph_path, benchmark_copy_log_dir, single_thread: bool = False):
     bin_version = _get_kuzu_version()
 
     if not os.path.exists(serialized_graph_path):
@@ -42,8 +43,17 @@ def serialize(dataset_name, dataset_path, serialized_graph_path, benchmark_copy_
     shutil.rmtree(serialized_graph_path, ignore_errors=True)
     os.mkdir(serialized_graph_path)
 
-    with open(os.path.join(base_dir, 'serialize.cypher'), 'r') as f:
-        serialize_queries = f.readlines()
+    serialize_queries = []
+    if single_thread:
+        serialize_queries.append("CALL THREADS=1")
+    if os.path.exists(os.path.join(dataset_path, 'schema.cypher')):
+        with open(os.path.join(dataset_path, 'schema.cypher'), 'r') as f:
+            serialize_queries += f.readlines()
+        with open(os.path.join(dataset_path, 'copy.cypher'), 'r') as f:
+            serialize_queries += f.readlines()
+    else:
+        with open(os.path.join(base_dir, 'serialize.cypher'), 'r') as f:
+            serialize_queries += f.readlines()
     serialize_queries = [q.strip().replace('{}', dataset_path)
                          for q in serialize_queries]
 
@@ -51,26 +61,32 @@ def serialize(dataset_name, dataset_path, serialized_graph_path, benchmark_copy_
 
     for s in serialize_queries:
         logging.info('Executing query: %s', s)
-        create_match = re.match('create\s+(.+?)\s+table\s+(.+?)\s*\(', s, re.IGNORECASE)
+        create_match = re.match(r'create\s+(.+?)\s+table\s+(.+?)\s*\(', s, re.IGNORECASE)
+        copy_match = re.match(r'copy\s+(.+?)\s+from', s, re.IGNORECASE)
         # Run kuzu shell one query at a time. This ensures a new process is
         # created for each query to avoid memory leaks.
         process = subprocess.Popen([kuzu_exec_path, serialized_graph_path],
-            stdin=subprocess.PIPE, stdout=sys.stdout if create_match else subprocess.PIPE)
-        process.stdin.write((s + ";" + "\n").encode("ascii"))
+            stdin=subprocess.PIPE, stdout=sys.stdout if create_match else subprocess.PIPE, encoding="utf-8")
+        process.stdin.write(s + ";\n")
         process.stdin.close()
         if create_match:
             table_types[create_match.group(2)] = create_match.group(1).lower()
-        else:
-            copy_match = re.match('copy\s+(.+?)\s+from', s, re.IGNORECASE)
+        elif copy_match:
             filename = table_types[copy_match.group(1)] + '-' + copy_match.group(1).replace('_', '-') + '_log.txt'
-            with open(os.path.join(benchmark_copy_log_dir, filename), 'ab') as f:
-                for line in iter(process.stdout.readline, b''):
-                    print(line.decode("utf-8"), end='', flush=True)
-                    f.write(line)
+            if benchmark_copy_log_dir:
+                os.makedirs(benchmark_copy_log_dir, exist_ok=True)
+                with open(os.path.join(benchmark_copy_log_dir, filename), 'a', encoding="utf-8") as f:
+                    for line in process.stdout.readlines():
+                        print(line, end="", flush=True)
+                        print(line, file=f)
+        elif s == "CALL THREADS=1":
+            pass
+        else:
+            raise RuntimeError(f"Unrecognized query {s}")
+
         process.wait()
         if process.returncode != 0:
-            logging.error('Error executing query: %s', s)
-            raise subprocess.CalledProcessError
+            raise RuntimeError(f'Error {process.returncode} executing query: {s}')
 
     with open(os.path.join(serialized_graph_path, 'version.txt'), 'w') as f:
         f.write(bin_version)
@@ -78,16 +94,18 @@ def serialize(dataset_name, dataset_path, serialized_graph_path, benchmark_copy_
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    dataset_name = sys.argv[1]
-    dataset_path = sys.argv[2]
-    serialized_graph_path = sys.argv[3]
-    benchmark_copy_log_dir = sys.argv[4]
+    parser = argparse.ArgumentParser(description='Serializes dataset to a kuzu database')
+    parser.add_argument("dataset_name", help="Name of the dataset for dispay purposes")
+    parser.add_argument("dataset_path", help="Input path of the dataset to serialize")
+    parser.add_argument("serialized_graph_path", help="Output path of the database. Will be created if it does not exist already")
+    parser.add_argument("benchmark_copy_log_dir", help="Optional directory to store copy logs", nargs="?")
+    parser.add_argument("--single-thread", help="If true, copy single threaded, which makes the results more reproduceable", action="store_true")
+    args = parser.parse_args()
     try:
-        serialize(dataset_name, dataset_path, serialized_graph_path, benchmark_copy_log_dir)
+        serialize(args.dataset_name, args.dataset_path, args.serialized_graph_path, args.benchmark_copy_log_dir, args.single_thread)
     except Exception as e:
-        logging.error('Error serializing dataset %s', dataset_name)
-        logging.error(e)
-        sys.exit(1)
+        logging.error(f'Error serializing dataset {args.dataset_name}')
+        raise e
     finally:
         shutil.rmtree(os.path.join(base_dir, 'history.txt'),
                       ignore_errors=True)
