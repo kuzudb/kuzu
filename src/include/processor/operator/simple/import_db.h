96f2776d773a17303a25b90656d44fe1d5924d7b
Guodong Jin <guod.jin@gmail.com>
2024-05-24 13:33:34 -0400

    bump extension version

--- src/include/processor/operator/simple/import_db.h
+++ src/include/processor/operator/simple/import_db.h
@@ -5,15 +5,15 @@
 namespace kuzu {
 namespace processor {
 
-class ImportDB : public Simple {
+class ImportDB final : public Simple {
 public:
     ImportDB(std::string query, const DataPos& outputPos, uint32_t id,
         const std::string& paramsString)
         : Simple{PhysicalOperatorType::IMPORT_DATABASE, outputPos, id, paramsString}, query{query} {
     }
 
-    void executeInternal(ExecutionContext* context) final;
-    std::string getOutputMsg() final;
+    void executeInternal(ExecutionContext* context) override;
+    std::string getOutputMsg() override;
 
     inline std::unique_ptr<PhysicalOperator> clone() override {
         return std::make_unique<ImportDB>(query, outputPos, id, paramsString);
