0c352c726af4790f7ef772682d974ddab6d0ed3d
Chang Liu <liuc223@gmail.com>
2024-06-01 21:46:48 -0400

    Add compare_highlight_threshold parameter to get_compare_result function

--- benchmark/benchmark_runner.py
+++ benchmark/benchmark_runner.py
@@ -354,6 +354,7 @@ def get_compare_result(report_id):
     params = {
         'master_commit_hash': master_commit_hash,
         'branch_result_id': report_id,
+        'compare_highlight_threshold': 10,
     }
 
     url = benchmark_server_url + '/compare'
