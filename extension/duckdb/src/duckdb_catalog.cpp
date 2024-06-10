dffb21e3e3e1eabe823212d58dc3dc0db3d74c0a
ziyi chen <chenziyi990424@gmail.com>
2024-06-10 16:34:48 +0800

    update

--- extension/duckdb/src/duckdb_catalog.cpp
+++ extension/duckdb/src/duckdb_catalog.cpp
@@ -140,6 +140,7 @@ std::unique_ptr<binder::BoundCreateTableInfo> DuckDBCatalog::bindCreateTableInfo
         return nullptr;
     }
     return std::make_unique<binder::BoundCreateTableInfo>(common::TableType::FOREIGN, tableName,
+        parser::OnConflictOperation::ERROR,
         std::make_unique<duckdb_extension::BoundExtraCreateDuckDBTableInfo>(dbPath, catalogName,
             getDefaultSchemaName(), std::move(propertyInfos)));
 }
