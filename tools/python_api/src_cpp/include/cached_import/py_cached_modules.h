e3003b9d990f51ff7c1b98e5ed2087ebea612f90
mxwli <maxwli@outlook.com>
2024-06-11 14:23:00 -0400

    add requested changes

--- tools/python_api/src_cpp/include/cached_import/py_cached_modules.h
+++ tools/python_api/src_cpp/include/cached_import/py_cached_modules.h
@@ -5,7 +5,6 @@
 namespace kuzu {
 
 class DateTimeCachedItem : public PythonCachedItem {
-
 public:
     DateTimeCachedItem()
         : PythonCachedItem("datetime"), date("date", this), datetime("datetime", this),
@@ -17,7 +16,6 @@ public:
 };
 
 class DecimalCachedItem : public PythonCachedItem {
-
 public:
     DecimalCachedItem() : PythonCachedItem("decimal"), Decimal("Decimal", this) {}
 
@@ -25,12 +23,10 @@ public:
 };
 
 class ImportLibCachedItem : public PythonCachedItem {
-
     class UtilCachedItem : public PythonCachedItem {
-
     public:
         explicit UtilCachedItem(PythonCachedItem* parent)
-            : PythonCachedItem("util", parent), find_spec("find_spec", this) {}
+            : PythonCachedItem{"util", parent}, find_spec{"find_spec", this} {}
 
         PythonCachedItem find_spec;
     };
@@ -42,7 +38,6 @@ public:
 };
 
 class InspectCachedItem : public PythonCachedItem {
-
 public:
     InspectCachedItem()
         : PythonCachedItem("inspect"), currentframe("currentframe", this),
@@ -54,7 +49,6 @@ public:
 };
 
 class NumpyMaCachedItem : public PythonCachedItem {
-
 public:
     NumpyMaCachedItem() : PythonCachedItem("numpy.ma"), masked_array("masked_array", this) {}
 
@@ -62,7 +56,6 @@ public:
 };
 
 class PandasCachedItem : public PythonCachedItem {
-
     class SeriesCachedItem : public PythonCachedItem {
     public:
         explicit SeriesCachedItem(PythonCachedItem* parent)
@@ -107,7 +100,6 @@ public:
 };
 
 class PyarrowCachedItem : public PythonCachedItem {
-
     class RecordBatchCachedItem : public PythonCachedItem {
     public:
         explicit RecordBatchCachedItem(PythonCachedItem* parent)
@@ -151,7 +143,6 @@ public:
 };
 
 class UUIDCachedItem : public PythonCachedItem {
-
 public:
     UUIDCachedItem() : PythonCachedItem("uuid"), UUID("UUID", this) {}
 
