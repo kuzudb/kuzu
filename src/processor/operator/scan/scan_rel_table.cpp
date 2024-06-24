b1004beaadd38eb6fcac98ff21cea2ac637b6c97
yiyun-sj <samyiyunjia@gmail.com>
2024-06-24 15:42:30 -0400

    passes basic case

--- src/processor/operator/scan/scan_rel_table.cpp
+++ src/processor/operator/scan/scan_rel_table.cpp
@@ -14,6 +14,9 @@ void ScanRelTableInfo::initScanState() {
 
 void ScanRelTableInfo::scanIfNecessary(Transaction* transaction) const {
     auto& relScanState = localScanState->dataScanState->cast<RelDataReadState>();
+    auto& outSelVector = localScanState->outputVectors[0]->state->getSelVectorUnsafe();
+    auto& nodeIDSelVector = localScanState->nodeIDVector->state->getSelVectorUnsafe();
+    nodeIDSelVector.setToUnfiltered(relScanState.totalNodeIdxs);
     // We need to reinitialize per node group
     if (relScanState.currNodeIdx == relScanState.endNodeIdx) {
         table->initializeScanState(transaction, *localScanState);
@@ -23,18 +26,32 @@ void ScanRelTableInfo::scanIfNecessary(Transaction* transaction) const {
         table->scan(transaction, *localScanState);
     }
     auto startNodeOffset = StorageUtils::getStartOffsetOfNodeGroup(relScanState.nodeGroupIdx);
-    auto& outSelVector = localScanState->outputVectors[0]->state->getSelVectorUnsafe();
+    relScanState.currentNodeOffset = 
+        localScanState->nodeIDVector->readNodeOffset(nodeIDSelVector[relScanState.currNodeIdx]);
+    // This assumes nodeIDVector is initially unfiltered, which is not safe
+    // we should do this using similar logic to Flatten
+    nodeIDSelVector.getMultableBuffer()[0] = nodeIDSelVector[relScanState.currNodeIdx];
+    nodeIDSelVector.setToFiltered(1);
+    auto currCSROffset =
+        relScanState.csrHeaderChunks.getEndCSROffset(relScanState.currentNodeOffset - startNodeOffset) - 
+        relScanState.csrHeaderChunks.getStartCSROffset(relScanState.currentNodeOffset - startNodeOffset);
     auto currCSRSize =
         relScanState.csrHeaderChunks.getCSRLength(relScanState.currentNodeOffset - startNodeOffset);
-    if (currCSRSize > DEFAULT_VECTOR_CAPACITY - relScanState.currentCSROffset) {
-        currCSRSize = DEFAULT_VECTOR_CAPACITY - relScanState.currentCSROffset;
+    if (relScanState.currentCSROffset == 0) {
+        currCSROffset -= relScanState.posInLastCSR;
+        currCSRSize = relScanState.posInLastCSR > currCSRSize ? 0 : currCSRSize - relScanState.posInLastCSR;
+    }
+    auto spaceLeft = DEFAULT_VECTOR_CAPACITY - relScanState.currentCSROffset;
+    if (currCSROffset > spaceLeft) {
+        currCSROffset = spaceLeft;
+        currCSRSize = std::min(currCSRSize, spaceLeft);
     } else {
         relScanState.currNodeIdx++;
     }
     for (auto i = 0u; i < currCSRSize; i++) {
         outSelVector.getMultableBuffer()[i] = i + relScanState.currentCSROffset;
     }
-    relScanState.currentCSROffset += currCSRSize;
+    relScanState.currentCSROffset += currCSROffset;
     outSelVector.setToFiltered(currCSRSize);
 }
 
