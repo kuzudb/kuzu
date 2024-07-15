3d7a436bf7a4926f7f5bca41e8ef990c828845ab
mxwli <maxwli@outlook.com>
2024-07-15 15:39:35 -0400

    initial

--- tools/python_api/src_cpp/cached_import/py_cached_item.cpp
+++ tools/python_api/src_cpp/cached_import/py_cached_item.cpp
@@ -1,12 +1,14 @@
 #include "cached_import/py_cached_item.h"
 
+#include "common/assert.h"
 #include "cached_import/py_cached_import.h"
+#include "common/exception/not_implemented.h"
 #include "common/exception/runtime.h"
 
 namespace kuzu {
 
 py::handle PythonCachedItem::operator()() {
-    assert((bool)PyGILState_Check());
+    KU_ASSERT((bool)PyGILState_Check());
     // load if unloaded, return cached object if already loaded
     if (loaded) {
         return object;
@@ -18,6 +20,18 @@ py::handle PythonCachedItem::operator()() {
     }
     loaded = true;
     return object;
+}
+
+py::handle NumpyMaCachedItem::operator()() {
+    // TODO: implement compatibility with numpy 2.0.0
+    KU_ASSERT((bool)PyGILState_Check());
+    auto obj = numpy();
+    if (py::cast<std::string>(obj.attr("__version__"))[0] >= '2') {
+        throw common::NotImplementedException(
+            "Kuzu cannot currently support numpy versions at or above 2.0.0"
+            "Try 1.26.x");
+    }
+    return PythonCachedItem::operator()();
 }
 
 } // namespace kuzu
