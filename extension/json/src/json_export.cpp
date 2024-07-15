693bcb82e5b7dc226f7e5b9b56afd6774927bd44
mxwli <maxwli@outlook.com>
2024-07-15 12:31:58 -0400

    clang-tidy

--- extension/json/src/json_export.cpp
+++ extension/json/src/json_export.cpp
@@ -34,7 +34,7 @@ struct ExportJSONSharedState : public ExportFuncSharedState {
     std::unique_ptr<FileInfo> fileInfo;
     std::vector<std::string> jsonValues;
 
-    virtual void init(main::ClientContext& context, const ExportFuncBindData& bindData) {
+    void init(main::ClientContext& context, const ExportFuncBindData& bindData) override {
         fileInfo = context.getVFSUnsafe()->openFile(bindData.fileName, O_WRONLY | O_CREAT | O_TRUNC,
             &context);
     }
