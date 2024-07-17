29c7e9b7df0ae49312eae27dcca3e9628b14f80b
mxwli <maxwli@outlook.com>
2024-07-17 13:16:50 -0400

    revert to master pybind11/tools to see what happens

--- third_party/pybind11/tools/codespell_ignore_lines_from_errors.py
+++ third_party/pybind11/tools/codespell_ignore_lines_from_errors.py
@@ -9,27 +9,22 @@ python3 tools/codespell_ignore_lines_from_errors.py /tmp/codespell_errors.txt > 
 git diff to review changes, then commit, push.
 """
 
-from __future__ import annotations
-
 import sys
+from typing import List
 
 
-def run(args: list[str]) -> None:
+def run(args: List[str]) -> None:
     assert len(args) == 1, "codespell_errors.txt"
     cache = {}
     done = set()
-    with open(args[0]) as f:
-        lines = f.read().splitlines()
-
-    for line in sorted(lines):
+    for line in sorted(open(args[0]).read().splitlines()):
         i = line.find(" ==> ")
         if i > 0:
             flds = line[:i].split(":")
             if len(flds) >= 2:
                 filename, line_num = flds[:2]
                 if filename not in cache:
-                    with open(filename) as f:
-                        cache[filename] = f.read().splitlines()
+                    cache[filename] = open(filename).read().splitlines()
                 supp = cache[filename][int(line_num) - 1]
                 if supp not in done:
                     print(supp)
