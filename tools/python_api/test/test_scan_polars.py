64d6e4b646a4f5c84c1c96353756e34e9e16b540
Maxwell <49460053+mxwli@users.noreply.github.com>
2024-07-19 15:28:37 -0400

    Add pyarrow `COPY FROM` testing (#3873)

--- tools/python_api/test/test_scan_polars.py
+++ tools/python_api/test/test_scan_polars.py
@@ -5,8 +5,8 @@ import pytest
 from type_aliases import ConnDB
 
 
-def test_polars_basic(conn_db_readonly: ConnDB) -> None:
-    conn, db = conn_db_readonly
+def test_polars_basic(conn_db_readwrite: ConnDB) -> None:
+    conn, db = conn_db_readwrite
     df = pl.DataFrame([
         pl.Series("col1", [1, 2], dtype=pl.Int64),
         pl.Series("col2", ["a", "b"], dtype=pl.String),
@@ -24,6 +24,18 @@ def test_polars_basic(conn_db_readonly: ConnDB) -> None:
     assert result["col3"].dtype == pl.Float64
     assert result["col4"].dtype == pl.Datetime
     # since polars just uses arrow as its backend, it's probably not necessary to extensively test this functionality
+    conn.execute("CREATE NODE TABLE polarstab(col1 INT64, col2 STRING, col3 DOUBLE, col4 TIMESTAMP, PRIMARY KEY(col1))")
+    conn.execute("COPY polarstab FROM df")
+    result = conn.execute("MATCH (t:polarstab) RETURN t.col1 AS col1, t.col2 AS col2, t.col3 AS col3, t.col4 AS col4 ORDER BY col1").get_as_pl()
+    equivalency = result == df
+    assert equivalency["col1"].all()
+    assert equivalency["col2"].all()
+    assert equivalency["col3"].all()
+    assert equivalency["col4"].all()
+    assert result["col1"].dtype == pl.Int64
+    assert result["col2"].dtype == pl.String
+    assert result["col3"].dtype == pl.Float64
+    assert result["col4"].dtype == pl.Datetime
 
 
 def test_polars_error(conn_db_readonly: ConnDB) -> None:
