5333102aa40666b9dfb6a8c9d343f1cfcc7a7462
mxwli <maxwli@outlook.com>
2024-07-19 13:27:14 -0400

    add order in scan tests

--- tools/python_api/test/test_scan_pyarrow.py
+++ tools/python_api/test/test_scan_pyarrow.py
@@ -14,7 +14,7 @@ def test_pyarrow_basic(conn_db_readonly : ConnDB) -> None:
         ],
         names=["col1", "col2", "col3"]
     )
-    result = conn.execute("LOAD FROM tab RETURN *")
+    result = conn.execute("LOAD FROM tab RETURN * ORDER BY col1")
     assert (result.get_next() == [1, 'a', True])
     assert (result.get_next() == [2, 'b', False])
     assert (result.get_next() == [3, 'c', None])
@@ -31,7 +31,7 @@ def test_pyarrow_copy_from(conn_db_readwrite : ConnDB) -> None:
     )
     conn.execute("CREATE NODE TABLE pyarrowtab(id INT32, A STRING, B BOOL, PRIMARY KEY(id))")
     conn.execute("COPY pyarrowtab FROM tab")
-    result = conn.execute("MATCH (t:pyarrowtab) RETURN t.id AS id, t.A AS A, t.B AS B")
+    result = conn.execute("MATCH (t:pyarrowtab) RETURN t.id AS id, t.A AS A, t.B AS B ORDER BY t.id")
     assert (result.get_next() == [1, 'a', True])
     assert (result.get_next() == [2, 'b', False])
     assert (result.get_next() == [3, 'c', None])
@@ -45,7 +45,7 @@ def test_pyarrow_copy_from(conn_db_readwrite : ConnDB) -> None:
     )
     conn.execute("CREATE REL TABLE pyarrowrel(FROM pyarrowtab TO pyarrowtab)")
     conn.execute("COPY pyarrowrel FROM rels")
-    result = conn.execute("MATCH (a:pyarrowtab)-[:pyarrowrel]->(b:pyarrowtab) RETURN a.id, b.id")
+    result = conn.execute("MATCH (a:pyarrowtab)-[:pyarrowrel]->(b:pyarrowtab) RETURN a.id, b.id ORDER BY a.id")
     assert result.get_next() == [1, 2]
     assert result.get_next() == [2, 3]
     assert result.get_next() == [3, 1]
@@ -63,7 +63,7 @@ def test_pyarrow_copy_from(conn_db_readwrite : ConnDB) -> None:
 
     conn.execute("CREATE REL TABLE pyarrowrel(FROM pyarrowtab TO pyarrowtab, a_distraction STRING)")
     conn.execute("COPY pyarrowrel FROM rels")
-    result = conn.execute("MATCH (a:pyarrowtab)-[r:pyarrowrel]->(b:pyarrowtab) RETURN a.id, r.a_distraction, b.id")
+    result = conn.execute("MATCH (a:pyarrowtab)-[r:pyarrowrel]->(b:pyarrowtab) RETURN a.id, r.a_distraction, b.id ORDER BY a.id")
     assert result.get_next() == [1, "honk", 2]
     assert result.get_next() == [2, "shoo", 3]
     assert result.get_next() == [3, "mimimimimimimi", 1]
