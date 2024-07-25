mwatts/ladybug

Optimize small rel table lookups (#3813)
Adds a random access version of OnDiskGraph::scanFwd which reads only the CSR header information necessary for the requested nodes
Also fixes the caching in OnDiskGraph scans so that the cache isn't regenerated for every scan

src/function/gds/gds_task.cpp
@@ -9,11 +9,12 @@ void GDSTask::run() {
     RangeFrontierMorsel frontierMorsel;
     auto numApproxActiveNodesForNextIter = 0u;
     std::unique_ptr<graph::Graph> graph = sharedState->graph->copy();
+    auto state = graph->prepareScan(sharedState->relTableIDToScan);
     while (sharedState->frontiers.getNextFrontierMorsel(frontierMorsel)) {
         while (frontierMorsel.hasNextVertex()) {
             nodeID_t nodeID = frontierMorsel.getNextVertex();
             if (sharedState->frontiers.curFrontier->isActive(nodeID)) {
-                auto nbrIDs = graph->scanFwd(nodeID, sharedState->relTableIDToScan);
+                auto nbrIDs = graph->scanFwd(nodeID, *state);
                 for (auto nbrID : nbrIDs) {
                     if (sharedState->fc.edgeCompute(nodeID, nbrID)) {
                         sharedState->frontiers.nextFrontier->setActive(nbrID);

kuzudb/kuzu

src/function/gds/gds_task.cpp
@@ -9,11 +9,12 @@ void GDSTask::run() {
     RangeFrontierMorsel frontierMorsel;
     auto numApproxActiveNodesForNextIter = 0u;
     std::unique_ptr<graph::Graph> graph = sharedState->graph->copy();
+    auto state = graph->prepareScan(sharedState->relTableIDToScan);
     while (sharedState->frontiers.getNextFrontierMorsel(frontierMorsel)) {
         while (frontierMorsel.hasNextVertex()) {
             nodeID_t nodeID = frontierMorsel.getNextVertex();
             if (sharedState->frontiers.curFrontier->isActive(nodeID)) {
-                auto nbrs = graph->scanFwd(nodeID, sharedState->relTableIDToScan);
+                auto nbrs = graph->scanFwd(nodeID, *state);
                 for (auto nbr : nbrs) {
                     if (sharedState->fc.edgeCompute(nbr)) {
                         sharedState->frontiers.nextFrontier->setActive(nbr);