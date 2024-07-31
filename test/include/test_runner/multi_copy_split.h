e650c9bdc46f52eb1b2d3f11fe775bc87df85e5d
yiyun-sj <samyiyunjia@gmail.com>
2024-07-31 12:39:31 -0400

    use load from to create tmp file

--- test/include/test_runner/multi_copy_split.h
+++ test/include/test_runner/multi_copy_split.h
@@ -11,15 +11,18 @@ namespace testing {
 // Insert a dataset row by row instead of batch insert (copy)
 class SplitMultiCopyRandom {
 public:
-    SplitMultiCopyRandom(uint32_t numSplit, std::string tableName, std::string filePath, main::Connection& connection)
-        : numSplit{numSplit}, tableName{tableName}, filePath{filePath}, connection{connection} {}
+    SplitMultiCopyRandom(uint32_t numSplit, std::string tableName, std::string source, main::Connection& connection)
+        : numSplit{numSplit}, tableName{tableName}, source{source}, connection{connection} {}
     void init();
     void run();
 
 private:
+    std::vector<uint32_t> getLineEnds(std::string path) const;
+
+private:
     uint32_t numSplit;
     std::string tableName;
-    std::string filePath;
+    std::string source;
     main::Connection& connection;
     std::vector<std::string> splitFilePaths;
 };
