0d0ac6a24ec4e05ef110be06e65e445071e6c6b8
囧囧 <liuc223@gmail.com>
2024-08-01 16:49:36 -0400

    Release cpp unique_ptr and manually create opaque pointer for C API tests (#4005)

--- test/include/c_api_test/c_api_test.h
+++ test/include/c_api_test/c_api_test.h
@@ -13,16 +13,10 @@ public:
 
     void SetUp() override {
         APIDBTest::SetUp();
-        // In C API tests, we don't use the database and connection created by DBTest because
-        // they are not C++ objects.
-        conn.reset();
-        database.reset();
-        auto databasePath = getDatabasePath();
-        auto databasePathCStr = databasePath.c_str();
-        auto systemConfig = kuzu_default_system_config();
-        systemConfig.buffer_pool_size = 512 * 1024 * 1024;
-        kuzu_database_init(databasePathCStr, systemConfig, &_database);
-        kuzu_connection_init(&_database, &connection);
+        auto* connCppPointer = conn.release();
+        auto* databaseCppPointer = database.release();
+        connection = kuzu_connection{connCppPointer};
+        _database = kuzu_database{databaseCppPointer};
     }
 
     std::string getDatabasePath() { return databasePath; }
