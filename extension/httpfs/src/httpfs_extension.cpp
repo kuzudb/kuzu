f6b416f9cb97da025e32b3bdafa1c334bcbca66b
ziyi chen <chenziyi990424@gmail.com>
2024-08-08 11:17:08 +0800

    update

--- extension/httpfs/src/httpfs_extension.cpp
+++ extension/httpfs/src/httpfs_extension.cpp
@@ -23,8 +23,8 @@ static void registerExtensionOptions(main::Database* db) {
         common::Value{(int64_t)10000});
     db->addExtensionOption("s3_uploader_threads_limit", common::LogicalTypeID::INT64,
         common::Value{(int64_t)50});
-    db->addExtensionOption(HTTPCacheInMemoryConfig::HTTP_CACHE_FILE_OPTION,
-        common::LogicalTypeID::BOOL, common::Value{HTTPCacheInMemoryConfig::DEFAULT_CACHE_FILE});
+    db->addExtensionOption(HTTPCacheFileConfig::HTTP_CACHE_FILE_OPTION, common::LogicalTypeID::BOOL,
+        common::Value{HTTPCacheFileConfig::DEFAULT_CACHE_FILE});
 }
 
 static void registerFileSystem(main::Database* db) {
