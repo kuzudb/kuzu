e0ce72e6934d9e665a071619db7cec05b7b92991
Royi Luo <r34luo@uwaterloo.ca>
2024-08-16 11:03:22 -0400

    Add tests for query result warnings

--- test/copy/multi_copy_test.cpp
+++ test/copy/multi_copy_test.cpp
@@ -8,6 +8,7 @@
 #include "common/file_system/local_file_system.h"
 #include "common/string_format.h"
 #include "graph_test/graph_test.h"
+#include "processor/operator/persistent/reader/csv/csv_error.h"
 
 namespace kuzu {
 namespace testing {
@@ -24,26 +25,66 @@ public:
         ASSERT_TRUE(result->isSuccess()) << result->toString();
     }
 
-    std::string generateFile(size_t values) {
-        auto tempDir = TestHelper::getTempDir(getTestGroupAndName());
-        auto filePath = common::LocalFileSystem::joinPath(tempDir, "tmp.csv");
+    std::string generateFile(uint64_t values,
+        std::optional<std::vector<uint64_t>> providedValues = {},
+        std::optional<const std::string> providedFilePath = {}) {
+        if (providedValues.has_value()) {
+            EXPECT_EQ(values, providedValues->size());
+        }
+
+        std::string filePath;
+        if (providedFilePath.has_value()) {
+            filePath = providedFilePath.value();
+        } else {
+            auto tempDir = TestHelper::getTempDir(getTestGroupAndName());
+            filePath = common::LocalFileSystem::joinPath(tempDir, "tmp.csv");
+            std::filesystem::create_directories(tempDir);
+        }
 #if defined(_WIN32)
         std::replace(filePath.begin(), filePath.end(), '\\', '/');
 #endif
-        std::filesystem::create_directories(tempDir);
         std::ofstream file(filePath);
-        for (size_t i = 0; i < values; i++) {
-            file << (totalTuples + i) << std::endl;
+        if (providedValues.has_value()) {
+            for (uint64_t value : providedValues.value()) {
+                file << value << std::endl;
+            }
+        } else {
+            for (size_t i = 0; i < values; i++) {
+                file << (totalTuples + i) << std::endl;
+            }
         }
         file.close();
         totalTuples += values;
         return filePath;
     }
 
-    void copy(size_t values) {
-        auto filePath = generateFile(values);
-        auto result = conn->query(common::stringFormat("COPY Test FROM '{}'", filePath));
+    void checkCopyQueryResult(std::unique_ptr<main::QueryResult>& result,
+        const std::vector<processor::PopulatedCSVError>& expectedWarnings) {
+        EXPECT_EQ(result->getNumWarnings(), expectedWarnings.size());
+        for (const auto& expectedWarning : expectedWarnings) {
+            ASSERT_TRUE(result->hasNextWarning());
+            auto nextWarning = result->getNextWarning();
+            EXPECT_EQ(nextWarning->getValue(0)->getValue<std::string>(), expectedWarning.message);
+            EXPECT_EQ(nextWarning->getValue(1)->getValue<std::string>(), expectedWarning.filePath);
+            EXPECT_EQ(nextWarning->getValue(2)->getValue<uint64_t>(), expectedWarning.lineNumber);
+            EXPECT_EQ(nextWarning->getValue(3)->getValue<std::string>(),
+                expectedWarning.reconstructedLine);
+        }
+        EXPECT_FALSE(result->hasNextWarning());
+    }
+
+    void copy(uint64_t values, const std::optional<std::vector<uint64_t>>& providedValues = {},
+        const std::optional<std::vector<processor::PopulatedCSVError>>& expectedWarnings = {},
+        const std::optional<std::string>& providedFilePath = {}) {
+        auto filePath = generateFile(values, providedValues, providedFilePath);
+        auto result =
+            conn->query(common::stringFormat("COPY Test FROM '{}'(IGNORE_ERRORS=true)", filePath));
         ASSERT_TRUE(result->isSuccess()) << result->toString();
+        if (expectedWarnings.has_value()) {
+            checkCopyQueryResult(result, expectedWarnings.value());
+            result->resetWarningIterator();
+            checkCopyQueryResult(result, expectedWarnings.value());
+        }
     }
 
     void copySerial(size_t values) {
@@ -105,6 +146,24 @@ TEST_F(MultiCopyTest, OneNodeGroup) {
     copy(1);
     copy(5);
     validate();
+}
+
+TEST_F(MultiCopyTest, WarningsArePopulated) {
+    std::vector<uint64_t> values(10, 1LL << 60);
+    std::vector<processor::PopulatedCSVError> expectedWarnings;
+    auto tempDir = TestHelper::getTempDir(getTestGroupAndName());
+    const auto filePath = common::LocalFileSystem::joinPath(tempDir, "tmp.csv");
+    std::filesystem::create_directories(tempDir);
+    for (size_t i = 0; i < values.size(); ++i) {
+        expectedWarnings.push_back({
+            .message = "Conversion exception: Cast failed. Could not convert "
+                       "\"1152921504606846976\" to INT32.",
+            .filePath = filePath,
+            .reconstructedLine = std::to_string(values[0]),
+            .lineNumber = i + 1,
+        });
+    }
+    copy(values.size(), values, expectedWarnings, filePath);
 }
 
 // Tests that a second copy that does not modify any existing node groups succeeds
