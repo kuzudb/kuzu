73f797264f493ef40c3e4b6e0c939de8268f9f21
ziyi chen <chenziyi990424@gmail.com>
2024-08-23 14:43:31 +0800

    update

--- src/function/list/quantifier_functions.cpp
+++ src/function/list/quantifier_functions.cpp
@@ -13,22 +13,24 @@ void execQuantifierFunc(quantifier_handler handler,
     const std::vector<std::shared_ptr<ValueVector>>& input, ValueVector& result, void* bindData) {
     auto listLambdaBindData = reinterpret_cast<evaluator::ListLambdaBindData*>(bindData);
     auto inputVector = input[0].get();
-    auto listSize = ListVector::getDataVectorSize(inputVector);
     if (!listLambdaBindData->lambdaParamEvaluators.empty()) {
+        auto listSize = ListVector::getDataVectorSize(inputVector);
         auto lambdaParamVector = listLambdaBindData->lambdaParamEvaluators[0]->resultVector.get();
         lambdaParamVector->state->getSelVectorUnsafe().setSelSize(listSize);
     }
+    auto filterVector = input[1].get();
+    bool isConstantTrueExpr = listLambdaBindData->lambdaParamEvaluators.empty() &&
+                              filterVector->getValue<bool>(filterVector->state->getSelVector()[0]);
     listLambdaBindData->rootEvaluator->evaluate();
     KU_ASSERT(input.size() == 2);
     auto& listInputSelVector = input[0]->state->getSelVector();
-    auto filterVector = input[1].get();
     uint64_t numSelectedValues;
     for (auto i = 0u; i < listInputSelVector.getSelSize(); ++i) {
         numSelectedValues = 0;
         auto srcListEntry = inputVector->getValue<list_entry_t>(listInputSelVector[i]);
         for (auto j = 0u; j < srcListEntry.size; j++) {
             auto pos = srcListEntry.offset + j;
-            if (filterVector->getValue<bool>(pos)) {
+            if (isConstantTrueExpr || filterVector->getValue<bool>(pos)) {
                 numSelectedValues++;
             }
         }
