22217f00153047d68a8d963d174ab806d426048a
Chang Liu <liuc223@gmail.com>
2024-09-03 19:52:37 -0400

    X

--- tools/java_api/src/test/java/com/kuzudb/test/ExtensionTest.java
+++ tools/java_api/src/test/java/com/kuzudb/test/ExtensionTest.java
@@ -16,7 +16,7 @@ public class ExtensionTest extends TestBase {
         Path path = Path.of(currentDir, "../../extension/httpfs/build/libhttpfs.kuzu_extension");
         Path absPath = path.normalize().toAbsolutePath();
         String absPathStr = absPath.toString();
-        absPathStr = absPathStr.replaceAll("\\", "/");
+        absPathStr = absPathStr.replace("\\", "/");
         result = conn.query("LOAD EXTENSION " + "\"" + absPath + "\"");
         
         assertTrue(result.isSuccess());
