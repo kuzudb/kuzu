018c37a02cec86a82dccea22d2ff6c92981346ec
sterling <jiashuo.shi@uwaterloo.ca>
2024-09-06 19:55:11 -0400

    fix code style for class

--- src/include/processor/operator/persistent/reader/csv/driver.h
+++ src/include/processor/operator/persistent/reader/csv/driver.h
@@ -15,11 +15,6 @@ namespace processor {
 // TODO(Keenan): Split up this file.
 class BaseCSVReader;
 class ParsingDriver {
-    common::DataChunk& chunk;
-
-protected:
-    bool rowEmpty;
-
 public:
     explicit ParsingDriver(common::DataChunk& chunk);
     virtual ~ParsingDriver() = default;
@@ -31,39 +26,43 @@ public:
 private:
     virtual bool doneEarly() = 0;
     virtual BaseCSVReader* getReader() = 0;
+
+protected:
+    bool rowEmpty;
+
+private:
+    common::DataChunk& chunk;
 };
 
 class ParallelCSVReader;
 
 class ParallelParsingDriver : public ParsingDriver {
-    ParallelCSVReader* reader;
-
 public:
     ParallelParsingDriver(common::DataChunk& chunk, ParallelCSVReader* reader);
     bool doneEarly() override;
 
 private:
     BaseCSVReader* getReader() override;
+
+private:
+    ParallelCSVReader* reader;
 };
 
 class SerialCSVReader;
 
 class SerialParsingDriver : public ParsingDriver {
-
-protected:
-    SerialCSVReader* reader;
-
 public:
     SerialParsingDriver(common::DataChunk& chunk, SerialCSVReader* reader);
-
     bool doneEarly() override;
 
 private:
     BaseCSVReader* getReader() override;
+
+protected:
+    SerialCSVReader* reader;
 };
 
 class SniffCSVNameAndTypeDriver : public SerialParsingDriver {
-
 public:
     SniffCSVNameAndTypeDriver(SerialCSVReader* reader,
         const function::ScanTableFuncBindInput* bindInput);
@@ -71,6 +70,7 @@ public:
     bool done(uint64_t rowNum) const;
     bool addValue(uint64_t rowNum, common::column_id_t columnIdx, std::string_view value);
 
+private:
     std::vector<std::pair<std::string, common::LogicalType>> columns;
     std::vector<bool> sniffType;
     // if the type isn't declared in the header, sniff it
