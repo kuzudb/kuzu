76b5f7711022c92214539e2ee528f10db4a56d3e
Chang Liu <liuc223@gmail.com>
2024-09-10 13:34:40 -0400

    x

--- tools/python_api/test/test_extension.py
+++ tools/python_api/test/test_extension.py
@@ -4,6 +4,7 @@ import platform
 import urllib.request
 import os
 from pathlib import Path
+import hashlib
 
 import pytest
 from type_aliases import ConnDB
@@ -12,21 +13,27 @@ EXTENSION_CMAKE_PREFIX = 'add_definitions(-DKUZU_EXTENSION_VERSION="'
 
 
 @pytest.fixture()
-def expected_extension_dir_prefix() -> str:
+def extension_extension_dir_prefix() -> str:
     system = platform.system()
-    expected_extension_dir_prefix = None
+    extension_extension_dir_prefix = None
     if system == "Windows":
-        expected_extension_dir_prefix = "win_amd64"
+        extension_extension_dir_prefix = "win_amd64"
     elif system == "Linux":
-        expected_extension_dir_prefix = (
-            "linux_arm64" if platform.machine() == "aarch64" or platform.machine() == "arm64" else "linux_amd64"
+        extension_extension_dir_prefix = (
+            "linux_arm64"
+            if platform.machine() == "aarch64" or platform.machine() == "arm64"
+            else "linux_amd64"
         )
     elif system == "Darwin":
-        expected_extension_dir_prefix = "osx_arm64" if platform.machine() == "arm64" else "osx_amd64"
-    return expected_extension_dir_prefix
+        extension_extension_dir_prefix = (
+            "osx_arm64" if platform.machine() == "arm64" else "osx_amd64"
+        )
+    return extension_extension_dir_prefix
 
 
-def test_extension_install_httpfs(conn_db_readonly: ConnDB, tmpdir: str, expected_extension_dir_prefix: str) -> None:
+def test_extension_install_httpfs(
+    conn_db_readonly: ConnDB, tmpdir: str, extension_extension_dir_prefix: str
+) -> None:
     current_dir = Path(__file__).resolve().parent
     cmake_list_file = Path(current_dir).parent.parent.parent / "CMakeLists.txt"
     extension_version = None
@@ -36,24 +43,39 @@ def test_extension_install_httpfs(conn_db_readonly: ConnDB, tmpdir: str, expecte
                 extension_version = line.split(EXTENSION_CMAKE_PREFIX)[1].split('"')[0]
                 break
     userdir = os.path.expanduser("~")  # noqa: PTH111
-    expected_path = (
+    extension_path = (
         Path(userdir)
-        .joinpath(".kuzu", "extension", extension_version, expected_extension_dir_prefix, "httpfs", "libhttpfs.kuzu_extension")
+        .joinpath(
+            ".kuzu",
+            "extension",
+            extension_version,
+            extension_extension_dir_prefix,
+            "httpfs",
+            "libhttpfs.kuzu_extension",
+        )
         .resolve()
     )
+    if Path.exists(extension_path):
+        os.remove(extension_path)
     opener = urllib.request.build_opener()
     opener.addheaders = [("User-agent", "Kùzu Test Suite")]
     urllib.request.install_opener(opener)
-    expected_download_url = f"http://extension.kuzudb.com/v{extension_version}/{expected_extension_dir_prefix}/httpfs/libhttpfs.kuzu_extension"
-    urllib.request.urlretrieve(expected_download_url, Path(tmpdir) / "libhttpfs.kuzu_extension")
+    download_url = f"http://extension.kuzudb.com/v{extension_version}/{extension_extension_dir_prefix}/httpfs/libhttpfs.kuzu_extension"
+    temp_path = Path(tmpdir) / "libhttpfs.kuzu_extension"
+    urllib.request.urlretrieve(download_url, temp_path)
 
     conn, _ = conn_db_readonly
     conn.execute("INSTALL httpfs")
 
-    assert Path.exists(expected_path)
+    assert Path.exists(extension_path)
 
-    with Path.open(expected_path, "rb") as f:
-        expected_content = f.read()
-    with Path.open(Path(tmpdir) / "libhttpfs.kuzu_extension", "rb") as f:
-        actual_content = f.read()
-    assert expected_content == actual_content
+    extension_size = Path(extension_path).stat().st_size
+    expected_size = Path(temp_path).stat().st_size
+
+    with Path.open(extension_path, "rb") as f:
+        extension_md5 = hashlib.md5(f.read()).hexdigest()
+    with Path.open(temp_path, "rb") as f:
+        expected_md5 = hashlib.md5(f.read()).hexdigest()
+
+    assert extension_size == expected_size
+    assert extension_md5 == expected_md5
