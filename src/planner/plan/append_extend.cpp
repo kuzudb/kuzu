e64bb2611e092cd845b6803c609e7bb6cde285c9
Guodong Jin <guod.jin@gmail.com>
2024-09-15 20:17:10 -0400

    fix

--- src/planner/plan/append_extend.cpp
+++ src/planner/plan/append_extend.cpp
@@ -6,7 +6,6 @@
 #include "catalog/catalog.h"
 #include "catalog/catalog_entry/rel_table_catalog_entry.h"
 #include "common/enums/join_type.h"
-#include "common/exception/runtime.h"
 #include "planner/join_order/cost_model.h"
 #include "planner/operator/extend/logical_extend.h"
 #include "planner/operator/extend/logical_recursive_extend.h"
