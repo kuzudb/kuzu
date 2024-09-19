31fdc9c2816772b18dab21e3a70bef559cf6d1c3
howe <howe.wang0030@gmail.com>
2024-09-19 17:18:16 -0400

    Removing include

--- src/include/processor/operator/partitioner.h
+++ src/include/processor/operator/partitioner.h
@@ -2,7 +2,6 @@
 
 #include "common/enums/column_evaluate_type.h"
 #include "expression_evaluator/expression_evaluator.h"
-#include "processor/operator/persistent/rel_batch_insert_progress_state.h"
 #include "processor/operator/sink.h"
 #include "storage/store/in_mem_chunked_node_group_collection.h"
 
