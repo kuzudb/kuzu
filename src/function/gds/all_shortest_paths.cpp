7a977af104834a676dfbc52e9ad71bead73f7faf
xiyang <x74feng@uwaterloo.ca>
2024-09-24 15:32:42 +0800

    Add remove RJInputType

--- src/function/gds/all_shortest_paths.cpp
+++ src/function/gds/all_shortest_paths.cpp
@@ -5,6 +5,7 @@
 #include "main/client_context.h"
 #include "processor/execution_context.h"
 #include "processor/result/factorized_table.h"
+#include "binder/expression/expression_util.h"
 
 using namespace kuzu::processor;
 using namespace kuzu::common;
@@ -547,10 +548,11 @@ struct AllSPPathsEdgeCompute : public EdgeCompute {
  * of paths to each destination.
  */
 
-class AllSPDestinationsAlgorithm final : public RJAlgorithm {
+class AllSPDestinationsAlgorithm final : public SPAlgorithm {
 public:
     AllSPDestinationsAlgorithm() = default;
-    AllSPDestinationsAlgorithm(const AllSPDestinationsAlgorithm& other) : RJAlgorithm{other} {}
+    AllSPDestinationsAlgorithm(const AllSPDestinationsAlgorithm& other)
+        : SPAlgorithm{other} {}
 
     expression_vector getResultColumns(Binder*) const override { return getNodeIDResultColumns(); }
 
@@ -574,10 +576,10 @@ private:
     }
 };
 
-class AllSPLengthsAlgorithm final : public RJAlgorithm {
+class AllSPLengthsAlgorithm final : public SPAlgorithm {
 public:
     AllSPLengthsAlgorithm() = default;
-    AllSPLengthsAlgorithm(const AllSPLengthsAlgorithm& other) : RJAlgorithm{other} {}
+    AllSPLengthsAlgorithm(const AllSPLengthsAlgorithm& other) : SPAlgorithm{other} {}
 
     expression_vector getResultColumns(Binder* binder) const override {
         auto columns = getNodeIDResultColumns();
@@ -605,10 +607,10 @@ private:
     }
 };
 
-class AllSPPathsAlgorithm final : public RJAlgorithm {
+class AllSPPathsAlgorithm final : public SPAlgorithm {
 public:
     AllSPPathsAlgorithm() = default;
-    AllSPPathsAlgorithm(const AllSPPathsAlgorithm& other) : RJAlgorithm{other} {}
+    AllSPPathsAlgorithm(const AllSPPathsAlgorithm& other) : SPAlgorithm{other} {}
 
     expression_vector getResultColumns(Binder* binder) const override {
         auto columns = getNodeIDResultColumns();
@@ -672,10 +674,34 @@ struct VarLenJoinsEdgeCompute : public EdgeCompute {
  */
 class VarLenJoinsAlgorithm final : public RJAlgorithm {
 public:
-    explicit VarLenJoinsAlgorithm() : RJAlgorithm(RJInputType::HAS_LOWER_BOUND) {}
+    VarLenJoinsAlgorithm() = default;
     VarLenJoinsAlgorithm(const VarLenJoinsAlgorithm& other) : RJAlgorithm(other) {}
 
-    binder::expression_vector getResultColumns(binder::Binder* binder) const override {
+    /*
+     * Inputs include the following:
+     *
+     * graph::ANY
+     * srcNode::NODE
+     * lowerBound::INT64
+     * upperBound::INT64
+     * outputProperty::BOOL
+     */
+    std::vector<LogicalTypeID> getParameterTypeIDs() const override {
+        return {LogicalTypeID::ANY, LogicalTypeID::NODE, LogicalTypeID::INT64, LogicalTypeID::INT64, LogicalTypeID::BOOL};
+    }
+
+    void bind(const expression_vector &params, Binder *binder, graph::GraphEntry &graphEntry) override {
+        auto nodeInput = params[1];
+        auto nodeOutput = bindNodeOutput(binder, graphEntry);
+        auto lowerBound = ExpressionUtil::getLiteralValue<int64_t>(*params[2]);
+        auto upperBound = ExpressionUtil::getLiteralValue<int64_t>(*params[3]);
+        validateLowerUpperBound(lowerBound, upperBound);
+        auto outputProperty = ExpressionUtil::getLiteralValue<bool>(*params[4]);
+        bindData = std::make_unique<RJBindData>(nodeInput, nodeOutput, outputProperty, lowerBound, upperBound);
+    }
+
+
+    binder::expression_vector getResultColumns(binder::Binder *binder) const override {
         auto columns = getNodeIDResultColumns();
         columns.push_back(getLengthColumn(binder));
         columns.push_back(getPathNodeIDsColumn(binder));
