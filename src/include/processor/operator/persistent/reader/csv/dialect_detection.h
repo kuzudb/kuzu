91eba87d61915978ce3966629b3dd8b06967b823
sterling <jiashuo.shi@uwaterloo.ca>
2024-10-01 12:39:13 -0400

    fixed dialect option

--- src/include/processor/operator/persistent/reader/csv/dialect_detection.h
+++ src/include/processor/operator/persistent/reader/csv/dialect_detection.h
@@ -14,9 +14,9 @@ struct DialectOption {
 };
 
 struct DialectCandidates {
-    constexpr std::array delimiters = CopyConstants::DEFAULT_CSV_DELIMITER_SEARCH_SPACE;
-    constexpr std::array quoteChars = CopyConstants::DEFAULT_CSV_QUOTE_SEARCH_SPACE;
-    constexpr std::array escapeChars = CopyConstants::DEFAULT_CSV_ESCAPE_SEARCH_SPACE;
+    static constexpr std::array delimiters = CopyConstants::DEFAULT_CSV_DELIMITER_SEARCH_SPACE;
+    static constexpr std::array quoteChars = CopyConstants::DEFAULT_CSV_QUOTE_SEARCH_SPACE;
+    static constexpr std::array escapeChars = CopyConstants::DEFAULT_CSV_ESCAPE_SEARCH_SPACE;
 };
 
 std::vector<DialectOption> generateDialectOptions(const DialectCandidates& candidates,
