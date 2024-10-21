f50092baefe6492835b0289ca9c49793d1aeb566
MSebanc <mattiassebanc@gmail.com>
2024-10-20 17:26:33 -0700

    Added function names to cli autocomplete

--- tools/shell/embedded_shell.cpp
+++ tools/shell/embedded_shell.cpp
@@ -76,6 +76,8 @@ std::vector<std::string> nodeTableNames;
 std::vector<std::string> relTableNames;
 std::unordered_map<std::string, std::vector<std::string>> tableColumnNames;
 
+std::vector<std::string> functionNames;
+
 bool continueLine = false;
 std::string currLine;
 std::string historyLine;
@@ -105,6 +107,14 @@ void EmbeddedShell::updateTableNames() {
             columnNames.push_back(column.getName());
         }
         tableColumnNames[tableEntry->getName()] = columnNames;
+    }
+}
+
+void EmbeddedShell::updateFunctionNames() {
+    functionNames.clear();
+    auto function = database->catalog->getFunctions(&transaction::DUMMY_TRANSACTION);
+    for (auto& [_, entry] : function->getEntries(&transaction::DUMMY_TRANSACTION)) {
+        functionNames.push_back(entry->getName());
     }
 }
 
@@ -173,6 +183,26 @@ void findTableVariableNames(const std::string buf, std::regex tableRegex,
         std::smatch match = *i;
         tempTableNames.push_back(match[1].str());
         foundTableNames.push_back(match[2].str());
+    }
+}
+
+void keywordCompletion(std::string buf, std::string prefix, std::string keyword, linenoiseCompletions* lc) {
+    std::string bufEscaped = regex_replace(buf, specialChars, R"(\$&)");
+    if (regex_search(keyword, std::regex("^" + bufEscaped, std::regex_constants::icase))) {
+        std::string transformedKeyword = keyword;
+        for (size_t i = 0; i < buf.size() && i < keyword.size(); ++i) {
+            if (islower(buf[i])) {
+                transformedKeyword[i] = tolower(keyword[i]);
+            } else if (isupper(buf[i])) {
+                transformedKeyword[i] = toupper(keyword[i]);
+            }
+        }
+        // make the rest of the keyword the same case as the last character of buf
+        auto caseTransform = islower(buf.back()) ? ::tolower : ::toupper;
+        std::transform(keyword.begin() + buf.size(), keyword.end(),
+            transformedKeyword.begin() + buf.size(), caseTransform);
+
+        linenoiseAddCompletion(lc, (prefix + transformedKeyword).c_str());
     }
 }
 
@@ -253,23 +283,11 @@ void completion(const char* buffer, linenoiseCompletions* lc) {
     }
 
     for (std::string keyword : keywordList) {
-        std::string bufEscaped = regex_replace(buf, specialChars, R"(\$&)");
-        if (regex_search(keyword, std::regex("^" + bufEscaped, std::regex_constants::icase))) {
-            std::string transformedKeyword = keyword;
-            for (size_t i = 0; i < buf.size() && i < keyword.size(); ++i) {
-                if (islower(buf[i])) {
-                    transformedKeyword[i] = tolower(keyword[i]);
-                } else if (isupper(buf[i])) {
-                    transformedKeyword[i] = toupper(keyword[i]);
-                }
-            }
-            // make the rest of the keyword the same case as the last character of buf
-            auto caseTransform = islower(buf.back()) ? ::tolower : ::toupper;
-            std::transform(keyword.begin() + buf.size(), keyword.end(),
-                transformedKeyword.begin() + buf.size(), caseTransform);
+        keywordCompletion(buf, prefix, keyword, lc);
+    }
 
-            linenoiseAddCompletion(lc, (prefix + transformedKeyword).c_str());
-        }
+    for (std::string function : functionNames) {
+        keywordCompletion(buf, prefix, function, lc);
     }
 }
 
@@ -361,6 +379,7 @@ EmbeddedShell::EmbeddedShell(std::shared_ptr<Database> database, std::shared_ptr
     drawingCharacters = std::move(shellConfig.drawingCharacters);
     stats = shellConfig.stats;
     updateTableNames();
+    updateFunctionNames();
     KU_ASSERT(signal(SIGINT, interruptHandler) != SIG_ERR);
 }
 
