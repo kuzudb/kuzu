3a36afbbff51aa46fbd069c3fc97690402626886
howe <howe.wang0030@gmail.com>
2024-10-21 15:29:47 -0400

    adding case switch of SERIAL

--- third_party/parquet/parquet_types.cpp
+++ third_party/parquet/parquet_types.cpp
@@ -113,6 +113,9 @@ std::ostream &operator<<(std::ostream &out, const ConvertedType::type &val) {
 	case ConvertedType::INTERVAL:
 		out << "INTERVAL";
 		return out;
+  case ConvertedType::SERIAL:
+    out << "SERIAL";
+    return out;
 	case ConvertedType::NULL_TYPE:
 		out << "NULL";
 		return out;
