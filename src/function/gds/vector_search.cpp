b2f9149d4078fff50927e75f38c11908fa0c6f8d
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-10-28 04:43:39 -0400

    Some more changes

--- src/function/gds/vector_search.cpp
+++ src/function/gds/vector_search.cpp
@@ -611,7 +611,7 @@ namespace kuzu {
                         return;
                     }
 
-                    if (selectivity > 0.5) {
+                    if (selectivity > 0.3) {
                         filteredSearch(context, query, nodeTableId, graph, quantizedDc.get(), filterMask, *state.get(),
                                        entrypoint,
                                        entrypointDist, results, visited.get(), header, efSearch);
