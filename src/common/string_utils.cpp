11dbb65522285006cfd9f99b8131b88c07da47a6
Sterling Shi <156466823+SterlingT3485@users.noreply.github.com>
2024-11-06 16:50:39 -0500

    Fix duplicate quote char in JSON CASTING (#4473)
    * not finished
    
    * not tested yet
    
    * unfinished
    
    * use stringTOJSON in jsonify
    
    * remove comment
    
    * remove duplicate include
    
    * remove unused function
    
    * remove unused function
    
    * add test
    
    * fix test
    
    * fix the string to strcut cast
    
    * add test from duckdb for extract JSON
    
    * ported more test from duckdb
    
    * change 0u to 0
    
    * Run clang-format
    
    ---------
    
    Co-authored-by: CI Bot <SterlingT3485@users.noreply.github.com>

--- src/common/string_utils.cpp
+++ src/common/string_utils.cpp
@@ -50,7 +50,7 @@ std::vector<std::string_view> StringUtils::smartSplit(std::string_view input, ch
     for (auto i = 0u; i < input.size(); i++) {
         char c = input[i];
 
-        if (c == '\'' && (stk.size() == 0u || stk.back() != '\'')) {
+        if (c == '\'' && (stk.size() == 0 || stk.back() != '\'')) {
             // Entering/Exiting a single-quoted block.
             insideSingleQuote = !insideSingleQuote;
         } else if (c == splitChar && stk.size() == 0u && !insideSingleQuote) {
@@ -61,9 +61,10 @@ std::vector<std::string_view> StringUtils::smartSplit(std::string_view input, ch
                 result.push_back(input.substr(currentItem, i - currentItem));
                 currentItem = i + 1;
             }
-        } else if (c == '{' || c == '(' || c == '[' || c == '\"') {
+        } else if (c == '{' || c == '(' || c == '[' ||
+                   (c == '\"' && (stk.size() == 0u || stk.back() != '\"'))) {
             stk.push_back(c);
-        } else if (stk.size() > 0u && openingBracket(c) == stk.back()) {
+        } else if (stk.size() > 0 && openingBracket(c) == stk.back()) {
             stk.pop_back();
         }
     }
