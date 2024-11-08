0909287a82d8f393b62a68b807c6ea6e2c39ca52
howe <howe.wang0030@gmail.com>
2024-11-07 21:37:00 -0500

    Adding Suggestion

--- src/function/gds/weakly_connected_components.cpp
+++ src/function/gds/weakly_connected_components.cpp
@@ -297,6 +297,8 @@ public:
         auto frontier = std::make_shared<WCCFrontier>(graph->getNumNodesMap(clientContext->getTx()), clientContext->getMemoryManager());
         auto frontierPair = std::make_unique<WCCFrontierPair>(frontier, totalNumNodes, clientContext->getMaxNumThreadForExec());
         auto edgeCompute = std::make_unique<WCCEdgeCompute>(frontierPair.get());
+        // GDS::Utils::RunUntilConvergence shouldn't explicitly call RJCompState since other algorithms can also use RunUntilConvergence.
+        // A solution could be to have a general CompState class which RJCompState derives from.
         auto computeState = RJCompState(std::move(frontierPair), std::move(edgeCompute), nullptr, nullptr);
         GDSUtils::runFrontiersUntilConvergence(context, computeState, graph, ExtendDirection::FWD, 10);
         auto vertexCompute = std::make_unique<WCCVertexCompute>(clientContext->getMemoryManager(), sharedState.get(), *frontier);
