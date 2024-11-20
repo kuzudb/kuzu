ea4aa91f5d294a07c2bf029976f8859752897871
Royi Luo <r34luo@uwaterloo.ca>
2024-11-20 14:11:49 -0500

    Fix hash index checkpoint rollback

--- test/copy/copy_test.cpp
+++ test/copy/copy_test.cpp
@@ -285,6 +285,139 @@ TEST_F(CopyTest, OutOfMemoryRecovery) {
     }
 }
 
+TEST_F(CopyTest, RelCopyBMExceptionRecovery) {
+    if (inMemMode) {
+        GTEST_SKIP();
+    }
+    resetDB(64 * 1024 * 1024);
+    conn->query("CREATE NODE TABLE account(ID INT64, PRIMARY KEY(ID))");
+    conn->query("CREATE REL TABLE follows(FROM account TO account);");
+    {
+        auto result = conn->query(common::stringFormat(
+            "COPY account FROM \"{}/dataset/snap/twitter/csv/twitter-nodes.csv\"",
+            KUZU_ROOT_DIRECTORY));
+        ASSERT_TRUE(result->isSuccess()) << result->toString();
+    }
+    for (int i = 0;; i++) {
+        ASSERT_LT(i, 20);
+
+        resetDBFlaky();
+        resetDBFlaky();
+        auto result = conn->query(common::stringFormat(
+            "COPY follows FROM '{}/dataset/snap/twitter/csv/twitter-edges.csv' (DELIM=' ')",
+            KUZU_ROOT_DIRECTORY));
+        if (!result->isSuccess()) {
+            ASSERT_EQ(result->getErrorMessage(), "Buffer manager exception: Unable to allocate "
+                                                 "memory! The buffer pool is full and no "
+                                                 "memory could be freed!");
+        } else {
+            // the copy shouldn't succeed first try
+            ASSERT_GT(i, 0);
+            break;
+        }
+    }
+    // Try opening then closing the database
+    resetDB(256 * 1024 * 1024);
+    // Try again with a larger buffer pool size
+    resetDB(256 * 1024 * 1024);
+    {
+        // If BM exception happens during checkpoint the values from the COPY actually persist in
+        // the table So we just test to see that there are >0 tuples in the table and no
+        // crashes/assertion failures occur
+        auto result = conn->query("MATCH (a:account)-[:follows]->(b:account) RETURN COUNT(*)");
+        ASSERT_TRUE(result->isSuccess()) << result->getErrorMessage();
+        ASSERT_TRUE(result->hasNext());
+        ASSERT_GT(result->getNext()->getValue(0)->getValue<int64_t>(), 0);
+    }
+}
+
+TEST_F(CopyTest, NodeCopyBMExceptionRecoverySameConnection) {
+    if (inMemMode) {
+        GTEST_SKIP();
+    }
+    static constexpr uint64_t dbSize = 64 * 1024 * 1024;
+    resetDB(dbSize);
+    conn->query("CREATE NODE TABLE account(ID INT64, PRIMARY KEY(ID))");
+    resetDBFlaky();
+    bool copyCommitted = false;
+    for (int i = 0;; i++) {
+        ASSERT_LT(i, 20);
+
+        const auto queryString =
+            copyCommitted ?
+                "CHECKPOINT" :
+                common::stringFormat(
+                    "COPY account FROM \"{}/dataset/snap/twitter/csv/twitter-nodes.csv\"",
+                    KUZU_ROOT_DIRECTORY);
+
+        auto result = conn->query(queryString);
+        if (!result->isSuccess()) {
+            if (result->getErrorMessage().starts_with(
+                    "Copy exception: Found duplicated primary key")) {
+                // previous BM exception was during checkpoint so the copy was already committed
+                copyCommitted = true;
+                continue;
+            }
+            ASSERT_EQ(result->getErrorMessage(), "Buffer manager exception: Unable to allocate "
+                                                 "memory! The buffer pool is full and no "
+                                                 "memory could be freed!");
+        } else {
+            // the copy shouldn't succeed first try
+            ASSERT_GT(i, 0);
+            break;
+        }
+    }
+
+    // Reopen the DB so no spurious errors occur during the query
+    resetDB(dbSize);
+    {
+        // Test that the table copied as expected after the query
+        auto result = conn->query("MATCH (a:account) RETURN COUNT(*)");
+        ASSERT_TRUE(result->isSuccess()) << result->getErrorMessage();
+        ASSERT_TRUE(result->hasNext());
+        ASSERT_EQ(result->getNext()->getValue(0)->getValue<int64_t>(), 81306);
+    }
+}
+
+TEST_F(CopyTest, NodeCopyBMExceptionRecoveryResetDB) {
+    if (inMemMode) {
+        GTEST_SKIP();
+    }
+    static constexpr uint64_t dbSize = 64 * 1024 * 1024;
+    resetDB(dbSize);
+    conn->query("CREATE NODE TABLE account(ID INT64, PRIMARY KEY(ID))");
+    for (int i = 0;; i++) {
+        ASSERT_LT(i, 20);
+
+        resetDBFlaky();
+        resetDBFlaky();
+        auto result = conn->query(common::stringFormat(
+            "COPY account FROM \"{}/dataset/snap/twitter/csv/twitter-nodes.csv\"",
+            KUZU_ROOT_DIRECTORY));
+        if (!result->isSuccess()) {
+            ASSERT_EQ(result->getErrorMessage(), "Buffer manager exception: Unable to allocate "
+                                                 "memory! The buffer pool is full and no "
+                                                 "memory could be freed!");
+        } else {
+            // the copy shouldn't succeed first try
+            ASSERT_GT(i, 0);
+            break;
+        }
+    }
+    // Try opening then closing the database
+    resetDB(dbSize);
+    // Try again with a larger buffer pool size
+    resetDB(dbSize);
+
+    {
+        // Test that the table copied as expected after the query
+        auto result = conn->query("MATCH (a:account) RETURN COUNT(*)");
+        ASSERT_TRUE(result->isSuccess()) << result->getErrorMessage();
+        ASSERT_TRUE(result->hasNext());
+        ASSERT_EQ(result->getNext()->getValue(0)->getValue<int64_t>(), 81306);
+    }
+}
+
 TEST_F(CopyTest, OutOfMemoryRecoveryDropTable) {
     if (inMemMode) {
         GTEST_SKIP();
