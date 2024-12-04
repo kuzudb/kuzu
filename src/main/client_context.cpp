bc076219ca59c9ebb27317b8c7fe9d7b91af4ab5
Royi Luo <r34luo@uwaterloo.ca>
2024-12-04 18:08:38 -0500

    Self-review

--- src/main/client_context.cpp
+++ src/main/client_context.cpp
@@ -512,10 +512,7 @@ std::unique_ptr<QueryResult> ClientContext::executeNoLock(PreparedStatement* pre
         }
     } catch (std::exception& e) {
         transactionContext->rollback();
-        getMemoryManager()->getBufferManager()->getSpillerOrSkip(
-            [](auto& spiller) { spiller.clearFile(); });
-        progressBar->endProgress(executionContext->queryID);
-        return queryResultWithError(e.what());
+        return handleFailedExecute(executionContext.get(), e);
     }
 
     if (autoCommitTriggered) {
@@ -527,10 +524,7 @@ std::unique_ptr<QueryResult> ClientContext::executeNoLock(PreparedStatement* pre
             KU_ASSERT(locks.size() == 2);
             this->transactionContext->rollbackCheckpoint(locks[0], locks[1]);
 
-            getMemoryManager()->getBufferManager()->getSpillerOrSkip(
-                [](auto& spiller) { spiller.clearFile(); });
-            progressBar->endProgress(executionContext->queryID);
-            return queryResultWithError(e.what());
+            return handleFailedExecute(executionContext.get(), e);
         }
     }
 
@@ -543,6 +537,14 @@ std::unique_ptr<QueryResult> ClientContext::executeNoLock(PreparedStatement* pre
     queryResult->initResultTableAndIterator(std::move(resultFT));
 
     return queryResult;
+}
+
+std::unique_ptr<QueryResult> ClientContext::handleFailedExecute(ExecutionContext* executionContext,
+    std::exception& e) {
+    getMemoryManager()->getBufferManager()->getSpillerOrSkip(
+        [](auto& spiller) { spiller.clearFile(); });
+    progressBar->endProgress(executionContext->queryID);
+    return queryResultWithError(e.what());
 }
 
 // If there is an active transaction in the context, we execute the function in current active
