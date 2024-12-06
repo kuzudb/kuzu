9e596d17860df1631b6ea64ac69514023c2a4ce1
Royi Luo <r34luo@uwaterloo.ca>
2024-12-06 17:47:07 -0500

    Add sanity tests for cardinality populating

---
+++ test/planner/cardinality_test.cpp
@@ -0,0 +1,123 @@
+#include "graph_test/graph_test.h"
+#include "planner/operator/logical_plan_util.h"
+#include "test_runner/test_runner.h"
+
+namespace kuzu {
+namespace testing {
+
+class CardinalityTest : public DBTest {
+public:
+    std::string getInputDir() override {
+        return TestHelper::appendKuzuRootPath("dataset/tinysnb/");
+    }
+
+    std::string getEncodedPlan(const std::string& query) {
+        return planner::LogicalPlanUtil::encodeJoin(*TestRunner::getLogicalPlan(query, *conn));
+    }
+    std::unique_ptr<planner::LogicalPlan> getRoot(const std::string& query) {
+        return TestRunner::getLogicalPlan(query, *conn);
+    }
+    std::pair<planner::LogicalOperator*, planner::LogicalOperator*> getSource(
+        planner::LogicalOperator* op, planner::LogicalOperator* parent = nullptr) {
+        if (op->getNumChildren() == 0) {
+            return {parent, op};
+        }
+        return getSource(op->getChild(0).get(), op);
+    }
+    planner::LogicalOperator* getOpWithType(planner::LogicalOperator* op,
+        planner::LogicalOperatorType type) {
+        if (op->getOperatorType() == type) {
+            return op;
+        }
+        if (op->getNumChildren() == 0) {
+            return nullptr;
+        }
+        return getOpWithType(op->getChild(0).get(), type);
+    }
+};
+
+TEST_F(CardinalityTest, TestOperators) {
+    // Filter
+    {
+        auto plan = getRoot("MATCH (p1: person) WHERE p1.gender=1 RETURN p1.ID");
+        auto [parent, source] = getSource(plan->getLastOperator().get());
+        ASSERT_EQ(planner::LogicalOperatorType::SCAN_NODE_TABLE, source->getOperatorType());
+        ASSERT_EQ(8, source->getCardinality());
+        ASSERT_EQ(planner::LogicalOperatorType::FILTER, parent->getOperatorType());
+        ASSERT_EQ(4, parent->getCardinality());
+    }
+
+    // Limit
+    {
+        auto plan = getRoot("MATCH (p1: person) RETURN p1.ID LIMIT 2");
+        ASSERT_EQ(planner::LogicalOperatorType::LIMIT, plan->getLastOperator()->getOperatorType());
+        ASSERT_EQ(2, plan->getLastOperator()->getCardinality());
+    }
+
+    // Aggregate
+    {
+        auto plan = getRoot("MATCH (p1: person) RETURN COUNT(*), MAX(p1.age)");
+        auto* aggregateOp =
+            getOpWithType(plan->getLastOperator().get(), planner::LogicalOperatorType::AGGREGATE);
+        ASSERT_NE(nullptr, aggregateOp);
+        ASSERT_EQ(2, aggregateOp->getCardinality());
+    }
+
+    // Cross Product
+    {
+        auto plan = getRoot("MATCH (p1: person), (p2: person) RETURN p1.ID, p2.ID");
+        auto* productOp = getOpWithType(plan->getLastOperator().get(),
+            planner::LogicalOperatorType::CROSS_PRODUCT);
+        ASSERT_NE(nullptr, productOp);
+        ASSERT_EQ(64, productOp->getCardinality());
+    }
+
+    // Extend + Hash Join
+    {
+        auto plan = getRoot("MATCH (p1: person)-[k:knows]->(p2: person) RETURN p1.ID, p2.ID");
+        auto* extendOp =
+            getOpWithType(plan->getLastOperator().get(), planner::LogicalOperatorType::EXTEND);
+        ASSERT_NE(nullptr, extendOp);
+        static constexpr auto numRelsInKnows = 14;
+        ASSERT_EQ(numRelsInKnows, extendOp->getCardinality());
+
+        auto* joinOp =
+            getOpWithType(plan->getLastOperator().get(), planner::LogicalOperatorType::HASH_JOIN);
+        ASSERT_NE(nullptr, joinOp);
+        ASSERT_GT(joinOp->getCardinality(), 0);
+    }
+
+    // Intersect + Flatten
+    {
+        auto plan = getRoot("MATCH (p1: person)-[k1:knows]->(p2: person)-[k2:knows]->(p3:person), "
+                            "(p1)-[k3:knows]->(p3) "
+                            "HINT ((p1 JOIN k1 JOIN p2) MULTI_JOIN k2 MULTI_JOIN k3) JOIN p3 "
+                            "RETURN p1.ID, p2.ID, p3.ID");
+        auto* intersect =
+            getOpWithType(plan->getLastOperator().get(), planner::LogicalOperatorType::INTERSECT);
+        ASSERT_NE(nullptr, intersect);
+        ASSERT_GT(intersect->getCardinality(), 0);
+
+        auto* flatten =
+            getOpWithType(plan->getLastOperator().get(), planner::LogicalOperatorType::FLATTEN);
+        ASSERT_NE(nullptr, intersect);
+        ASSERT_GT(flatten->getCardinality(), 0);
+    }
+}
+
+TEST_F(CardinalityTest, TestPopulatedAfterOptimizations) {
+    // Filter push down
+    auto plan = getRoot("MATCH (a:person)-[e]->(b) "
+                        "WHERE a.ID < 0 AND a.fName='Alice' "
+                        "RETURN a.gender;");
+    std::function<void(planner::LogicalOperator*)> checkFunc;
+    checkFunc = [&checkFunc](planner::LogicalOperator* op) {
+        EXPECT_GT(op->getCardinality(), 0);
+        for (uint i = 0; i < op->getNumChildren(); ++i) {
+            checkFunc(op->getChild(i).get());
+        }
+    };
+}
+
+} // namespace testing
+} // namespace kuzu
