93aca38e101352651e88defde30cf036b8ad8fbb
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-12-16 16:58:34 -0500

    Some more changes

--- src/include/processor/operator/mask.h
+++ src/include/processor/operator/mask.h
@@ -73,10 +73,9 @@ public:
     // Note: blindly update mask does not parallelize well, so we minimize write by first checking
     // if the mask is set to true (mask value is equal to the expected currentMaskValue) or not.
     inline void incrementMaskValue(common::offset_t offset, uint8_t currentMaskValue) {
-//        if (maskData->isMasked(offset, currentMaskValue)) {
-//
-//        }
-        maskData->setMask(offset, currentMaskValue + 1);
+        if (maskData->isMasked(offset, currentMaskValue)) {
+            maskData->setMask(offset, currentMaskValue + 1);
+        }
     }
 
     void incrementNumMaskedNodes(uint64_t delta) { maskData->incrementNumMaskedNodes(delta); }
