e4b7a7c0e3279ee8cb2eea200f7d8c4af92245c6
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-12-16 16:35:05 -0500

    Implement buffer manager pass through

--- tools/benchmark_vector/main.cpp
+++ tools/benchmark_vector/main.cpp
@@ -34,7 +34,7 @@ private:
     std::vector<std::string> tokens;
 };
 
-std::vector<std::string> readQueriesFromFile(const std::string &filePath) {
+std::vector<std::string> readQueriesFromFile(const std::string &filePath, int efSearch, int maxK) {
     std::ifstream inputFile(filePath);
     std::vector<std::string> queries;
 
@@ -45,7 +45,18 @@ std::vector<std::string> readQueriesFromFile(const std::string &filePath) {
 
     std::string line;
     while (std::getline(inputFile, line)) {
-        queries.push_back(line); // Push each line as a separate query
+        // Replace <efsearch> with the value of efSearch
+        size_t pos;
+        while ((pos = line.find("<efsearch>")) != std::string::npos) {
+            line.replace(pos, 10, std::to_string(efSearch));
+        }
+
+        // Replace <maxK> with the value of maxK
+        while ((pos = line.find("<maxK>")) != std::string::npos) {
+            line.replace(pos, 6, std::to_string(maxK));
+        }
+
+        queries.push_back(line); // Push the modified line as a query
     }
 
     inputFile.close();
@@ -66,71 +77,23 @@ int main(int argc, char **argv) {
     int warmupTimes = stoi(input.getCmdOption("-warmup"));
     int actualTimes = stoi(input.getCmdOption("-actual"));
     int k = stoi(input.getCmdOption("-k"));
-//    int runSelectivityQueries = stoi(input.getCmdOption("-runSelectivityQueries"));
+    int efSearch = stoi(input.getCmdOption("-efSearch"));
+    int maxK = stoi(input.getCmdOption("-maxK"));
 
-//    std::string initQueriesPath = basePath + "/kuzu_init.sql";
-//    std::string dataPath = basePath + "/kuzu_data.csv";
-//    std::string testQueriesPath = basePath + "/kuzu_queries.sql";
-//    std::string databasePath = basePath + "/kuzu_data";
-//    std::string gtPath = basePath + "/kuzu_gt.bin";
     std::vector<std::string> testQueries;
     testQueries.push_back(queryPath);
 
-//    std::string baseBenchPath = basePath + "/" + benchSuffix;
-
-//    if (!runSelectivityQueries) {
-//        testQueries = {testQueriesPath};
-//    } else {
-//        // selectivity queries file paths     selectivity = [0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.98, 0.99, 0.995]
-//        testQueries = {
-//                baseBenchPath + "/queries_90.00.sql",
-//                baseBenchPath + "/queries_75.00.sql",
-//                baseBenchPath + "/queries_50.00.sql",
-//                baseBenchPath + "/queries_25.00.sql",
-//                baseBenchPath + "/queries_10.00.sql",
-//                baseBenchPath + "/queries_5.00.sql",
-//                baseBenchPath + "/queries_2.00.sql",
-//                baseBenchPath + "/queries_1.00.sql"
-////                baseBenchPath + "/queries_0.50.sql"
-//        };
-//    }
-
-//    if (isCreateSchema) {
-//        std::string command = "rm -rf " + databasePath;
-//        system(command.c_str());
-//    }
-
     auto db = Database(databasePath);
     auto conn = Connection(&db);
-//    if (isCreateSchema) {
-//        // read all the schema queries
-//        auto initQueries = readQueriesFromFile(initQueriesPath);
-//        for (int i = 0; i < initQueries.size(); i++) {
-//            printf("Running schema query: %s\n", initQueries[i].c_str());
-//            auto start = std::chrono::high_resolution_clock::now();
-//            auto res = conn.query(initQueries[i]);
-//            // print res
-//            while (res->hasNext()) {
-//                auto row = res->getNext();
-//                printf("%s\n", row->getValue(0)->getValue<std::string>().c_str());
-//            }
-//            auto end = std::chrono::high_resolution_clock::now();
-//            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-//            printf("Schema query time: %lld ms\n", duration);
-//        }
-//    }
 
     for (auto &queriesPath: testQueries) {
         printf("Running queries from: %s\n", queriesPath.c_str());
-        auto queries = readQueriesFromFile(queriesPath);
+        auto queries = readQueriesFromFile(queriesPath, efSearch, maxK);
         auto queryNumVectors = queries.size();
         vector_id_t *gtVecs = new vector_id_t[queryNumVectors * k];
         loadFromFile(gtPath, reinterpret_cast<uint8_t *>(gtVecs), queryNumVectors * k * sizeof(vector_id_t));
 
         // Randomly select warmupTimes queries from the queries
-//        std::random_device rd;
-//        std::mt19937 gen(rd());
-//        std::uniform_int_distribution<> dis(0, queries.size() - 1);
         for (int i = 0; i < warmupTimes; i++) {
             printf("Warmup started %d\n", i);
             auto start = std::chrono::high_resolution_clock::now();
@@ -156,17 +119,18 @@ int main(int argc, char **argv) {
         auto totalQueries = 0;
         auto totalQueriesSkipped = 0;
         long totalDuration = 0;
-        for (int i = 0; i < queries.size(); i++) {
+        long compilationTime = 14.0;
+        for (auto i = 0; i < queries.size(); i++) {
             auto localRecall = 0;
             start = std::chrono::high_resolution_clock::now();
             auto res = conn.query(queries[i]);
             end = std::chrono::high_resolution_clock::now();
             duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-            totalDuration += duration;
-//            if (res->getNumTuples() < k) {
-//                totalQueriesSkipped += 1;
-//                continue;
-//            }
+            totalDuration += (duration - compilationTime);
+            if (res->getNumTuples() < k) {
+                totalQueriesSkipped += 1;
+                continue;
+            }
             assert(res->getNumTuples() >= k);
             auto gt = gtVecs + i * k;
             while (res->hasNext()) {
@@ -178,7 +142,7 @@ int main(int argc, char **argv) {
                 }
             }
             printf("Recall for query %d: %d\n", i, localRecall);
-            printf("duration: %ld ms\n", duration);
+            printf("duration: %lld ms\n", duration);
             printf("====================================\n");
             totalQueries++;
         }
@@ -188,6 +152,9 @@ int main(int argc, char **argv) {
         printf("Avg latency: %f ms\n", (double) totalDuration / totalQueries);
         printf("Total queries skipped: %d\n", totalQueriesSkipped);
         printf("Recall: %f\n", (double) recall / (queryNumVectors * k));
+
+        // release memory
+        delete[] gtVecs;
     }
     return 0;
 }
