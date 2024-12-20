8f4d024f02ca8a945a211aa0abc6eda797d993a9
Royi Luo <r34luo@uwaterloo.ca>
2024-12-20 14:32:11 -0500

    Add skip+limit config for scan from pandas

---
+++ tools/python_api/src_cpp/include/py_scan_config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "common/case_insensitive_map.h"
+#include "common/types/value/value.h"
+
+namespace kuzu {
+
+struct PyScanConfig {
+    uint64_t skipNum;
+    uint64_t limitNum;
+    bool ignoreErrors;
+    explicit PyScanConfig(const common::case_insensitive_map_t<common::Value>& options);
+};
+
+} // namespace kuzu
