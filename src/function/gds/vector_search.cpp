6c5d3b0555d0150ea5ef2e2a9f42193b7ee9b507
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2024-12-30 20:46:00 -0500

    Some more changes

--- src/function/gds/vector_search.cpp
+++ src/function/gds/vector_search.cpp
@@ -593,7 +593,7 @@ namespace kuzu {
                 }
                 auto endTime = std::chrono::high_resolution_clock::now();
                 auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime);
-                printf("addFilteredNodesToCandidates: %f ms\n", (float)duration.count() / (1000000.0));
+                printf("addFilteredNodesToCandidates (%d, %d): %f ms\n", nbrsAdded, maxNodesToAdd, (float)duration.count() / (1000000.0));
             }
 
             void filteredSearch(processor::ExecutionContext *context, const float *query,
