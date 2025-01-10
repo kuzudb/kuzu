6c8970790abc79c4722818d17e73263200e786e1
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2025-01-10 17:52:19 -0500

    Some more changes

--- src/function/gds/vector_search.cpp
+++ src/function/gds/vector_search.cpp
@@ -647,33 +647,33 @@ namespace kuzu {
                     }
 
                     // Calculate local selectivity
-                    float localSelectivity = 0;
-                    for (auto &neighbor: firstHopNbrs) {
-                        if (filterMask->isMasked(neighbor.offset)) {
-                            localSelectivity += 1;
-                        }
-                    }
-                    localSelectivity /= filterNbrsToFind;
+//                    float localSelectivity = 0;
+//                    for (auto &neighbor: firstHopNbrs) {
+//                        if (filterMask->isMasked(neighbor.offset)) {
+//                            localSelectivity += 1;
+//                        }
+//                    }
+//                    localSelectivity /= filterNbrsToFind;
 
-                    if (localSelectivity >= 0.5) {
-                        // If the selectivity is high, we will simply do one hop search since we can find the next
-                        // closest directly from candidates priority queue.
-                        oneHopSearch(candidates, firstHopNbrs, dc, filterMask, results, visited, vectorArray, size,
-                                     efSearch, stats);
-                        stats.oneHopCalls->increase(1);
-                    } else if ((filterNbrsToFind * filterNbrsToFind * localSelectivity) > (filterNbrsToFind * 3)) {
-                        // We will use this metric to skip unwanted distance computation in the first hop
-                        dynamicTwoHopSearch(candidates, candidate, filterNbrsToFind, cachedNbrsCount,
-                                            firstHopNbrs, tableId, graph, dc, filterMask,
-                                            state, results, visited, vectorArray, size, efSearch, stats);
-                        stats.dynamicTwoHopCalls->increase(1);
-                    } else {
+//                    if (localSelectivity >= 0.5) {
+//                        // If the selectivity is high, we will simply do one hop search since we can find the next
+//                        // closest directly from candidates priority queue.
+//                        oneHopSearch(candidates, firstHopNbrs, dc, filterMask, results, visited, vectorArray, size,
+//                                     efSearch, stats);
+//                        stats.oneHopCalls->increase(1);
+//                    } else if ((filterNbrsToFind * filterNbrsToFind * localSelectivity) > (filterNbrsToFind * 3)) {
+//                        // We will use this metric to skip unwanted distance computation in the first hop
+//                        dynamicTwoHopSearch(candidates, candidate, filterNbrsToFind, cachedNbrsCount,
+//                                            firstHopNbrs, tableId, graph, dc, filterMask,
+//                                            state, results, visited, vectorArray, size, efSearch, stats);
+//                        stats.dynamicTwoHopCalls->increase(1);
+//                    } else {
                         // If the selectivity is low, we will not do dynamic two hop search since it does some extra
                         // distance computations to reduce listNbrs call which are redundant.
                         twoHopSearch(candidates, firstHopNbrs, tableId, graph, dc, filterMask, state, results, visited,
                                         vectorArray, size, efSearch, stats);
                         stats.twoHopCalls->increase(1);
-                    }
+//                    }
                     batchComputeDistance(vectorArray, size, dc, candidates, results, efSearch, stats);
                 }
             }
