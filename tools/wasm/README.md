3607b8ffeb21c605ae9d0348969a069d0444cde8
Chang Liu <ci@kuzudb.com>
2025-01-12 21:54:45 +0800

    x

---
+++ tools/wasm/README.md
@@ -0,0 +1,9 @@
+# kuzu-wasm
+[WIP] WebAssembly build of Kùzu in-process property graph database management system.
+
+## Installation
+```bash
+npm i kuzu-wasm
+```
+
+More information coming soon.
--- tools/wasm/build.mjs
+++ tools/wasm/build.mjs
@@ -6,6 +6,7 @@ import fs from 'fs'
 
 const SRC_PATH = path.resolve("..", "..");
 const THREADS = os.cpus().length;
+const KUZU_VERSION_TEXT = "Kuzu VERSION";
 const ES_BUILD_CONFIG = {
   entryPoints: ['./build/sync/index.js', './build/index.js', 'build/worker.js'],
   bundle: true,
@@ -42,9 +43,27 @@ const ES_BUILD_CONFIG_MULTI = JSON.parse(JSON.stringify(ES_BUILD_CONFIG));
 ES_BUILD_CONFIG_MULTI.outdir = 'package/multithreaded';
 await esbuild.build(ES_BUILD_CONFIG_MULTI);
 
+const CMakeListsTxt = await fs.promises.readFile(
+  path.join(SRC_PATH, "CMakeLists.txt"),
+  { encoding: "utf-8" }
+);
+
 console.log('Creating package.json...');
 const packageJsonText = await fs.promises.readFile(path.resolve(".", 'package.json'), 'utf-8');
 const packageJson = JSON.parse(packageJsonText);
+const lines = CMakeListsTxt.split("\n");
+for (const line of lines) {
+  if (line.includes(KUZU_VERSION_TEXT)) {
+    const versionSplit = line.split(" ")[2].trim().split(".");
+    let version = versionSplit.slice(0, 3).join(".");
+    if (versionSplit.length >= 4) {
+      version += "-dev." + versionSplit.slice(3).join(".");
+    }
+    console.log("Found version string from CMakeLists.txt: " + version);
+    packageJson.version = version;
+    break;
+  }
+}
 delete packageJson.scripts;
 delete packageJson.devDependencies;
 delete packageJson.dependencies;
@@ -54,5 +73,9 @@ console.log('Copying LICENSE...');
 await fs.promises.copyFile(path.resolve(SRC_PATH, 'LICENSE'), path.resolve(".", 'package', 'LICENSE'));
 
 console.log('Copying README.md...');
-await fs.promises.copyFile(path.resolve(SRC_PATH, 'README.md'), path.resolve(".", 'package', 'README.md'));
+await fs.promises.copyFile(path.resolve('.', 'README.md'), path.resolve(".", 'package', 'README.md'));
+
+console.log('Creating tarball...');
+execSync("tar -czf kuzu-wasm.tar.gz package", { cwd: path.resolve("."), stdio: "inherit" });
+
 console.log('All done!');
