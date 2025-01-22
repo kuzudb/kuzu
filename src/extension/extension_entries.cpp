88a02a0e3e74e8a2ce655a9ace0a81cc508913ea
Royi Luo <r34luo@uwaterloo.ca>
2025-01-22 17:16:41 -0500

    Fix segfault

--- src/extension/extension_entries.cpp
+++ src/extension/extension_entries.cpp
@@ -3,17 +3,16 @@
 
 namespace kuzu::extension {
 
-static constexpr size_t NUM_EXTENSION_FUNCTIONS = 27;
-static constexpr std::array<ExtensionEntry, NUM_EXTENSION_FUNCTIONS> extensionFunctions = {
-    {{"STEM", "FTS"}, {"QUERY_FTS_INDEX", "FTS"}, {"CREATE_FTS_INDEX", "FTS"},
-        {"DROP_FTS_INDEX", "FTS"}, {"TO_JSON", "JSON"}, {"JSON_QUOTE", "JSON"},
-        {"ARRAY_TO_JSON", "JSON"}, {"ROW_TO_JSON", "JSON"}, {"CAST_TO_JSON", "JSON"},
-        {"JSON_ARRAY", "JSON"}, {"JSON_OBJECT", "JSON"}, {"JSON_MERGE_PATCH", "JSON"},
-        {"COPY_JSON", "JSON"}, {"JSON_EXTRACT", "JSON"}, {"JSON_ARRAY_LENGTH", "JSON"},
-        {"JSON_CONTAINS", "JSON"}, {"JSON_KEYS", "JSON"}, {"JSON_STRUCTURE", "JSON"},
-        {"JSON_TYPE", "JSON"}, {"JSON_VALID", "JSON"}, {"JSON", "JSON"},
-        {"CLEAR_ATTACHED_DB_CACHE", "DUCKDB"}, {"DELTA_SCAN", "DELTA"}, {"ICEBERG_SCAN", "ICEBERG"},
-        {"ICEBERG_METADATA", "ICEBERG"}, {"ICEBERG_SNAPSHOTS", "ICEBERG"}}};
+static constexpr ExtensionEntry extensionFunctionsRaw[] = {{"STEM", "FTS"},
+    {"QUERY_FTS_INDEX", "FTS"}, {"CREATE_FTS_INDEX", "FTS"}, {"DROP_FTS_INDEX", "FTS"},
+    {"TO_JSON", "JSON"}, {"JSON_QUOTE", "JSON"}, {"ARRAY_TO_JSON", "JSON"}, {"ROW_TO_JSON", "JSON"},
+    {"CAST_TO_JSON", "JSON"}, {"JSON_ARRAY", "JSON"}, {"JSON_OBJECT", "JSON"},
+    {"JSON_MERGE_PATCH", "JSON"}, {"COPY_JSON", "JSON"}, {"JSON_EXTRACT", "JSON"},
+    {"JSON_ARRAY_LENGTH", "JSON"}, {"JSON_CONTAINS", "JSON"}, {"JSON_KEYS", "JSON"},
+    {"JSON_STRUCTURE", "JSON"}, {"JSON_TYPE", "JSON"}, {"JSON_VALID", "JSON"}, {"JSON", "JSON"},
+    {"CLEAR_ATTACHED_DB_CACHE", "DUCKDB"}, {"DELTA_SCAN", "DELTA"}, {"ICEBERG_SCAN", "ICEBERG"},
+    {"ICEBERG_METADATA", "ICEBERG"}, {"ICEBERG_SNAPSHOTS", "ICEBERG"}};
+static constexpr std::array extensionFunctions = std::to_array(extensionFunctionsRaw);
 
 static constexpr size_t NUM_EXTENSION_TYPES = 1;
 static constexpr std::array<ExtensionEntry, NUM_EXTENSION_TYPES> extensionTypes = {
