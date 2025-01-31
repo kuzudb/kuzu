5cda2114d3e51a40eb4963b6c40b507f02793fcb
Chang Liu <ci@kuzudb.com>
2025-01-31 19:12:36 +0800

    Remove unused LoggingLevel import from index.js

--- tools/nodejs_api/src_js/index.js
+++ tools/nodejs_api/src_js/index.js
@@ -2,7 +2,6 @@
 
 const Connection = require("./connection.js");
 const Database = require("./database.js");
-const LoggingLevel = require("./logging_level.js");
 const PreparedStatement = require("./prepared_statement.js");
 const QueryResult = require("./query_result.js");
 
