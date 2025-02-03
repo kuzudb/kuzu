7e504bb30eacb6ecc75793af42ab74e917a2ca61
Royi Luo <r34luo@uwaterloo.ca>
2025-02-03 16:43:20 -0500

    Add test token to disable test for vector size 2

--- test/test_runner/test_parser.cpp
+++ test/test_runner/test_parser.cpp
@@ -136,6 +136,12 @@ void TestParser::parseHeader() {
             }
             break;
         }
+        case TokenType::SKIP_VECTOR_SIZE_2: {
+            if constexpr (DEFAULT_VECTOR_CAPACITY == 2) {
+                testGroup->group = "DISABLED_" + testGroup->group;
+            }
+            break;
+        }
         case TokenType::SEPARATOR: {
             return;
         }
@@ -465,6 +471,12 @@ void TestParser::parseBody() {
 #ifdef __WASM__
             testCaseName = "DISABLED_" + testCaseName;
 #endif
+            break;
+        }
+        case TokenType::SKIP_VECTOR_SIZE_2: {
+            if constexpr (DEFAULT_VECTOR_CAPACITY == 2) {
+                testCaseName = "DISABLED_" + testCaseName;
+            }
             break;
         }
         case TokenType::SKIP_IN_MEM: {
