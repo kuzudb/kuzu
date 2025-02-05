21b32f081b38ce9551c9c9f5623c71cc2555b92b
Royi Luo <r34luo@uwaterloo.ca>
2025-02-04 19:01:29 -0500

    Set cardinality to previous operator

--- src/planner/plan/append_extend.cpp
+++ src/planner/plan/append_extend.cpp
@@ -94,6 +94,7 @@ void Planner::appendNonRecursiveExtend(const std::shared_ptr<NodeExpression>& bo
     // Update cost & cardinality. Note that extend does not change factorized cardinality.
     const auto extensionRate =
         cardinalityEstimator.getExtensionRate(*rel, *boundNode, clientContext->getTransaction());
+    extend->setCardinality(plan.getLastOperator()->getCardinality());
     plan.setCost(CostModel::computeExtendCost(plan));
     auto group = extend->getSchema()->getGroup(nbrNode->getInternalID());
     group->setMultiplier(extensionRate);
