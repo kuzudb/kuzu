f1404724cfdd32621ab54874de9ba9b2d7a4c7e8
Gaurav Sehgal <gaurav.sehgal8297@gmail.com>
2025-02-11 18:22:42 -0500

    some more changes

--- src/function/gds/vector_search.cpp
+++ src/function/gds/vector_search.cpp
@@ -714,13 +714,14 @@ namespace kuzu {
                     // Multiply by 0.6 due to the overlapping factor
                     auto estimatedFullTwoHopDistanceComp = (totalNbrs * filteredNbrs + filteredNbrs) * 0.4;
                     auto estimatedDirectedDistanceComp = totalNbrs + (totalNbrs - filteredNbrs);
-                    if (localSelectivity >= 0.5) {
-                        // If the selectivity is high, we will simply do one hop search since we can find the next
-                        // closest directly from candidates priority queue.
-                        oneHopSearch(candidates, firstHopNbrs, dc, filterMask, results, visited, vectorArray, size,
-                                     efSearch, stats);
-                        stats.oneHopCalls->increase(1);
-                    } else if (estimatedFullTwoHopDistanceComp > estimatedDirectedDistanceComp) {
+//                    if (localSelectivity >= 0.5) {
+//                        // If the selectivity is high, we will simply do one hop search since we can find the next
+//                        // closest directly from candidates priority queue.
+//                        oneHopSearch(candidates, firstHopNbrs, dc, filterMask, results, visited, vectorArray, size,
+//                                     efSearch, stats);
+//                        stats.oneHopCalls->increase(1);
+//                    } else
+                    if (estimatedFullTwoHopDistanceComp > estimatedDirectedDistanceComp) {
                         // We will use this metric to skip
                         // wanted distance computation in the first hop
                         directedTwoHopSearch(candidates, candidate, totalNbrs, cachedNbrsCount,
