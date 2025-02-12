9025128a63a28cc6a3681f9f5ffbd25ecc72bbc1
Royi Luo <r34luo@uwaterloo.ca>
2025-02-12 13:44:01 -0500

    Fix

--- src/common/file_system/local_file_system.cpp
+++ src/common/file_system/local_file_system.cpp
@@ -341,7 +341,8 @@ std::string LocalFileSystem::expandPath(main::ClientContext* context,
 
 bool LocalFileSystem::isLocalPath(const std::string& path) {
     return path.rfind("s3://", 0) != 0 && path.rfind("gs://", 0) != 0 &&
-           path.rfind("http://", 0) != 0 && path.rfind("https://", 0) != 0;
+           path.rfind("gcs://", 0) != 0 && path.rfind("http://", 0) != 0 &&
+           path.rfind("https://", 0) != 0;
 }
 
 void LocalFileSystem::readFromFile(FileInfo& fileInfo, void* buffer, uint64_t numBytes,
