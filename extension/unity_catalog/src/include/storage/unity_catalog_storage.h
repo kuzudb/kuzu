b3a61ea8f2616e6e8a48ba69c1326153bb6c5905
ziyi chen <chenziyi990424@gmail.com>
2025-02-14 16:47:08 -0500

    Implement unity catalog extension (#4890)

---
+++ extension/unity_catalog/src/include/storage/unity_catalog_storage.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "storage/storage_extension.h"
+
+namespace kuzu {
+namespace main {
+class Database;
+} // namespace main
+
+namespace unity_catalog_extension {
+
+class UnityCatalogStorageExtension final : public storage::StorageExtension {
+public:
+    static constexpr const char* DB_TYPE = "UC_CATALOG";
+
+    static constexpr const char* DEFAULT_SCHEMA_NAME = "default";
+
+    explicit UnityCatalogStorageExtension(main::Database* database);
+
+    bool canHandleDB(std::string dbType) const override;
+};
+
+} // namespace unity_catalog_extension
+} // namespace kuzu
