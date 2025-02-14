271c9957b505391e41206e5610d54be5fa8fbdfd
ziyi chen <chenziyi990424@gmail.com>
2025-02-14 00:59:16 -0500

    Fix `skipWhiteSpace()` function (#4907)

--- src/function/cast_from_string_functions.cpp
+++ src/function/cast_from_string_functions.cpp
@@ -172,7 +172,16 @@ void CastStringHelper::cast(const char* input, uint64_t len, ku_uuid_t& result,
 
 // ---------------------- cast String to nested types ------------------------------ //
 static void skipWhitespace(const char*& input, const char* end) {
-    while (input < end && isspace(*input)) {
+    while (input < end) {
+        if (*input & 0x80) {
+            // We only skip ASCII white spaces there.
+            break;
+        } else {
+            KU_ASSERT(*input >= -1);
+            if (!isspace(*input)) {
+                break;
+            }
+        }
         input++;
     }
 }
