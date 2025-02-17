ac3bbd0c253f853a2e23fff4d667fe33a9bcea39
Guodong Jin <guod.jin@gmail.com>
2025-02-17 15:52:05 +0800

    disable creating vector index in manual transactions

--- src/function/table/hnsw/create_hnsw_index.cpp
+++ src/function/table/hnsw/create_hnsw_index.cpp
@@ -23,8 +23,8 @@ namespace function {
 CreateHNSWSharedState::CreateHNSWSharedState(const CreateHNSWIndexBindData& bindData)
     : TableFuncSharedState{bindData.maxOffset}, name{bindData.indexName},
       nodeTable{bindData.context->getStorageManager()
-                    ->getTable(bindData.tableEntry->getTableID())
-                    ->cast<storage::NodeTable>()},
+              ->getTable(bindData.tableEntry->getTableID())
+              ->cast<storage::NodeTable>()},
       numNodes{bindData.numNodes}, bindData{&bindData} {
     hnswIndex = std::make_unique<storage::InMemHNSWIndex>(bindData.context, nodeTable,
         bindData.tableEntry->getColumnID(bindData.propertyID), bindData.config.copy());
@@ -32,7 +32,7 @@ CreateHNSWSharedState::CreateHNSWSharedState(const CreateHNSWIndexBindData& bind
         *bindData.context->getMemoryManager());
 }
 
-static std::unique_ptr<TableFuncBindData> bindFunc(main::ClientContext* context,
+static std::unique_ptr<TableFuncBindData> internalBindFunc(main::ClientContext* context,
     const TableFuncBindInput* input) {
     const auto indexName = input->getLiteralVal<std::string>(0);
     const auto tableName = input->getLiteralVal<std::string>(1);
@@ -48,6 +48,12 @@ static std::unique_ptr<TableFuncBindData> bindFunc(main::ClientContext* context,
     auto maxOffset = numNodes > 0 ? numNodes - 1 : 0;
     return std::make_unique<CreateHNSWIndexBindData>(context, indexName, tableEntry, propertyID,
         numNodes, maxOffset, std::move(config));
+}
+
+static std::unique_ptr<TableFuncBindData> bindFunc(main::ClientContext* context,
+    const TableFuncBindInput* input) {
+    storage::IndexUtils::validateAutoTransaction(*context, CreateHNSWIndexFunction::name);
+    return internalBindFunc(context, input);
 }
 
 static std::unique_ptr<TableFuncSharedState> initCreateHNSWSharedState(
@@ -235,7 +241,7 @@ function_set InternalCreateHNSWIndexFunction::getFunctionSet() {
     std::vector inputTypes = {LogicalTypeID::STRING, LogicalTypeID::STRING, LogicalTypeID::STRING};
     auto func = std::make_unique<TableFunction>(name, inputTypes);
     func->tableFunc = tableFunc;
-    func->bindFunc = bindFunc;
+    func->bindFunc = internalBindFunc;
     func->initSharedStateFunc = initCreateHNSWSharedState;
     func->initLocalStateFunc = initCreateHNSWLocalState;
     func->progressFunc = progressFunc;
