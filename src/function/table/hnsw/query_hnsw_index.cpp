c7c05905c919d642182e24c7abcb33f0ccf8bf77
ziyi chen <chenziyi990424@gmail.com>
2025-03-14 23:43:11 -0400

    Change hnsw input parameter types (#5042)
    * fix casting in hnsw

--- src/function/table/hnsw/query_hnsw_index.cpp
+++ src/function/table/hnsw/query_hnsw_index.cpp
@@ -1,10 +1,11 @@
 #include "binder/binder.h"
+#include "binder/expression/expression_util.h"
 #include "binder/expression/literal_expression.h"
-#include "binder/expression/parameter_expression.h"
 #include "binder/query/reading_clause/bound_table_function_call.h"
 #include "catalog/catalog_entry/hnsw_index_catalog_entry.h"
 #include "catalog/catalog_entry/node_table_catalog_entry.h"
 #include "common/types/value/nested.h"
+#include "expression_evaluator/expression_evaluator_utils.h"
 #include "function/table/bind_data.h"
 #include "function/table/hnsw/hnsw_index_functions.h"
 #include "planner/operator/logical_hash_join.h"
@@ -45,7 +46,7 @@ static std::vector<common::LogicalType> inferInputTypes(const binder::expression
     if (inputQueryExpression->expressionType == common::ExpressionType::LITERAL) {
         auto val = inputQueryExpression->constCast<binder::LiteralExpression>().getValue();
         inputTypes.push_back(
-            common::LogicalType::ARRAY(common::LogicalType::DOUBLE(), val.getChildrenSize()));
+            common::LogicalType::ARRAY(common::LogicalType::FLOAT(), val.getChildrenSize()));
     } else {
         inputTypes.push_back(common::LogicalType::ANY());
     }
@@ -83,81 +84,31 @@ static std::unique_ptr<TableFuncBindData> bindFunc(main::ClientContext* context,
         outputNode);
 }
 
-static common::PhysicalTypeID getChildQueryValType(const common::Value& value) {
-    auto childValType = common::PhysicalTypeID::ANY;
-    switch (value.getDataType().getPhysicalType()) {
-    case common::PhysicalTypeID::ARRAY: {
-        childValType = value.getDataType()
-                           .getExtraTypeInfo()
-                           ->constPtrCast<common::ArrayTypeInfo>()
-                           ->getChildType()
-                           .getPhysicalType();
-    } break;
-    case common::PhysicalTypeID::LIST: {
-        childValType = value.getDataType()
-                           .getExtraTypeInfo()
-                           ->constPtrCast<common::ListTypeInfo>()
-                           ->getChildType()
-                           .getPhysicalType();
-    } break;
-    default: {
-        throw common::RuntimeException(
-            common::stringFormat("Unsupported data type {} as a query vector in {}",
-                value.getDataType().toString(), QueryHNSWIndexFunction::name));
-    }
-    }
-    return childValType;
-}
-
-template<typename T>
-static void convertQueryVector(const common::Value& value, std::vector<float>& queryVector) {
-    auto numElements = value.getChildrenSize();
-    queryVector.resize(numElements);
-    for (auto i = 0u; i < queryVector.size(); i++) {
-        queryVector[i] = common::NestedVal::getChildVal(&value, i)->getValue<T>();
-    }
-}
-
-static common::Value getQueryValue(const binder::Expression& queryExpression) {
-    auto value = common::Value::createDefaultValue(queryExpression.dataType);
-    switch (queryExpression.expressionType) {
-    case common::ExpressionType::LITERAL: {
-        value = queryExpression.constCast<binder::LiteralExpression>().getValue();
-    } break;
-    case common::ExpressionType::PARAMETER: {
-        value = queryExpression.constCast<binder::ParameterExpression>().getValue();
-    } break;
-    default: {
-        throw common::RuntimeException(common::stringFormat("Unsupported expression type {} in {}",
-            common::ExpressionTypeUtil::toString(queryExpression.expressionType),
-            QueryHNSWIndexFunction::name));
-    }
-    }
-    return value;
-}
-
-static std::vector<float> getQueryVector(const binder::Expression& queryExpression,
-    uint64_t dimension) {
-    auto value = getQueryValue(queryExpression);
-    common::PhysicalTypeID childValType = getChildQueryValType(value);
+static std::vector<float> convertQueryVector(const common::Value& value) {
     std::vector<float> queryVector;
-    if (value.getChildrenSize() != dimension) {
-        throw common::RuntimeException("Query vector dimension does not match index dimension.");
-    }
-    switch (childValType) {
-    case common::PhysicalTypeID::FLOAT: {
-        convertQueryVector<float>(value, queryVector);
-    } break;
-    case common::PhysicalTypeID::DOUBLE: {
-        convertQueryVector<double>(value, queryVector);
-    } break;
-    default: {
-        throw common::RuntimeException(
-            common::stringFormat("Unsupported data type {} as a query vector in {}",
-                value.getDataType().toString(), QueryHNSWIndexFunction::name));
-    }
+    auto numElements = value.getChildrenSize();
+    queryVector.reserve(numElements);
+    for (auto i = 0u; i < numElements; i++) {
+        queryVector[i] = common::NestedVal::getChildVal(&value, i)->getValue<float>();
     }
     return queryVector;
+}
+
+static std::vector<float> getQueryVector(main::ClientContext* context,
+    const std::shared_ptr<binder::Expression> queryExpression, uint64_t dimension) {
+    std::shared_ptr<binder::Expression> literalExpression = queryExpression;
+    if (queryExpression->expressionType == common::ExpressionType::PARAMETER) {
+        literalExpression = std::make_shared<binder::LiteralExpression>(
+            binder::ExpressionUtil::evaluateAsLiteralValue(*queryExpression),
+            queryExpression->getUniqueName());
+    }
+    binder::Binder binder{context};
+    auto expr = binder.getExpressionBinder()->implicitCastIfNecessary(literalExpression,
+        common::LogicalType::ARRAY(common::LogicalType::FLOAT(), dimension));
+    auto value = evaluator::ExpressionEvaluatorUtils::evaluateConstantExpression(expr, context);
+    KU_ASSERT(
+        common::NestedVal::getChildVal(&value, 0)->getDataType() == common::LogicalType::FLOAT());
+    return convertQueryVector(value);
 }
 
 static const common::LogicalType& getIndexColumnType(const BoundQueryHNSWIndexInput& boundInput) {
@@ -182,7 +133,8 @@ static common::offset_t tableFunc(const TableFuncInput& input, TableFuncOutput& 
         index->setDefaultLowerEntryPoint(auxInfo.lowerEntryPoint);
         auto dimension =
             common::ArrayType::getNumElements(getIndexColumnType(bindData->boundInput));
-        auto queryVector = getQueryVector(*bindData->boundInput.queryExpression, dimension);
+        auto queryVector = getQueryVector(input.context->clientContext,
+            bindData->boundInput.queryExpression, dimension);
         localState->result = index->search(input.context->clientContext->getTransaction(),
             queryVector, bindData->boundInput.k, bindData->config, localState->visited,
             *localState->embeddingScanState.scanState);
