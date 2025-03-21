764e6050e5a27a0c0c591b589f1ce104ba05076f
Royi Luo <r34luo@uwaterloo.ca>
2025-03-21 16:30:26 -0400

    Add synchronous interrupt test

--- tools/python_api/test/test_connection.py
+++ tools/python_api/test/test_connection.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import threading
+import time
 from typing import TYPE_CHECKING
 
 import kuzu
 import pytest
+from type_aliases import ConnDB
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -27,3 +30,20 @@ def test_connection_close_context_manager(tmp_path: Path, build_dir: Path) -> No
         assert conn.is_closed
         pytest.raises(RuntimeError, conn.execute, "RETURN 1")
     assert db.is_closed
+
+
+def run_long_query(conn):
+    query = "UNWIND RANGE(1,1000000) AS x UNWIND RANGE(1, 1000000) AS y RETURN COUNT(x + y);"
+    with pytest.raises(RuntimeError) as excinfo:
+        conn.execute(query)
+    assert "Interrupted" in str(excinfo.value)
+
+
+def test_connection_interrupt(conn_db_readwrite: ConnDB) -> None:
+    conn, _ = conn_db_readwrite
+    execute_thread = threading.Thread(target=run_long_query, args=(conn,))
+    execute_thread.start()
+    time.sleep(5)
+    conn.interrupt()
+    execute_thread.join(timeout=100)
+    assert not execute_thread.is_alive()
