389e534e6ee7f652f98f77dac4a5f7c51aec0d6c
andyfeng <xiyangf1997@gmail.com>
2025-03-24 12:30:58 +0800

    Avoid project constant expression in WITH (#5099)
    * Avoid project constant expression in WITH
    
    * Run clang-format
    
    ---------
    
    Co-authored-by: CI Bot <andyfengHKU@users.noreply.github.com>

--- src/include/binder/query/return_with_clause/bound_with_clause.h
+++ src/include/binder/query/return_with_clause/bound_with_clause.h
@@ -10,11 +10,11 @@ public:
     explicit BoundWithClause(BoundProjectionBody projectionBody)
         : BoundReturnClause{std::move(projectionBody)} {}
 
-    inline void setWhereExpression(std::shared_ptr<Expression> expression) {
+    void setWhereExpression(std::shared_ptr<Expression> expression) {
         whereExpression = std::move(expression);
     }
-    inline bool hasWhereExpression() const { return whereExpression != nullptr; }
-    inline std::shared_ptr<Expression> getWhereExpression() const { return whereExpression; }
+    bool hasWhereExpression() const { return whereExpression != nullptr; }
+    std::shared_ptr<Expression> getWhereExpression() const { return whereExpression; }
 
 private:
     std::shared_ptr<Expression> whereExpression;
