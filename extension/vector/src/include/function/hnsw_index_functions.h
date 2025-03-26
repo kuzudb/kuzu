031fcfa4537d5c27b8da29d04016040281d9b5aa
Guodong Jin <guod.jin@gmail.com>
2025-03-26 03:11:08 -0400

    Rename the default return variable `_distance` to `distance` in QueryVectorIndex (#5109)
    * rename the default return variable _distance to distance in QueryVectorIndex
    
    * clean

--- extension/vector/src/include/function/hnsw_index_functions.h
+++ extension/vector/src/include/function/hnsw_index_functions.h
@@ -148,6 +148,9 @@ struct DropVectorIndexFunction final {
 struct QueryVectorIndexFunction final {
     static constexpr const char* name = "QUERY_VECTOR_INDEX";
 
+    static constexpr const char* nnColumnName = "nn";
+    static constexpr const char* distanceColumnName = "distance";
+
     static function::function_set getFunctionSet();
 };
 
