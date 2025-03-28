6e667475f209802608852b842dcebdfc1d2623b8
Siddhartha Sahu <dev@sdht.in>
2025-03-28 19:13:19 -0400

    gds: init louvain

--- src/function/function_collection.cpp
+++ src/function/function_collection.cpp
@@ -242,7 +242,8 @@ FunctionCollection* FunctionCollection::getFunctions() {
 
         // Algorithm functions
         TABLE_FUNCTION(WeaklyConnectedComponentsFunction), TABLE_FUNCTION(SCCKosarajuFunction),
-        TABLE_FUNCTION(SCCFunction), TABLE_FUNCTION(KCoreDecompositionFunction),
+        TABLE_FUNCTION(SCCFunction), TABLE_FUNCTION(LouvainFunction),
+        TABLE_FUNCTION(KCoreDecompositionFunction),
         TABLE_FUNCTION(PageRankFunction),
 
         // Export functions
