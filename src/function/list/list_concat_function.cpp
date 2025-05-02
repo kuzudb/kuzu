bc2de140645682bf2fbd4584b088b031eae6069e
ziyi chen <chenziyi990424@gmail.com>
2025-05-02 11:09:25 +0800

    fix-list-concat functions

--- src/function/list/list_concat_function.cpp
+++ src/function/list/list_concat_function.cpp
@@ -1,5 +1,6 @@
 #include "function/list/functions/list_concat_function.h"
 
+#include "binder/expression/expression_util.h"
 #include "common/exception/binder.h"
 #include "common/exception/message.h"
 #include "function/list/vector_list_functions.h"
@@ -28,13 +29,42 @@ void ListConcat::operation(common::list_entry_t& left, common::list_entry_t& rig
     }
 }
 
-std::unique_ptr<FunctionBindData> ListConcatFunction::bindFunc(const ScalarBindFuncInput& input) {
-    if (input.arguments[0]->getDataType() != input.arguments[1]->getDataType()) {
-        throw BinderException(ExceptionMessage::listFunctionIncompatibleChildrenType(name,
-            input.arguments[0]->getDataType().toString(),
-            input.arguments[1]->getDataType().toString()));
+static void resolveEmptyList(const ScalarBindFuncInput& input, std::vector<LogicalType>& types) {
+    auto isArg0Empty = binder::ExpressionUtil::isEmptyList(*input.arguments[0]);
+    auto isArg1Empty = binder::ExpressionUtil::isEmptyList(*input.arguments[1]);
+    if (isArg0Empty || isArg1Empty) {
+        auto& typeToUse = input.arguments[isArg0Empty ? 1 : 0]->getDataType();
+        types[0] = typeToUse.copy();
+        types[1] = typeToUse.copy();
     }
-    return FunctionBindData::getSimpleBindData(input.arguments, input.arguments[0]->getDataType());
+}
+
+static void resolveNullList(const ScalarBindFuncInput& input, std::vector<LogicalType>& types) {
+    auto isArg0AnyType = input.arguments[0]->dataType.getLogicalTypeID() == LogicalTypeID::ANY;
+    auto isArg1AnyType = input.arguments[1]->dataType.getLogicalTypeID() == LogicalTypeID::ANY;
+    LogicalType targetType;
+    if (isArg0AnyType && isArg1AnyType) {
+        targetType = LogicalType::LIST(LogicalType::INT64());
+    } else if (isArg0AnyType || isArg1AnyType) {
+        targetType = input.arguments[isArg0AnyType ? 1 : 0]->getDataType().copy();
+    } else {
+        return;
+    }
+    types[0] = targetType.copy();
+    types[1] = targetType.copy();
+}
+
+std::unique_ptr<FunctionBindData> ListConcatFunction::bindFunc(const ScalarBindFuncInput& input) {
+    std::vector<LogicalType> types;
+    types.push_back(input.arguments[0]->getDataType().copy());
+    types.push_back(input.arguments[1]->getDataType().copy());
+    resolveEmptyList(input, types);
+    resolveNullList(input, types);
+    if (types[0] != types[1]) {
+        throw BinderException(ExceptionMessage::listFunctionIncompatibleChildrenType(name,
+            types[0].toString(), types[1].toString()));
+    }
+    return std::make_unique<FunctionBindData>(std::move(types), types[0].copy());
 }
 
 function_set ListConcatFunction::getFunctionSet() {
