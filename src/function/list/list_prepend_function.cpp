acbf460303f5990f5835667b4e08c1fefb431e9c
tgahunia05 <tgahunia@uwaterloo.ca>
2025-05-06 08:58:00 -0400

    Fix list prepend

--- src/function/list/list_prepend_function.cpp
+++ src/function/list/list_prepend_function.cpp
@@ -1,8 +1,10 @@
 #include "common/exception/binder.h"
 #include "common/exception/message.h"
 #include "common/type_utils.h"
+#include "common/types/types.h"
 #include "function/list/vector_list_functions.h"
 #include "function/scalar_function.h"
+#include "binder/expression/expression_util.h"
 
 using namespace kuzu::common;
 
@@ -28,14 +30,24 @@ struct ListPrepend {
     }
 };
 
+static void resolveEmptyList(const ScalarBindFuncInput& input, std::vector<LogicalType>& types)
+{
+    auto isEmpty = binder::ExpressionUtil::isEmptyList(*input.arguments[0]);
+    if (isEmpty)
+    {
+        auto elementType = input.arguments[1]->getDataType().copy();
+
+        types[0] = LogicalType::LIST(elementType.copy()).copy();
+        types[1] = elementType.copy();
+    }
+}
+
+
 static void validateArgumentType(const binder::expression_vector& arguments) {
 
     if (arguments[0]->getChildren().empty())
         return;
-
-    if (arguments[1]->getChildren().empty())
-        return;
-
+    
     if (arguments[0]->getDataType().getLogicalTypeID() != LogicalTypeID::ANY &&
         arguments[1]->dataType != ListType::getChildType(arguments[0]->dataType)) {
         throw BinderException(ExceptionMessage::listFunctionIncompatibleChildrenType(
@@ -47,14 +59,29 @@ static void validateArgumentType(const binder::expression_vector& arguments) {
 
 static std::unique_ptr<FunctionBindData> bindFunc(const ScalarBindFuncInput& input) {
     validateArgumentType(input.arguments);
-    const auto& resultType = input.arguments[0]->getDataType();
+
+    std::vector<LogicalType> types;
+    types.push_back(input.arguments[0]->getDataType().copy());
+    types.push_back(input.arguments[1]->getDataType().copy());
+
+    resolveEmptyList(input, types);
+
+    if (types[0].getLogicalTypeID() != LogicalTypeID::ANY &&
+        types[1] != ListType::getChildType(types[0]))
+            throw BinderException(ExceptionMessage::listFunctionIncompatibleChildrenType
+            (
+                ListPrependFunction::name,
+                types[0].toString(),
+                types[1].toString()
+            ));
+
     auto scalarFunction = input.definition->ptrCast<ScalarFunction>();
-    TypeUtils::visit(input.arguments[1]->getDataType().getPhysicalType(),
+    TypeUtils::visit(types[1].getPhysicalType(),
         [&scalarFunction]<typename T>(T) {
             scalarFunction->execFunc = ScalarFunction::BinaryExecListStructFunction<list_entry_t, T,
                 list_entry_t, ListPrepend>;
         });
-    return FunctionBindData::getSimpleBindData(input.arguments, resultType.copy());
+    return FunctionBindData::getSimpleBindData(input.arguments, types[0]);
 }
 
 function_set ListPrependFunction::getFunctionSet() {
