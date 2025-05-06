e1056c595a453f7aec56783908e53a80d7e41c90
tgahunia05 <tgahunia@uwaterloo.ca>
2025-05-06 08:46:28 -0400

    fix list_append

--- src/function/list/list_append_function.cpp
+++ src/function/list/list_append_function.cpp
@@ -1,6 +1,9 @@
+#include "binder/expression/expression_util.h"
 #include "common/exception/binder.h"
 #include "common/exception/message.h"
 #include "common/type_utils.h"
+#include "common/types/types.h"
+#include "function/function.h"
 #include "function/list/vector_list_functions.h"
 #include "function/scalar_function.h"
 
@@ -28,12 +31,23 @@ struct ListAppend {
     }
 };
 
+
+static void resolveEmptyList(const ScalarBindFuncInput& input, std::vector<LogicalType>& types)
+{
+    auto isEmpty = binder::ExpressionUtil::isEmptyList(*input.arguments[0]);
+    if (isEmpty)
+    {
+        auto elementType = input.arguments[1]->getDataType().copy();
+
+        types[0] = LogicalType::LIST(elementType.copy()).copy();
+        types[1] = elementType.copy();
+    }
+}
+
+
 static void validateArgumentType(const binder::expression_vector& arguments) {
 
     if (arguments[0]->getChildren().empty())
-        return;
-
-    if (arguments[1]->getChildren().empty())
         return;
 
     if (ListType::getChildType(arguments[0]->dataType) != arguments[1]->getDataType()) {
@@ -45,13 +59,28 @@ static void validateArgumentType(const binder::expression_vector& arguments) {
 
 static std::unique_ptr<FunctionBindData> bindFunc(const ScalarBindFuncInput& input) {
     validateArgumentType(input.arguments);
+
+    std::vector<LogicalType> types;
+    types.push_back(input.arguments[0]->getDataType().copy());
+    types.push_back(input.arguments[1]->getDataType().copy());
+
+    resolveEmptyList(input, types);
+
+
+    if (ListType::getChildType(types[0]) != types[1])
+         throw BinderException(
+            ExceptionMessage::listFunctionIncompatibleChildrenType(ListAppendFunction::name,
+                input.arguments[0]->getDataType().toString(), input.arguments[1]->getDataType().toString()));
+
+
+
     auto scalarFunction = input.definition->ptrCast<ScalarFunction>();
     TypeUtils::visit(input.arguments[1]->getDataType().getPhysicalType(),
         [&scalarFunction]<typename T>(T) {
             scalarFunction->execFunc = ScalarFunction::BinaryExecListStructFunction<list_entry_t, T,
                 list_entry_t, ListAppend>;
         });
-    return FunctionBindData::getSimpleBindData(input.arguments, input.arguments[0]->getDataType());
+    return FunctionBindData::getSimpleBindData(input.arguments, types[0]);
 }
 
 function_set ListAppendFunction::getFunctionSet() {
