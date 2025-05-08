48a04d9aa8030a976b5456f802fba5e62d0d7400
ziyi chen <chenziyi990424@gmail.com>
2025-05-08 22:32:01 +0800

    null handling refactor

--- src/function/list/list_function_utils.cpp
+++ src/function/list/list_function_utils.cpp
@@ -23,28 +23,46 @@ void ListFunctionUtils::resolveEmptyList(const ScalarBindFuncInput& input,
     }
 }
 
-void ListFunctionUtils::resolveNulls(std::vector<common::LogicalType>& types) {
+void ListFunctionUtils::resolveNulls(std::vector<common::LogicalType>& types,
+    empty_type_resolver bothNull, empty_type_resolver leftNull, empty_type_resolver rightNull,
+    empty_type_resolver finalTypeResolver) {
     auto isArg0AnyType = types[0].getLogicalTypeID() == common::LogicalTypeID::ANY;
     auto isArg1AnyType = types[1].getLogicalTypeID() == common::LogicalTypeID::ANY;
 
     common::LogicalType targetType;
     if (isArg0AnyType && isArg1AnyType) {
-        targetType = common::LogicalType::INT64();
+        bothNull(types, targetType);
     } else if (isArg0AnyType) {
-        targetType = types[0].copy();
+        leftNull(types, targetType);
     } else if (isArg1AnyType) {
-        targetType = common::ListType::getChildType(types[1]).copy();
+        rightNull(types, targetType);
     } else {
         return;
     }
-    types[0] = common::LogicalType::LIST(targetType.copy());
-    types[1] = targetType.copy();
+    finalTypeResolver(types, targetType);
 }
 
 void ListFunctionUtils::checkTypes(const ScalarBindFuncInput& input,
     std::vector<common::LogicalType>& types, const char* name) {
     resolveEmptyList(input, types);
-    resolveNulls(types);
+    empty_type_resolver bothNullResolver = [](std::vector<common::LogicalType>& types,
+                                               common::LogicalType& targetType) {
+        targetType = common::LogicalType::INT64();
+    };
+    empty_type_resolver leftNullResolver = [](std::vector<common::LogicalType>& types,
+                                               common::LogicalType& targetType) {
+        targetType = types[0].copy();
+    };
+    empty_type_resolver rightNullResolver = [](std::vector<common::LogicalType>& types,
+                                                common::LogicalType& targetType) {
+        targetType = common::ListType::getChildType(types[1]).copy();
+    };
+    empty_type_resolver finalTypeResolver = [](std::vector<common::LogicalType>& types,
+                                                common::LogicalType& targetType) {
+        types[0] = common::LogicalType::LIST(targetType.copy());
+        types[1] = targetType.copy();
+    };
+    resolveNulls(types, bothNullResolver, leftNullResolver, rightNullResolver, finalTypeResolver);
 
     if (types[0].getLogicalTypeID() != LogicalTypeID::ANY &&
         types[1] != ListType::getChildType(types[0]))
--- src/include/function/list/functions/list_function_utils.h
+++ src/include/function/list/functions/list_function_utils.h
@@ -10,7 +10,13 @@ public:
     static void resolveEmptyList(const ScalarBindFuncInput& input,
         std::vector<common::LogicalType>& types);
 
-    static void resolveNulls(std::vector<common::LogicalType>& types);
+    using empty_type_resolver = std::function<void(std::vector<common::LogicalType>& types,
+        common::LogicalType& targetType)>;
+
+    static void resolveNulls(std::vector<common::LogicalType>& types, empty_type_resolver bothNull,
+        empty_type_resolver leftNull, empty_type_resolver rightNull,
+        empty_type_resolver finalTypeResolver);
+
     static void checkTypes(const ScalarBindFuncInput& input,
         std::vector<common::LogicalType>& types, const char* name);
 };
