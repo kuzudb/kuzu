522919002cb736887d5a9e69e795f488bb25e9f0
tgahunia05 <tgahunia@uwaterloo.ca>
2025-05-22 10:43:43 -0400

    work on export and import work for runner

--- test/test_runner/test_parser.cpp
+++ test/test_runner/test_parser.cpp
@@ -60,8 +60,16 @@ void TestParser::extractDataset() {
             testGroup->dataset = currentToken.params[2];
         }
     } else if (datasetType == "CSV") {
-        testGroup->datasetType = TestGroup::DatasetType::CSV;
-        testGroup->dataset = currentToken.params[2];
+        if (TestHelper::E2E_IMPORT_TEST_DIR != "dataset/")
+        {
+            testGroup->datasetType = TestGroup::DatasetType::CSV;
+            testGroup->dataset = currentToken.params[2];
+        }
+        else
+        {
+            testGroup->datasetType = TestGroup::DatasetType::KUZU;
+            testGroup->dataset = currentToken.params[2];
+        }
     } else if (datasetType == "NPY") {
         testGroup->datasetType = TestGroup::DatasetType::NPY;
         testGroup->dataset = currentToken.params[2];
