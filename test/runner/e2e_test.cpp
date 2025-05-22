10176fda84d185e52971d49e9b46d2fd1b999d42
tgahunia05 <tgahunia@uwaterloo.ca>
2025-05-22 15:58:33 -0400

    fix errors

--- test/runner/e2e_test.cpp
+++ test/runner/e2e_test.cpp
@@ -159,9 +159,7 @@ public:
                     return result;
                 };
 
-                // THIS CHECK SHOULD NEVER WORK
                 if (normalize(stmt) != (normalize("-STATEMENT " + statement->query))) {
-                    KU_UNREACHABLE;
                     newFile += currLine + '\n';
                     continue;
                 }
