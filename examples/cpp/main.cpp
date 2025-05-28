167e54be5372fcf6ceeefaa0e54f9dd94419facb
tgahunia05 <tgahunia@uwaterloo.ca>
2025-05-28 10:30:09 -0400

    revert

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -7,8 +7,14 @@ int main() {
     auto database = std::make_unique<Database>("" /* fill db path */);
     auto connection = std::make_unique<Connection>(database.get());
 
-    connection->query("load vector;");
-    auto result = connection->query("return create_embedding();");
+    // Create schema.
+    connection->query("CREATE NODE TABLE Person(name STRING, age INT64, PRIMARY KEY(name));");
+    // Create nodes.
+    connection->query("CREATE (:Person {name: 'Alice', age: 25});");
+    connection->query("CREATE (:Person {name: 'Bob', age: 30});");
+
+    // Execute a simple query.
+    auto result = connection->query("MATCH (a:Person) RETURN a.name AS NAME, a.age AS AGE;");
     // Print query result.
     std::cout << result->toString();
 }
