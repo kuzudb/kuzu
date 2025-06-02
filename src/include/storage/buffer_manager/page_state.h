1e35a26684d75e704ef86f17345fd8aac5c72704
囧囧 <liuc223@gmail.com>
2025-06-02 08:46:27 -0400

    Use malloc-based buffer manager for Swift (#5482)

--- src/include/storage/buffer_manager/page_state.h
+++ src/include/storage/buffer_manager/page_state.h
@@ -6,7 +6,12 @@
 
 // Alternative variant of the buffer manager which doesn't rely on MADV_DONTNEED (on Unix) for
 // evicting pages (which is unavailable in Webassembly runtimes)
-#define BM_MALLOC __WASM__
+#ifdef __WASM__
+#define BM_MALLOC true
+#endif
+#ifdef __SWIFT__
+#define BM_MALLOC true
+#endif
 #if BM_MALLOC
 #include <memory>
 #endif
