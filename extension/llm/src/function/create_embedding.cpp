8f81e303db72246447304acec7d58c4e7a68f009
tgahunia05 <tgahunia@uwaterloo.ca>
2025-06-04 09:12:44 -0400

    more cleanup

--- extension/llm/src/function/create_embedding.cpp
+++ extension/llm/src/function/create_embedding.cpp
@@ -377,8 +377,8 @@ static void execFunc(const std::vector<std::shared_ptr<common::ValueVector>>& pa
     common::ValueVector& result, common::SelectionVector* resultSelVector, void* /*dataPtr*/) {
     
     
-    auto provider = parameters[1]->getValue<ku_string_t>(0).getAsString();
-    auto model = parameters[2]->getValue<ku_string_t>(0).getAsString();
+    auto provider = StringUtils::getLower(parameters[1]->getValue<ku_string_t>(0).getAsString());
+    auto model = StringUtils::getLower(parameters[2]->getValue<ku_string_t>(0).getAsString());
     httplib::Client client(getClient(provider));
     client.set_connection_timeout(30, 0); 
     client.set_read_timeout(30, 0);      
