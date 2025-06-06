f00c85f57604c7ae7eef9c7e6bcb12645236bf65
tgahunia05 <tgahunia@uwaterloo.ca>
2025-06-06 10:50:06 -0400

    WIP

--- src/include/common/crypto.h
+++ src/include/common/crypto.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "common/api.h"
 
 namespace kuzu {
 namespace common {
@@ -8,13 +9,13 @@ namespace common {
 typedef unsigned char hash_bytes[32];
 typedef unsigned char hash_str[64];
 
-void sha256(const char* in, size_t inLen, hash_bytes& out);
+KUZU_API void sha256(const char* in, size_t inLen, hash_bytes& out);
 
-void hmac256(const std::string& message, const char* secret, size_t secretLen, hash_bytes& out);
+KUZU_API void hmac256(const std::string& message, const char* secret, size_t secretLen, hash_bytes& out);
 
-void hmac256(std::string message, hash_bytes secret, hash_bytes& out);
+KUZU_API void hmac256(std::string message, hash_bytes secret, hash_bytes& out);
 
-void hex256(hash_bytes& in, hash_str& out);
+KUZU_API void hex256(hash_bytes& in, hash_str& out);
 
 } // namespace common
 } // namespace kuzu
