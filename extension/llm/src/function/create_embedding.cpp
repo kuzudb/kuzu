6a4e9ce8b4544a4e273e925483371d278a7c96a3
tgahunia05 <tgahunia@uwaterloo.ca>
2025-06-09 12:17:21 -0400

    Remove redundancy

--- extension/llm/src/function/create_embedding.cpp
+++ extension/llm/src/function/create_embedding.cpp
@@ -47,9 +47,9 @@ static void execFunc(const std::vector<std::shared_ptr<common::ValueVector>>& pa
         getInstance(StringUtils::getLower(parameters[1]->getValue<ku_string_t>(0).getAsString()));
     auto model = StringUtils::getLower(parameters[2]->getValue<ku_string_t>(0).getAsString());
     httplib::Client client(provider.getClient());
-    client.set_connection_timeout(30, 0);
-    client.set_read_timeout(30, 0);
-    client.set_write_timeout(30, 0);
+    client.set_connection_timeout(30);
+    client.set_read_timeout(30);
+    client.set_write_timeout(30);
     std::string path = provider.getPath(model);
 
     result.resetAuxiliaryBuffer();
