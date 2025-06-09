244668323ba688063c28c330c02e568f496caee1
tgahunia05 <tgahunia@uwaterloo.ca>
2025-06-09 13:15:17 -0400

    Cleanup NL

--- extension/llm/src/providers/amazon-bedrock.cpp
+++ extension/llm/src/providers/amazon-bedrock.cpp
@@ -40,15 +40,12 @@ httplib::Headers BedrockEmbedding::getHeaders(const nlohmann::json& payload) con
         if (!envAWSAccessKey) {
             errMsg += envVarAWSAccessKey + '\n';
         }
-
         if (!envAWSSecretAccessKey) {
             errMsg += envVarAWSSecretAccessKey + '\n';
         }
         throw(RuntimeException(errMsg));
     }
-
     std::string service = "bedrock";
-
     // Hardcoded for now, needs to change when a configuration scheme is
     // supported
     std::string region = "us-east-1";
@@ -60,15 +57,16 @@ httplib::Headers BedrockEmbedding::getHeaders(const nlohmann::json& payload) con
 
     std::string canonicalUri = "/model/amazon.titan-embed-text-v1/invoke";
     std::string canonicalQueryString = "";
+
     httplib::Headers headers{{"host", host}, {"x-amz-date", datetimeHeader}};
     std::string canonicalHeaders;
     std::string signedHeaders;
-    for (const auto& h : headers) {
-        canonicalHeaders += h.first + ":" + h.second + "\n";
+    for (const auto& header : headers) {
+        canonicalHeaders += header.first + ":" + header.second + "\n";
         if (!signedHeaders.empty()) {
             signedHeaders += ";";
         }
-        signedHeaders += h.first;
+        signedHeaders += header.first;
     }
 
     std::string payloadStr = payload.dump();
@@ -76,7 +74,6 @@ httplib::Headers BedrockEmbedding::getHeaders(const nlohmann::json& payload) con
     hash_str payloadHashHex;
     CryptoUtils::sha256(payloadStr.c_str(), payloadStr.size(), payloadHashBytes);
     CryptoUtils::hex256(payloadHashBytes, payloadHashHex);
-
     std::ostringstream canonicalRequest;
     canonicalRequest << "POST\n"
                      << canonicalUri << "\n"
@@ -91,17 +88,14 @@ httplib::Headers BedrockEmbedding::getHeaders(const nlohmann::json& payload) con
     CryptoUtils::sha256(canonicalRequestStr.c_str(), canonicalRequestStr.size(),
         canonicalRequestHashBytes);
     CryptoUtils::hex256(canonicalRequestHashBytes, canonicalRequestHashHex);
-
     std::string algorithm = "AWS4-HMAC-SHA256";
     std::string credentialScope =
         std::string(dateHeader) + "/" + region + "/" + service + "/" + "aws4_request";
-
     std::ostringstream stringToSign;
     stringToSign << algorithm << "\n"
                  << datetimeHeader << "\n"
                  << credentialScope << "\n"
                  << std::string(reinterpret_cast<char*>(canonicalRequestHashHex), sizeof(hash_str));
-
     std::string stringToSignStr = stringToSign.str();
 
     hash_bytes kDate, kRegion, kService, kSigning;
@@ -110,10 +104,8 @@ httplib::Headers BedrockEmbedding::getHeaders(const nlohmann::json& payload) con
     CryptoUtils::hmac256(region, kDate, kRegion);
     CryptoUtils::hmac256(service, kRegion, kService);
     CryptoUtils::hmac256("aws4_request", kService, kSigning);
-
     hash_bytes signatureBytes;
     hash_str signatureHex;
-
     CryptoUtils::hmac256(stringToSignStr, kSigning, signatureBytes);
     CryptoUtils::hex256(signatureBytes, signatureHex);
     std::ostringstream authorizationHeader;
@@ -122,7 +114,6 @@ httplib::Headers BedrockEmbedding::getHeaders(const nlohmann::json& payload) con
                         << "Signature="
                         << std::string(reinterpret_cast<const char*>(signatureHex),
                                sizeof(hash_str));
-
     headers.insert({"Authorization", authorizationHeader.str()});
     return headers;
 }
