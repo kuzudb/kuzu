f0f9d50967d3183978d9849b87863e1eff5a765d
ziyi chen <chenziyi990424@gmail.com>
2025-06-11 13:33:04 +0800

    update

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -19,16 +19,22 @@ void createFTS(Connection* connection) {
     auto result =
         connection->query("CREATE NODE TABLE doc(docid int64, content string, primary key(docid));")
             ->isSuccess();
-    assert(result);
+    if (!result) {
+        throw "abc";
+    }
     result =
         connection
             ->query("copy doc from (load from '/Users/z473chen/Desktop/code/kuzu/doc_first.csv' "
                     "(file_format='csv', delim = '\\\\t') return * limit 100000)")
             ->isSuccess();
-    assert(result);
+    if (!result) {
+        throw "def";
+    }
     result =
         connection->query("CALL CREATE_FTS_INDEX('doc', 'contentIdx',['content']);")->isSuccess();
-    assert(result);
+    if (!result) {
+        throw "gh";
+    }
 }
 
 void insertFTS(Connection* connection) {
@@ -36,7 +42,7 @@ void insertFTS(Connection* connection) {
     auto result = connection->query(
         "load from \"/Users/z473chen/Desktop/code/kuzu/insert.csv\"(delim = '\\\\t') "
         "with column0 as "
-        "t0, column1 as t1 limit 10000 create (d:doc {docid: t0, content: t1})");
+        "t0, column1 as t1 limit 15000 create (d:doc {docid: t0, content: t1})");
     printf("%s", result->toString().c_str());
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> duration = end - start;
@@ -44,12 +50,12 @@ void insertFTS(Connection* connection) {
 }
 
 int main() {
-    //deleteDir();
+    deleteDir();
     auto database = std::make_unique<Database>("/tmp/bench" /* fill db path */);
     auto connection = std::make_unique<Connection>(database.get());
     connection->query(
         "load extension "
         "\"/Users/z473chen/Desktop/code/kuzu/extension/fts/build/libfts.kuzu_extension\"");
-    //createFTS(connection.get());
+    createFTS(connection.get());
     insertFTS(connection.get());
 }
