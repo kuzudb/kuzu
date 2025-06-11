97b1d9306aa0767ae66974256441ec2651117abf
Guodong Jin <guod.jin@gmail.com>
2025-06-11 14:54:46 -0400

    address comments

--- src/include/storage/table/table.h
+++ src/include/storage/table/table.h
@@ -85,7 +85,8 @@ struct KUZU_API TableScanState {
 
 struct KUZU_API TableInsertState {
     std::vector<common::ValueVector*> propertyVectors;
-    bool logToWAL = true;
+    // TODO(Guodong): Remove this when we have a better way to skip WAL logging for FTS.
+    bool logToWAL;
 
     explicit TableInsertState(std::vector<common::ValueVector*> propertyVectors);
     virtual ~TableInsertState();
@@ -103,7 +104,8 @@ struct KUZU_API TableInsertState {
 struct KUZU_API TableUpdateState {
     common::column_id_t columnID;
     common::ValueVector& propertyVector;
-    bool logToWAL = true;
+    // TODO(Guodong): Remove this when we have a better way to skip WAL logging for FTS.
+    bool logToWAL;
 
     TableUpdateState(common::column_id_t columnID, common::ValueVector& propertyVector);
     virtual ~TableUpdateState();
