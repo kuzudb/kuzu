7931631b39b6f45e54b95e26b2c149ea0808a052
ziyi chen <chenziyi990424@gmail.com>
2025-06-16 22:49:14 +0800

    update

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -8,18 +8,16 @@ int main() {
     std::filesystem::remove_all("adsadsad");
     auto database = std::make_unique<Database>("adsadsad" /* fill db path */);
     auto connection = std::make_unique<Connection>(database.get());
-    auto result =
-        connection->query("CREATE NODE TABLE doc (ID UINT64, content STRING, PRIMARY KEY (ID));");
-    if (!result) {
-        throw "Create table failed";
-    }
-    result = connection->query(
-        "COPY doc from \"/home/z473chen/kuzu/50.tsv\" (file_format='csv', delim = '\\\\t');");
-    if (!result) {
-        throw "Copy failed";
-    }
-    result = connection->query("CALL CREATE_FTS_INDEX('doc', 'contentIdx', ['content']);");
-    if (!result) {
-        throw "create fts failed";
-    }
+    printf("%s",
+        connection->query("CREATE NODE TABLE doc (ID UINT64, content STRING, PRIMARY KEY (ID));")
+            ->toString()
+            .c_str());
+    printf("%s", connection
+                     ->query("COPY doc from \"/home/z473chen/kuzu/50.tsv\" (file_format='csv', "
+                             "delim = '\\\\t');")
+                     ->toString()
+                     .c_str());
+    printf("%s", connection->query("CALL CREATE_FTS_INDEX('doc', 'contentIdx', ['content']);")
+                     ->toString()
+                     .c_str());
 }
