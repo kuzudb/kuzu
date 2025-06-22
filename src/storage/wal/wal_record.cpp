e175aaadeadb72c4db7954be762f9db6789ae094
xiyang <xiyangf1997@gmail.com>
2025-06-23 05:55:50 +0800

    Fix alter rel add from to recovery test

--- src/storage/wal/wal_record.cpp
+++ src/storage/wal/wal_record.cpp
@@ -233,6 +233,13 @@ static decltype(auto) deserializeAlterRecord(Deserializer& deserializer) {
         deserializer.deserializeValue(newName);
         extraInfo = std::make_unique<BoundExtraRenameTableInfo>(std::move(newName));
     } break;
+    case AlterType::ADD_FROM_TO_CONNECTION:{
+        table_id_t fromTableID = INVALID_TABLE_ID;
+        table_id_t toTableID = INVALID_TABLE_ID;
+        deserializer.deserializeValue(fromTableID);
+        deserializer.deserializeValue(toTableID);
+        extraInfo = std::make_unique<BoundExtraAddFromToConnection>(fromTableID, toTableID);
+    } break;
     default: {
         KU_UNREACHABLE;
     }
