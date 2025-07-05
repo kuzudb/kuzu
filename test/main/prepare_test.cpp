7a419e7d55ccab3b6bdcc983a878cd77ad52dbf1
Guodong Jin <guod.jin@gmail.com>
2025-07-05 00:29:16 -0400

    x

--- test/main/prepare_test.cpp
+++ test/main/prepare_test.cpp
@@ -240,7 +240,7 @@ TEST_F(ApiTest, PrepareExport) {
     if (databasePath == "" || databasePath == ":memory:") {
         return;
     }
-    auto newDBPath = TestHelper::getTempDir("export_db").string() + "/newdb";
+    auto newDBPath = TestHelper::getTempDir("export_db") + "/newdb";
     auto preparedStatement = conn->prepare("EXPORT DATABASE '" + newDBPath + '\'');
     auto result = conn->execute(preparedStatement.get());
     ASSERT_TRUE(result->isSuccess());
