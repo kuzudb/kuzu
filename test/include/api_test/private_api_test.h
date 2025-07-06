10373a098d3af14badf9a09f618fe05a04434de6
Guodong Jin <guod.jin@gmail.com>
2025-07-05 23:13:24 -0400

    more clean up

--- test/include/api_test/private_api_test.h
+++ test/include/api_test/private_api_test.h
@@ -16,14 +16,6 @@ public:
     std::string getInputDir() override {
         return TestHelper::appendKuzuRootPath("dataset/tinysnb/");
     }
-
-    static void assertMatchPersonCountStar(main::Connection* conn) {
-        auto result = conn->query("MATCH (a:person) RETURN COUNT(*)");
-        ASSERT_TRUE(result->hasNext());
-        auto tuple = result->getNext();
-        ASSERT_EQ(tuple->getValue(0)->getValue<int64_t>(), 8);
-        ASSERT_FALSE(result->hasNext());
-    }
 };
 
 } // namespace testing
