5efff8044cdeabba22b7ed43aab5f0f740fdcdf6
ziyi chen <chenziyi990424@gmail.com>
2025-07-08 16:54:51 +0800

    add more static link test for extension

--- extension/extension_config.cmake
+++ extension/extension_config.cmake
@@ -1,6 +1,6 @@
 set(EXTENSION_LIST azure delta duckdb fts httpfs iceberg json llm postgres sqlite unity_catalog vector neo4j algo)
 
-#set(EXTENSION_STATIC_LINK_LIST json)
+#set(EXTENSION_STATIC_LINK_LIST httpfs)
 string(JOIN ", " joined_extensions ${EXTENSION_STATIC_LINK_LIST})
 message(STATUS "Static link extensions: ${joined_extensions}")
 foreach(extension IN LISTS EXTENSION_STATIC_LINK_LIST)
