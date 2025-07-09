578aa8f589bf9c0b956a07e17a18c73def605ccd
tgahunia05 <tgahunia@uwaterloo.ca>
2025-07-09 11:26:23 -0400

    More tests

--- extension/llm/src/function/create_embedding.cpp
+++ extension/llm/src/function/create_embedding.cpp
@@ -129,23 +129,21 @@ static std::unique_ptr<FunctionBindData> bindFunc(const ScalarBindFuncInput& inp
         throw(BinderException(
             formatErrorMsg("MODEL", "STRING", input.arguments[2]->getDataType().toString())));
     }
-    auto providerName =
-        ExpressionUtil::evaluateLiteral<std::string>(*input.arguments[1], LogicalType::STRING());
+    auto providerName = ExpressionUtil::evaluateLiteral<std::string>(*input.arguments[1], LogicalType::STRING());
     auto provider = EmbeddingProviderFactory::getProvider(providerName);
-    auto modelName =
-        ExpressionUtil::evaluateLiteral<std::string>(*input.arguments[2], LogicalType::STRING());
+    auto modelName = ExpressionUtil::evaluateLiteral<std::string>(*input.arguments[2], LogicalType::STRING());
+    StringUtils::toLower(modelName);
     if (input.arguments.size() == 5) {
         if (input.arguments[3]->getDataType() != LogicalType::INT64()) {
             throw(BinderException(formatErrorMsg("DIMENSIONS", "INT64",
                 input.arguments[3]->getDataType().toString())));
         }
-        numConfig = ExpressionUtil::evaluateLiteral<int64_t>(*input.arguments[3],
-            LogicalType::INT64(), validate);
-
         if (input.arguments[4]->getDataType() != LogicalType::STRING()) {
             throw(BinderException(formatErrorMsg("REGION/ENDPOINT", "STRING",
-                input.arguments[4]->getDataType().toString())));
+                                                 input.arguments[4]->getDataType().toString())));
         }
+        numConfig = ExpressionUtil::evaluateLiteral<int64_t>(*input.arguments[3],
+            LogicalType::INT64(), validate);
         stringConfig = ExpressionUtil::evaluateLiteral<std::string>(*input.arguments[4],
             LogicalType::STRING());
     } else if (input.arguments.size() == 4) {
@@ -161,6 +159,10 @@ static std::unique_ptr<FunctionBindData> bindFunc(const ScalarBindFuncInput& inp
             numConfig = ExpressionUtil::evaluateLiteral<int64_t>(*input.arguments[3],
                 LogicalType::INT64(), validate);
         }
+    }
+    if (stringConfig.has_value())
+    {
+        StringUtils::toLower(stringConfig.value());
     }
 
     try {
