068de1e4ac5ba6d2aa5d8b50018b2815175e9882
tgahunia05 <tgahunia@uwaterloo.ca>
2025-07-14 13:39:30 -0400

    try

--- src/function/union/union_value_function.cpp
+++ src/function/union/union_value_function.cpp
@@ -7,12 +7,12 @@ namespace kuzu {
 namespace function {
 
 static std::unique_ptr<FunctionBindData> bindFunc(const ScalarBindFuncInput& input) {
-    KU_ASSERT(input.arguments.size() == 1);
+    KU_ASSERT(input.optionalArguments.size() == 1);
     std::vector<StructField> fields;
-    if (input.arguments[0]->getDataType().getLogicalTypeID() == common::LogicalTypeID::ANY) {
-        input.arguments[0]->cast(LogicalType::STRING());
+    if (input.optionalArguments[0]->getDataType().getLogicalTypeID() == common::LogicalTypeID::ANY) {
+        input.optionalArguments[0]->cast(LogicalType::STRING());
     }
-    fields.emplace_back(input.arguments[0]->getAlias(), input.arguments[0]->getDataType().copy());
+    fields.emplace_back(input.optionalArguments[0]->getAlias(), input.optionalArguments[0]->getDataType().copy());
     auto resultType = LogicalType::UNION(std::move(fields));
     return FunctionBindData::getSimpleBindData(input.arguments, resultType);
 }
@@ -35,7 +35,7 @@ static void valueCompileFunc(FunctionBindData* /*bindData*/,
 function_set UnionValueFunction::getFunctionSet() {
     function_set functionSet;
     auto function = std::make_unique<ScalarFunction>(name,
-        std::vector<LogicalTypeID>{LogicalTypeID::ANY}, LogicalTypeID::UNION, execFunc);
+        std::vector<LogicalTypeID>{}, LogicalTypeID::UNION, execFunc);
     function->bindFunc = bindFunc;
     function->compileFunc = valueCompileFunc;
     functionSet.push_back(std::move(function));
