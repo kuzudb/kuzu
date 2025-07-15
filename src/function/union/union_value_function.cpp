009f4c506eb548f61e4ddc79b5fe031d8cc7a02f
tgahunia05 <tgahunia@uwaterloo.ca>
2025-07-15 08:35:45 -0400

    wip

--- src/function/union/union_value_function.cpp
+++ src/function/union/union_value_function.cpp
@@ -19,28 +19,21 @@ static std::unique_ptr<FunctionBindData> bindFunc(const ScalarBindFuncInput& inp
     return FunctionBindData::getSimpleBindData(input.optionalArguments, resultType);
 }
 
-static void execFunc(const std::vector<std::shared_ptr<common::ValueVector>>&,
+static void execFunc(const std::vector<std::shared_ptr<common::ValueVector>>& parameters,
     const std::vector<common::SelectionVector*>&, common::ValueVector& result,
     common::SelectionVector* resultSelVector, void* /*dataPtr*/) {
+    result.setState(parameters[0]->state);
+    UnionVector::getTagVector(&result)->setState(parameters[0]->state);
+    UnionVector::referenceVector(&result, UnionType::TAG_FIELD_IDX, parameters[0]);
     UnionVector::setTagField(result, *resultSelVector, UnionType::TAG_FIELD_IDX);
 }
 
-// TODO(Tanvir) move this to execFunc
-static void valueCompileFunc(FunctionBindData* /*bindData*/,
-    const std::vector<std::shared_ptr<ValueVector>>& parameters,
-    std::shared_ptr<ValueVector>& result) {
-    KU_ASSERT(parameters.size() == 1);
-    result->setState(parameters[0]->state);
-    UnionVector::getTagVector(result.get())->setState(parameters[0]->state);
-    UnionVector::referenceVector(result.get(), UnionType::TAG_FIELD_IDX, parameters[0]);
-}
 
 function_set UnionValueFunction::getFunctionSet() {
     function_set functionSet;
     auto function = std::make_unique<ScalarFunction>(name, std::vector<LogicalTypeID>{},
         LogicalTypeID::UNION, execFunc);
     function->bindFunc = bindFunc;
-    function->compileFunc = valueCompileFunc;
     functionSet.push_back(std::move(function));
     return functionSet;
 }
