aed5fe4346618c73319ad25d4d8ab84bed34483f
tgahunia05 <tgahunia@uwaterloo.ca>
2025-07-15 12:23:43 -0400

    implement sort_internal_ids

--- src/include/processor/operator/simple/export_db.h
+++ src/include/processor/operator/simple/export_db.h
@@ -28,24 +28,25 @@ class ExportDB final : public SimpleSink {
     static constexpr PhysicalOperatorType type_ = PhysicalOperatorType::EXPORT_DATABASE;
 
 public:
-    ExportDB(common::FileScanInfo boundFileInfo, bool schemaOnly,
+    ExportDB(common::FileScanInfo boundFileInfo, bool schemaOnly, bool sortInternalIds,
         std::shared_ptr<FactorizedTable> messageTable, physical_op_id id,
         std::unique_ptr<OPPrintInfo> printInfo)
         : SimpleSink{type_, std::move(messageTable), id, std::move(printInfo)},
-          boundFileInfo{std::move(boundFileInfo)}, schemaOnly{schemaOnly} {}
+          boundFileInfo{std::move(boundFileInfo)}, schemaOnly{schemaOnly}, sortInternalIds{sortInternalIds} {}
 
     void initGlobalStateInternal(ExecutionContext* context) override;
 
     void executeInternal(ExecutionContext* context) override;
 
     std::unique_ptr<PhysicalOperator> copy() override {
-        return std::make_unique<ExportDB>(boundFileInfo.copy(), schemaOnly, messageTable, id,
+        return std::make_unique<ExportDB>(boundFileInfo.copy(), schemaOnly, sortInternalIds, messageTable, id,
             printInfo->copy());
     }
 
 private:
     common::FileScanInfo boundFileInfo;
     bool schemaOnly;
+    bool sortInternalIds;
 };
 } // namespace processor
 } // namespace kuzu
