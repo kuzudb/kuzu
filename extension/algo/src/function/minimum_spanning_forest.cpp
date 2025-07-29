24dd32d3892e23f99617b57d7cc9d670b315ac8c
tgahunia05 <tgahunia@uwaterloo.ca>
2025-07-29 08:12:11 -0400

    move structures into a state

--- extension/algo/src/function/minimum_spanning_forest.cpp
+++ extension/algo/src/function/minimum_spanning_forest.cpp
@@ -37,10 +37,8 @@ public:
     void beginOnTableInternal(table_id_t /*tableID*/) override {}
 
     void vertexCompute(const offset_t startOffset, const offset_t endOffset, const table_id_t tableID) override {
-        for(auto i = startOffset; i < endOffset; ++i)
-        {
-            for(auto j = 0u; j < finalResults[i].size(); ++j)
-            {
+        for(auto i = startOffset; i < endOffset; ++i) {
+            for(auto j = 0u; j < finalResults[i].size(); ++j) {
                 const auto nodeID = nodeID_t{i, tableID};
                 nodeIDVector->setValue<nodeID_t>(0, nodeID);
                 toIDVector->setValue<nodeID_t>(0, nodeID_t{finalResults[i][j].first, tableID});
@@ -80,10 +78,10 @@ struct MSFOptionalParams final : public GDSOptionalParams {
 
 MSFOptionalParams::MSFOptionalParams(const expression_vector& optionalParams)
 {
-    static constexpr const char* PROPERTY = "property";
+    static constexpr const char* WEIGHTPROPERTY = "weight_property";
     for (auto& optionalParam : optionalParams) {
         auto paramName = StringUtils::getLower(optionalParam->getAlias());
-        if (paramName == PROPERTY) {
+        if (paramName == WEIGHTPROPERTY) {
             weightProperty = optionalParam;
         } else {
             throw BinderException{"Unknown optional parameter: " + optionalParam->getAlias()};
@@ -93,8 +91,7 @@ MSFOptionalParams::MSFOptionalParams(const expression_vector& optionalParams)
 
 std::unique_ptr<GDSConfig> MSFOptionalParams::getConfig() const {
     auto config = std::make_unique<MSFConfig>();
-    if (weightProperty == nullptr)
-    {
+    if (weightProperty == nullptr) {
         throw RuntimeException("No parameter specifying the weight\n");
     }
     config->weight_property = ExpressionUtil::evaluateLiteral<std::string>(*weightProperty, LogicalType::STRING());
@@ -104,8 +101,7 @@ std::unique_ptr<GDSConfig> MSFOptionalParams::getConfig() const {
 
 static offset_t find(const offset_t& u, ku_vector_t<offset_t>& parents)
 {
-    while(parents[u] != parents[parents[u]])
-    {
+    while(parents[u] != parents[parents[u]]) {
         parents[u] = parents[parents[u]];
     }
     return parents[u];
@@ -114,8 +110,7 @@ static offset_t find(const offset_t& u, ku_vector_t<offset_t>& parents)
 static void mergeComponents(const offset_t& pu, const offset_t& pv, ku_vector_t<offset_t>& parents, ku_vector_t<uint64_t>& rank)
 {
     KU_ASSERT_UNCONDITIONAL(pu != pv);
-    if (rank[pu] == rank[pv])
-    {
+    if (rank[pu] == rank[pv]) {
         auto newParent = std::min(pu, pv);
         auto newChild = std::max(pu, pv);
         parents[newChild] = newParent;
@@ -128,6 +123,30 @@ static void mergeComponents(const offset_t& pu, const offset_t& pv, ku_vector_t<
         parents[pv] = pu;
     }
 }
+
+struct KruskalState {
+    ku_vector_t<std::tuple<offset_t, offset_t, int64_t>> edges;
+    ku_vector_t<offset_t> parents;
+    ku_vector_t<uint64_t> rank;
+
+
+    // Cannot represent 2d vector with ku_vector_t. MmAllocator' has been explicitly marked deleted here
+    // ku_vector_t<ku_vector_t<std::pair<offset_t, int64_t>>> forest(mm);
+    // for(int i = 0; i < numNodes; ++i)
+    //{
+    //  forest.emplace_back({mm});
+    //}
+    // This may not be needed.
+    std::vector<std::vector<std::pair<offset_t, int64_t>>> forest;
+    KruskalState(storage::MemoryManager* mm, uint64_t numNodes) : edges{mm}, parents{mm, numNodes}, rank{mm, numNodes}, forest{numNodes} {
+        // parents[i] = i;
+        std::iota(parents.begin(), parents.end(), 0);
+        for(auto& r : rank) {
+            r = 0;
+        }
+    }
+};
+
 
 static offset_t tableFunc(const TableFuncInput& input, TableFuncOutput&) {
     const auto clientContext = input.context->clientContext;
@@ -148,17 +167,9 @@ static offset_t tableFunc(const TableFuncInput& input, TableFuncOutput&) {
     const auto scanState = graph->prepareRelScan(*nbrInfo.relGroupEntry, nbrInfo.relTableID, nbrInfo.dstTableID, {config.weight_property});
     const auto numNodes = graph->getMaxOffset(clientContext->getTransaction(), tableId);
 
-    // init dsu related structures.
-    ku_vector_t<offset_t> parents(mm, numNodes);
-    std::iota(parents.begin(), parents.end(), 0);
-    ku_vector_t<uint64_t> rank(mm, numNodes);
-    for(auto& r : rank) {
-        r = 0;
-    }
-
+    KruskalState state(mm, numNodes);
 
     // collect graph
-    ku_vector_t<std::tuple<offset_t, offset_t, int64_t>> edges(mm);
     for (auto nodeId = 0u; nodeId < numNodes; ++nodeId) {
         const nodeID_t nextNodeId = {nodeId, tableId};
         for (auto chunk : graph->scanFwd(nextNodeId, *scanState)) {
@@ -168,45 +179,35 @@ static offset_t tableFunc(const TableFuncInput& input, TableFuncOutput&) {
                     return;
                 }
                 auto weight = propertyVectors[0]->template getValue<int64_t>(i);
-                edges.push_back({nodeId, nbrId, weight});
+                state.edges.push_back({nodeId, nbrId, weight});
             });
         }
     } 
 
-    static const auto& cmp = [&](const auto& e1, const auto& e2)
-        {
+    static const auto& cmp = [&](const auto& e1, const auto& e2) {
             const auto& [v1, u1, w1] = e1;
             const auto& [v2, u2, w2] = e2;
             return std::tie(w1, v1, u1) > std::tie(w2, v2, u2);
         };
 
 
-    std::make_heap(edges.begin(), edges.end(), cmp);
-    // Cannot be init as done below. MmAllocator' has been explicitly marked deleted here
-    // ku_vector_t<ku_vector_t<std::pair<offset_t, int64_t>>> forest(mm);
-    // for(int i = 0; i < numNodes; ++i)
-    //{
-    //  forest.emplace_back({mm});
-    //}
-    std::vector<std::vector<std::pair<offset_t, int64_t>>> forest(numNodes);
+    std::make_heap(state.edges.begin(), state.edges.end(), cmp);
     offset_t numEdges = 0;
-    while(!edges.empty() && numEdges != numNodes - 1)
-    {
-        std::pop_heap(edges.begin(), edges.end(), cmp);
-        const auto [u, v, w] = edges.back();
-        edges.pop_back();
-        auto pu = find(u, parents);
-        auto pv = find(v, parents);
-        if (pu != pv)
-        {
+    while(!state.edges.empty() && numEdges != numNodes - 1) {
+        std::pop_heap(state.edges.begin(), state.edges.end(), cmp);
+        const auto [u, v, w] = state.edges.back();
+        state.edges.pop_back();
+        auto pu = find(u, state.parents);
+        auto pv = find(v, state.parents);
+        if (pu != pv) {
             ++numEdges;
-            forest[u].push_back({v, w});
-            forest[v].push_back({u, w});
-            mergeComponents(pu, pv, parents, rank);
+            state.forest[u].push_back({v, w});
+            state.forest[v].push_back({u, w});
+            mergeComponents(pu, pv, state.parents, state.rank);
         }
     }
 
-    const auto vertexCompute = make_unique<WriteResultsMSF>(mm, sharedState, forest);
+    const auto vertexCompute = make_unique<WriteResultsMSF>(mm, sharedState, state.forest);
     GDSUtils::runVertexCompute(input.context, GDSDensityState::DENSE, graph, *vertexCompute);
 
     sharedState->factorizedTablePool.mergeLocalTables();
