1c89a8031743010b63a2f19a68ecd1729d20869c
Tanvir Gahunia <tgahunia@uwaterloo.ca>
2025-08-07 11:22:31 -0400

    Update extension/algo/src/function/minimum_spanning_forest.cpp
    Co-authored-by: Siddhartha Sahu <sid@kuzudb.com>

--- extension/algo/src/function/minimum_spanning_forest.cpp
+++ extension/algo/src/function/minimum_spanning_forest.cpp
@@ -147,6 +147,7 @@ private:
 KruskalState::KruskalState(storage::MemoryManager* mm, offset_t numNodes)
     : edges{mm}, parents{mm, static_cast<size_t>(numNodes)},
       rank{mm, static_cast<size_t>(numNodes)}, forest{mm} {
+    // Mark all vertices as belonging to their own components.
     std::iota(parents.begin(), parents.end(), 0);
 }
 
