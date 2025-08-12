31f06529928b12ce9e36b9d30535c100e0b940bb
tgahunia05 <tgahunia@uwaterloo.ca>
2025-08-12 11:45:16 -0400

    wip

--- src/include/function/gds/gds_object_manager.h
+++ src/include/function/gds/gds_object_manager.h
@@ -162,6 +162,14 @@ public:
 
     auto end() const { return vec.begin(); }
 
+    auto front() { return vec.front(); }
+
+    auto back() { return vec.back(); }
+
+    auto front() const { return vec.front(); }
+
+    auto back() const { return vec.back(); }
+
     template<typename... Args>
     void emplace_back(Args&&... args) {
         vec.emplace_back(std::forward<Args>(args)...);
