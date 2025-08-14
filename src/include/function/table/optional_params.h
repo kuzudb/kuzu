006e1633395e0daeb0bee3df26938c5038be5d6b
tgahunia05 <tgahunia@uwaterloo.ca>
2025-08-14 12:50:11 -0400

    clean

--- src/include/function/table/optional_params.h
+++ src/include/function/table/optional_params.h
@@ -30,12 +30,6 @@ struct LogicalTypeMapping<common::LogicalTypeID::STRING> {
     using type = std::string;
 };
 
-
-template<>
-struct LogicalTypeMapping<common::LogicalTypeID::STRING> {
-    using type = std::string;
-};
-
 template<typename PARAM>
 struct OptionalParam {
     using T = typename LogicalTypeMapping<PARAM::TYPE>::type;
