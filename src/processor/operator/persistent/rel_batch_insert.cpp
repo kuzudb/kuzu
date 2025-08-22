8a3cf7e44b270397e392969c43e3a976c2e901e2
05st <stimsina@protonmail.com>
2025-08-22 10:40:55 -0400

    Clean up debugging artifacts

--- src/processor/operator/persistent/rel_batch_insert.cpp
+++ src/processor/operator/persistent/rel_batch_insert.cpp
@@ -1,7 +1,5 @@
 #include "processor/operator/persistent/rel_batch_insert.h"
 
-#include <iostream>
-
 #include "catalog/catalog.h"
 #include "common/exception/copy.h"
 #include "common/exception/message.h"
