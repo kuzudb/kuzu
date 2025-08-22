45052be944747c85e8be07da3fa0769e4ced7cff
05st <stimsina@protonmail.com>
2025-08-22 10:15:32 -0400

    Fix partition counts

--- src/processor/operator/persistent/rel_batch_insert.cpp
+++ src/processor/operator/persistent/rel_batch_insert.cpp
@@ -14,6 +14,8 @@
 #include "storage/table/column_chunk_data.h"
 #include "storage/table/rel_table.h"
 
+#include <iostream>
+
 using namespace kuzu::catalog;
 using namespace kuzu::common;
 using namespace kuzu::storage;
@@ -105,11 +107,11 @@ void RelBatchInsert::executeInternal(ExecutionContext* context) {
     while (true) {
         relLocalState->nodeGroupIdx =
             partitionerSharedState->getNextPartition(relInfo->partitioningIdx);
-        ++progressSharedState->partitionsDone;
         if (relLocalState->nodeGroupIdx == INVALID_PARTITION_IDX) {
             // No more partitions left in the partitioning buffer.
             break;
         }
+        ++progressSharedState->partitionsDone;
         // TODO(Guodong): We need to handle the concurrency between COPY and other insertions
         // into the same node group.
         auto& nodeGroup = relTable
