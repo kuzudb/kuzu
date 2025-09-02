86b23517e85735891757ba3e7b07426ed30ed587
Benjamin Winger <bmw@disroot.org>
2025-09-02 13:55:55 -0500

    Flush based on the number of values instead of the capacity (#5889)

--- src/include/storage/table/column_chunk_metadata.h
+++ src/include/storage/table/column_chunk_metadata.h
@@ -40,8 +40,8 @@ public:
 
     GetCompressionMetadata(const GetCompressionMetadata& other) = default;
 
-    ColumnChunkMetadata operator()(std::span<const uint8_t> buffer, uint64_t capacity,
-        uint64_t numValues, StorageValue min, StorageValue max) const;
+    ColumnChunkMetadata operator()(std::span<const uint8_t> buffer, uint64_t numValues,
+        StorageValue min, StorageValue max) const;
 };
 
 class GetBitpackingMetadata {
@@ -54,8 +54,8 @@ public:
 
     GetBitpackingMetadata(const GetBitpackingMetadata& other) = default;
 
-    ColumnChunkMetadata operator()(std::span<const uint8_t> buffer, uint64_t capacity,
-        uint64_t numValues, StorageValue min, StorageValue max);
+    ColumnChunkMetadata operator()(std::span<const uint8_t> buffer, uint64_t numValues,
+        StorageValue min, StorageValue max);
 };
 
 template<std::floating_point T>
@@ -70,13 +70,12 @@ public:
 
     GetFloatCompressionMetadata(const GetFloatCompressionMetadata& other) = default;
 
-    ColumnChunkMetadata operator()(std::span<const uint8_t> buffer, uint64_t capacity,
-        uint64_t numValues, StorageValue min, StorageValue max);
+    ColumnChunkMetadata operator()(std::span<const uint8_t> buffer, uint64_t numValues,
+        StorageValue min, StorageValue max);
 };
 
-ColumnChunkMetadata uncompressedGetMetadata(std::span<const uint8_t> buffer, uint64_t capacity,
-    uint64_t numValues, StorageValue min, StorageValue max);
+ColumnChunkMetadata uncompressedGetMetadata(common::PhysicalTypeID dataType, uint64_t numValues,
+    StorageValue min, StorageValue max);
 
-ColumnChunkMetadata booleanGetMetadata(std::span<const uint8_t> buffer, uint64_t capacity,
-    uint64_t numValues, StorageValue min, StorageValue max);
+ColumnChunkMetadata booleanGetMetadata(uint64_t numValues, StorageValue min, StorageValue max);
 } // namespace kuzu::storage
