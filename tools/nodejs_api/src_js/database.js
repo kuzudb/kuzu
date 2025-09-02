778dd43ab06f08900be4d8c8313415e39f999f2f
Royi Luo <r34luo@uwaterloo.ca>
2025-09-02 12:51:56 -0400

    Remove unused param from nodeJS docs

--- tools/nodejs_api/src_js/database.js
+++ tools/nodejs_api/src_js/database.js
@@ -21,7 +21,6 @@ class Database {
    * the WAL file exceeds the checkpoint threshold.
    * @param {Number} checkpointThreshold The threshold of the WAL file size in bytes. When the size of the
    * WAL file exceeds this threshold, the database will checkpoint if autoCheckpoint is true.
-   * @param {Boolean} forceCheckpointOnClose If true, the database will force checkpoint when closing.
    * @param {Boolean} throwOnWalReplayFailure If true, any WAL replaying failure when loading the database
    * will throw an error. Otherwise, Kuzu will silently ignore the failure and replay up to where
    * the error occured.
