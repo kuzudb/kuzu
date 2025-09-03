6db551d023ca6984da132c5231fc0ecec9b916e4
EnyuPan <eric.enyu.pan@gmail.com>
2025-09-03 15:57:42 -0400

    Add uint128_t type

---
+++ test/common/uint128_test.cpp
@@ -0,0 +1,92 @@
+#include <cstdint>
+
+#include "common/exception/overflow.h"
+
+#include "common/types/uint128_t.h"
+#include "gtest/gtest.h"
+
+
+using namespace kuzu::common;
+
+TEST(Uint128Tests, ImplicitConversionToUint128) {
+    { // conversion from int32_t
+        uint128_t value = 42;
+        EXPECT_EQ(value.low, 42);
+        EXPECT_EQ(value.high, 0);
+    }
+    
+    { // conversion from int128_t 2^64 + 1
+        uint128_t value = int128_t {1, 1};
+        EXPECT_EQ(value.low, 1);
+        EXPECT_EQ(value.high, 1);
+    }
+}
+
+TEST(Uint128Tests, AdditionTest) {
+    { // adding in upper bits: (1 << 64) + (2 << 64) = (3 << 64)
+        uint128_t value1{0, 1};
+        uint128_t value2{0, 2};
+        uint128_t expected_value{0, 3};
+        EXPECT_EQ(expected_value, value1 + value2);
+        EXPECT_EQ(expected_value, Uint128_t::Add(value1, value2));
+    }
+
+    { // adding with overflow from lower to upper bits: (2^64 - 1) + 1 = (1 << 64)
+        uint128_t value1{UINT64_MAX, 0};
+        uint128_t value2{1, 0};
+        uint128_t expected_value{0, 1};
+        EXPECT_EQ(expected_value, value1 + value2);
+        EXPECT_EQ(expected_value, Uint128_t::Add(value1, value2));
+    }
+
+    { // (2^127 - 1) + 1 = 2^128
+        uint128_t value1{UINT64_MAX, INT64_MAX}; // 01111111...111
+        uint128_t value2{1, 0};
+        uint128_t expected_value{0, (1ULL << 63)};
+        EXPECT_EQ(expected_value, value1 + value2);
+        EXPECT_EQ(expected_value, Uint128_t::Add(value1, value2));
+    }
+
+    { // overflow
+        uint128_t value1{UINT64_MAX, UINT64_MAX}; // 11111111...111
+        uint128_t value2{1, 0};
+        EXPECT_THROW(Uint128_t::Add(value1, value2), OverflowException);
+    }
+}
+
+TEST(Uint128Tests, SubtractionTest) {
+    { // subtracting in upper bits: (3 << 64) - (2 << 64) = (1 << 64)
+        uint128_t value1{0, 3};
+        uint128_t value2{0, 2};
+        uint128_t expected_value{0, 1};
+        EXPECT_EQ(expected_value, value1 - value2);
+        EXPECT_EQ(expected_value, Uint128_t::Sub(value1, value2));
+    }
+
+    { // subtracting with underflow from upper to lower bits: (1 << 64) - 1 = (2^64 - 1)
+        uint128_t value1{0, 1};
+        uint128_t value2{1, 0};
+        uint128_t expected_value{UINT64_MAX, 0};
+        EXPECT_EQ(expected_value, value1 - value2);
+        EXPECT_EQ(expected_value, Uint128_t::Sub(value1, value2));
+    }
+
+    { // (2^127) - 1 = (2^127 - 1)
+        uint128_t value1{0, (1ULL << 63)}; // 10000000...000
+        uint128_t value2{1, 0};
+        uint128_t expected_value{UINT64_MAX, INT64_MAX}; // 01111111...111
+        EXPECT_EQ(expected_value, value1 - value2);
+        EXPECT_EQ(expected_value, Uint128_t::Sub(value1, value2));
+    }
+
+    { // underflow
+        uint128_t value1{1, 0};
+        uint128_t value2{2, 0};
+        EXPECT_THROW(Uint128_t::Sub(value1, value2), OverflowException);
+    }
+    {
+        uint128_t value1{0, 3};
+        uint128_t value2{5, 3};
+        EXPECT_THROW(Uint128_t::Sub(value1, value2), OverflowException);
+    }
+}
