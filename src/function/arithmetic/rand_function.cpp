42d11ce6426c9ad3741fa31e16e99df3d1cf6e0b
ziyi chen <chenziyi990424@gmail.com>
2025-09-03 15:06:20 +0800

    update

---
+++ src/function/arithmetic/rand_function.cpp
@@ -0,0 +1,26 @@
+#include "common/random_engine.h"
+#include "function/arithmetic/vector_arithmetic_functions.h"
+#include "function/scalar_function.h"
+#include "main/client_context.h"
+
+namespace kuzu {
+namespace function {
+
+using namespace kuzu::common;
+
+struct Rand {
+    static void operation(double& result, void* dataPtr) {
+        auto context = reinterpret_cast<FunctionBindData*>(dataPtr)->clientContext;
+        result = (double)(context->getRandomEngine()->nextRandomInteger()) / (double)UINT32_MAX;
+    }
+};
+
+function_set RandFunction::getFunctionSet() {
+    function_set result;
+    result.push_back(std::make_unique<ScalarFunction>(name, std::vector<LogicalTypeID>{},
+        LogicalTypeID::DOUBLE, ScalarFunction::NullaryAuxilaryExecFunction<double, Rand>));
+    return result;
+}
+
+} // namespace function
+} // namespace kuzu
