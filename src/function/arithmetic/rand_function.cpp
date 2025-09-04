1a3d28edf53414042e02136c789ac3e65b66931d
ziyi chen <chenziyi990424@gmail.com>
2025-09-04 09:41:54 +0800

    Implement rand related functions (#5953)

---
+++ src/function/arithmetic/rand_function.cpp
@@ -0,0 +1,27 @@
+#include "common/random_engine.h"
+#include "function/arithmetic/vector_arithmetic_functions.h"
+#include "function/scalar_function.h"
+#include "main/client_context.h"
+
+namespace kuzu {
+namespace function {
+
+using namespace kuzu::common;
+
+struct Rand {
+    static void operation(double& result, void* dataPtr) {
+        auto context = reinterpret_cast<FunctionBindData*>(dataPtr)->clientContext;
+        result = static_cast<double>(context->getRandomEngine()->nextRandomInteger()) /
+                 static_cast<double>(UINT32_MAX);
+    }
+};
+
+function_set RandFunction::getFunctionSet() {
+    function_set result;
+    result.push_back(std::make_unique<ScalarFunction>(name, std::vector<LogicalTypeID>{},
+        LogicalTypeID::DOUBLE, ScalarFunction::NullaryAuxilaryExecFunction<double, Rand>));
+    return result;
+}
+
+} // namespace function
+} // namespace kuzu
