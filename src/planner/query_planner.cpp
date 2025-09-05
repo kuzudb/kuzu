2b3aeca1c4e1aaeb8a64a3b7a980aad282810af8
andyfeng <xiyangf1997@gmail.com>
2025-09-05 09:18:02 +0800

    Fix empty union (#5966)
    * Fix empty union
    
    * ci: auto code format
    
    ---------
    
    Co-authored-by: CI Bot <andyfengHKU@users.noreply.github.com>

--- src/planner/query_planner.cpp
+++ src/planner/query_planner.cpp
@@ -17,10 +17,12 @@ LogicalPlan Planner::planQuery(const BoundStatement& boundStatement) {
     for (auto i = 0u; i < regularQuery.getNumSingleQueries(); i++) {
         childrenPlans.push_back(planSingleQuery(*regularQuery.getSingleQuery(i)));
     }
-    return createUnionPlan(childrenPlans, regularQuery.getIsUnionAll(0));
+    auto exprs = regularQuery.getStatementResult()->getColumns();
+    return createUnionPlan(childrenPlans, exprs, regularQuery.getIsUnionAll(0));
 }
 
-LogicalPlan Planner::createUnionPlan(std::vector<LogicalPlan>& childrenPlans, bool isUnionAll) {
+LogicalPlan Planner::createUnionPlan(std::vector<LogicalPlan>& childrenPlans,
+    const expression_vector& expressions, bool isUnionAll) {
     KU_ASSERT(!childrenPlans.empty());
     auto plan = LogicalPlan();
     std::vector<std::shared_ptr<LogicalOperator>> children;
@@ -29,16 +31,15 @@ LogicalPlan Planner::createUnionPlan(std::vector<LogicalPlan>& childrenPlans, bo
         children.push_back(childPlan.getLastOperator());
     }
     // we compute the schema based on first child
-    auto union_ = std::make_shared<LogicalUnion>(
-        childrenPlans[0].getSchema()->getExpressionsInScope(), std::move(children));
+    auto union_ = std::make_shared<LogicalUnion>(expressions, std::move(children));
     for (auto i = 0u; i < childrenPlans.size(); ++i) {
         appendFlattens(union_->getGroupsPosToFlatten(i), childrenPlans[i]);
         union_->setChild(i, childrenPlans[i].getLastOperator());
     }
     union_->computeFactorizedSchema();
     plan.setLastOperator(union_);
-    if (!isUnionAll) {
-        appendDistinct(union_->getExpressionsToUnion(), plan);
+    if (!isUnionAll && !expressions.empty()) {
+        appendDistinct(expressions, plan);
     }
     return plan;
 }
