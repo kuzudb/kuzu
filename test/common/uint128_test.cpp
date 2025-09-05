220f35ac2d616eea4fd3837ba24beb946ac2409c
Enyu Pan <eric.enyu.pan@gmail.com>
2025-09-05 16:44:14 -0400

    Fix implicit casting between int128_t and uint128_t, using operator() instead of constructor / tryCastTo.

--- test/common/uint128_test.cpp
+++ test/common/uint128_test.cpp
@@ -3,6 +3,7 @@
 #include "common/exception/overflow.h"
 
 #include "common/types/uint128_t.h"
+#include "common/types/int128_t.h"
 #include "gtest/gtest.h"
 
 
@@ -16,9 +17,26 @@ TEST(Uint128Tests, ImplicitConversionToUint128) {
     }
     
     { // conversion from int128_t 2^64 + 1
-        uint128_t value = int128_t {1, 1};
+        uint128_t value = int128_t((uint64_t)1, (int64_t)1);
         EXPECT_EQ(value.low, 1);
         EXPECT_EQ(value.high, 1);
+    }
+
+    { // conversion to int128_t
+        uint128_t value = {35, 165};
+        int128_t int128_value = value;
+        EXPECT_EQ(int128_value.low, 35);
+        EXPECT_EQ(int128_value.high, 165);
+    }
+
+    { // conversion from int128_t (error: negative value)
+        int128_t negative_two = -2;
+        EXPECT_THROW(uint128_t value = negative_two, OverflowException);
+    }
+
+    { // conversion to int128_t (error: overflow)
+        uint128_t max_uint128 = {UINT64_MAX, UINT64_MAX};
+        EXPECT_THROW(int128_t value = max_uint128, OverflowException);
     }
 }
 
@@ -84,6 +102,7 @@ TEST(Uint128Tests, SubtractionTest) {
         uint128_t value2{2, 0};
         EXPECT_THROW(Uint128_t::Sub(value1, value2), OverflowException);
     }
+
     {
         uint128_t value1{0, 3};
         uint128_t value2{5, 3};
