a35352550377440c2ac0f25776188c824c295337
Jasonlmx <56242204+Jasonlmx@users.noreply.github.com>
2025-09-08 09:42:45 -0400

    added drop function for macro, but no entry

--- src/include/processor/operator/ddl/drop.h
+++ src/include/processor/operator/ddl/drop.h
@@ -39,6 +39,7 @@ public:
 private:
     void dropSequence(const main::ClientContext* context);
     void dropTable(const main::ClientContext* context);
+    void dropMacro(const main::ClientContext* context);
     void dropRelGroup(const main::ClientContext* context);
 
 private:
