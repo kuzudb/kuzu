4edb0976f494c6efa10c9901ada4132efa9bcc9a
Anthony Xu <xu_mingyang@outlook.com>
2025-09-09 16:32:54 -0400

    Adding a new SKIP_FSM_LEAK_CHECK token to e2e tests
    # Conflicts:
    #	test/test_runner/fsm_leak_checker.cpp

--- test/include/test_runner/test_parser.h
+++ test/include/test_runner/test_parser.h
@@ -10,7 +10,6 @@
 
 namespace kuzu {
 namespace testing {
-
 enum class TokenType {
     // header only
     DATASET,
@@ -49,6 +48,8 @@ enum class TokenType {
     MULTI_COPY_RANDOM,
     LOAD_DYNAMIC_EXTENSION,
     SEED,
+    // SKIP_FSM_LEAK_CHECKER works only per-test for now, not test-file-wide.
+    SKIP_FSM_LEAK_CHECKER,
 
     CHECKPOINT_WAIT_TIMEOUT,
     CREATE_CONNECTION,
@@ -93,7 +94,8 @@ const std::unordered_map<std::string, TokenType> TOKEN_MAP = {{"-DATASET", Token
     {"-CREATE_DATASET_SCHEMA", TokenType::CREATE_DATASET_SCHEMA},
     {"-INSERT_DATASET_BY_ROW", TokenType::INSERT_DATASET_BY_ROW},
     {"-MULTI_COPY_RANDOM", TokenType::MULTI_COPY_RANDOM}, {"-LOOP", TokenType::LOOP},
-    {"-ENDLOOP", TokenType::ENDLOOP}};
+    {"-ENDLOOP", TokenType::ENDLOOP},
+    {"-SKIP_FSM_LEAK_CHECK", TokenType::SKIP_FSM_LEAK_CHECKER}};
 
 class LogicToken {
 public:
