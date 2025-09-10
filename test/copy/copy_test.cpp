0db479ae7e860279af4109c06ed45c2ebbefa575
Royi Luo <r.luo@hotmail.ca>
2025-09-10 17:15:08 -0400

    Revert "Adding a new FSM token and running FSMLeakChecker for all e2e tests (…"
    This reverts commit 5a09d79fed5d30e5f8e5d137a7679cd62e63348c.

--- test/copy/copy_test.cpp
+++ test/copy/copy_test.cpp
@@ -5,7 +5,6 @@
 #include "storage/buffer_manager/buffer_manager.h"
 #include "storage/checkpointer.h"
 #include "storage/storage_manager.h"
-#include "test_runner/fsm_leak_checker.h"
 #include "test_runner/test_parser.h"
 #include "transaction/transaction_manager.h"
 
@@ -113,6 +112,57 @@ public:
     void BMExceptionRecoveryTest(BMExceptionRecoveryTestConfig cfg);
     std::atomic<uint64_t> failureFrequency;
     FlakyBufferManager* currentBM;
+};
+
+static decltype(auto) getDbSizeInPages(main::Connection* conn) {
+    return storage::StorageManager::Get(*conn->getClientContext())->getDataFH()->getNumPages();
+}
+
+static decltype(auto) getNumFreePages(main::Connection* conn) {
+    auto& pageManager = *storage::PageManager::Get(*conn->getClientContext());
+    auto numFreeEntries = pageManager.getNumFreeEntries();
+    auto entries = pageManager.getFreeEntries(0, numFreeEntries);
+    return std::accumulate(entries.begin(), entries.end(), 0ull,
+        [](auto a, auto b) { return a + b.numPages; });
+}
+
+struct FSMLeakChecker {
+    static void checkForLeakedPages(main::Connection* conn) {
+        conn->query("checkpoint");
+
+        std::vector<std::pair<std::string, std::string>> tableNames;
+        auto tableNamesResult = conn->query("call show_tables() return name, type");
+        while (tableNamesResult->hasNext()) {
+            auto nextResult = tableNamesResult->getNext();
+            tableNames.emplace_back(nextResult->getValue(0)->toString(),
+                nextResult->getValue(1)->toString());
+        }
+
+        // Drop rel tables first
+        for (const auto& [tableName, tableType] : tableNames) {
+            if (tableType == common::TableTypeUtils::toString(common::TableType::REL)) {
+                ASSERT_TRUE(
+                    conn->query(common::stringFormat("drop table {}", tableName))->isSuccess());
+            }
+        }
+        for (const auto& [tableName, tableType] : tableNames) {
+            if (tableType != common::TableTypeUtils::toString(common::TableType::REL)) {
+                ASSERT_TRUE(
+                    conn->query(common::stringFormat("drop table {}", tableName))->isSuccess());
+            }
+        }
+        conn->query("checkpoint");
+
+        const auto numTotalPages = getDbSizeInPages(conn);
+        const auto numUsedPages = numTotalPages - getNumFreePages(conn);
+
+        storage::Checkpointer checkpointer{*conn->getClientContext()};
+        auto databaseHeader = storage::StorageManager::Get(*conn->getClientContext())
+                                  ->getOrInitDatabaseHeader(*conn->getClientContext());
+        ASSERT_EQ(1 + databaseHeader->catalogPageRange.numPages +
+                      databaseHeader->metadataPageRange.numPages,
+            numUsedPages);
+    }
 };
 
 void CopyTest::BMExceptionRecoveryTest(BMExceptionRecoveryTestConfig cfg) {
