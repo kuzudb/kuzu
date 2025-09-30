cad08c35290b6b98d81738a3bd844f5edf3726e9
Chang Liu <liuc223@gmail.com>
2025-09-30 08:37:02 -0400

    Refactor static link extension configuration for clarity and consistency

--- extension/extension_config.cmake
+++ extension/extension_config.cmake
@@ -1,15 +1,9 @@
 set(EXTENSION_LIST azure delta duckdb fts httpfs iceberg json llm postgres sqlite unity_catalog vector neo4j algo)
 
-#set(EXTENSION_STATIC_LINK_LIST algo fts json vector)
+set(EXTENSION_STATIC_LINK_LIST algo fts json vector)
 string(JOIN ", " joined_extensions ${EXTENSION_STATIC_LINK_LIST})
 message(STATUS "Static link extensions: ${joined_extensions}")
 foreach(extension IN LISTS EXTENSION_STATIC_LINK_LIST)
     add_static_link_extension(${extension})
 endforeach()
 
-if (NOT BUILD_EXTENSIONS EQUAL "") 
-    add_static_link_extension(fts)
-    add_static_link_extension(json)
-    add_static_link_extension(vector)
-    add_static_link_extension(algo)
-endif()
